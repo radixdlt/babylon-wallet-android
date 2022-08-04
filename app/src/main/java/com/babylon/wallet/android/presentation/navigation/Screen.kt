da410dc80f571fe36ca8c073117af8494b7d01c0
giannis.tsepas <john87ts@gmail.com>
2022-08-04 19:00:36 +0300

    added Screen class which holds all screens and destinations

---
+++ app/src/main/java/com/babylon/wallet/android/presentation/navigation/Screen.kt
@@ -0,0 +1,31 @@
+package com.babylon.wallet.android.presentation.navigation
+
+interface Destination {
+    /**
+     * Defines a specific route this destination belongs to.
+     * Route is a String that defines the path to your composable.
+     * You can think of it as an implicit deep link that leads to a specific destination.
+     * Each destination should have a unique route.
+     */
+    val route: String
+}
+
+sealed class Screen(override val route: String) : Destination {
+
+    object WalletDestination : Screen("wallet_route")
+    object AccountDestination : Screen("account_route")
+
+    fun routeWithArgs(vararg args: String): String {
+        return buildString {
+            append(route)
+            args.forEach { arg ->
+                append("/$arg")
+            }
+        }
+    }
+
+    companion object {
+        const val ARG_ACCOUNT_ID = "arg_account_id"
+        const val ARG_ACCOUNT_NAME = "arg_account_name"
+    }
+}
