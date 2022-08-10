eb15e7b121dc0bd57827724ca622d23807cbb05c
giannis.tsepas <john87ts@gmail.com>
2022-08-11 00:24:30 +0300

    updated tests in WalletViewModelTest with the new data models

--- app/src/test/java/com/babylon/wallet/android/mockdata/MockAccounts.kt
+++ app/src/test/java/com/babylon/wallet/android/mockdata/MockAccounts.kt
@@ -2,31 +2,35 @@ package com.babylon.wallet.android.mockdata
 
 import com.babylon.wallet.android.data.AccountDto
 import com.babylon.wallet.android.data.AccountDto.Companion.toUiModel
+import com.babylon.wallet.android.data.mockdata.mockAssetDtoList
 
-val mockAccountsDto = listOf(
+val mockAccountDtoList = listOf(
     AccountDto(
         id = "a1",
         name = "My main account",
         hash = "0x589e5cb09935F67c441AEe6AF46A365274a932e3",
         value = 19195.0F,
-        currency = "$"
+        currency = "$",
+        assets = mockAssetDtoList
     ),
     AccountDto(
         id = "a2",
         name = "My fun account",
         hash = "0x589e5cb06635F67c441EAe6AF46A365278a932e1",
         value = 214945.5F,
-        currency = "$"
+        currency = "$",
+        assets = mockAssetDtoList
     ),
     AccountDto(
         id = "a2",
         name = "Only NFTs",
         hash = "0x559e5cb66035F67c441EAe6AF46A474278a932e1",
         value = 12149455.0F,
-        currency = "$"
+        currency = "$",
+        assets = mockAssetDtoList
     )
 )
 
-val mockAccountsUI = mockAccountsDto.map { accountDto ->
+val mockAccountUiList = mockAccountDtoList.map { accountDto ->
     accountDto.toUiModel()
 }
