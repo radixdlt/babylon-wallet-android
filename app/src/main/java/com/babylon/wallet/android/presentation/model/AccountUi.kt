97063ee4f98d6a980c0ec85baf87e523f5f3255f
giannis.tsepas <john87ts@gmail.com>
2022-08-16 22:34:49 +0300

    added read only val hasXrdToken in AccountUi

--- app/src/main/java/com/babylon/wallet/android/presentation/model/AccountUi.kt
+++ app/src/main/java/com/babylon/wallet/android/presentation/model/AccountUi.kt
@@ -9,6 +9,10 @@ data class AccountUi(
     val tokens: List<TokenUi> = emptyList(),
     val nfts: List<NftClassUi> = emptyList()
 ) {
+
+    val hasXrdToken: Boolean
+        get() = tokens[0].symbol == "XRD"
+
     val nftsSortedByName: List<NftClassUi> get() = nfts.sortedBy {
         it.name
     }
