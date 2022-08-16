1314c77595c2447e246b6e1b5745cc9a4dd7b82c
giannis.tsepas <john87ts@gmail.com>
2022-08-16 11:43:50 +0300

    added tokenItemTitle that used for displaying name of the token item

--- app/src/main/java/com/babylon/wallet/android/presentation/model/TokenUi.kt
+++ app/src/main/java/com/babylon/wallet/android/presentation/model/TokenUi.kt
@@ -3,8 +3,18 @@ package com.babylon.wallet.android.presentation.model
 data class TokenUi(
     val id: String,
     val name: String?,
-    val symbol: String?, // short name
+    val symbol: String?, // short capitalized name
     val tokenQuantity: String, // the amount of the token held
     val tokenValue: String?, // the current value in currency the user has selected for the wallet
-    val iconUrl: String?,
-)
+    val iconUrl: String?
+) {
+    /**
+     * The title to show in the token list item of the Account screen.
+     *
+     * It is based of the token display rule:
+     *
+     * https://radixdlt.atlassian.net/wiki/spaces/AT/pages/2844753933/Rules+for+Display+of+Assets+in+Account+Detail+View
+     *
+     */
+    val tokenItemTitle: String get() = symbol ?: name.orEmpty()
+}
