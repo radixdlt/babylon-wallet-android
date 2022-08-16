5e9317c96c870bec3725b43adbb0224658c8ca59
giannis.tsepas <john87ts@gmail.com>
2022-08-16 22:34:21 +0300

    added sorting algorithm for tokens in AccountDto

--- app/src/main/java/com/babylon/wallet/android/data/AccountDto.kt
+++ app/src/main/java/com/babylon/wallet/android/data/AccountDto.kt
@@ -4,6 +4,7 @@ import com.babylon.wallet.android.data.AssetDto.Companion.toTokenUiModel
 import com.babylon.wallet.android.data.AssetDto.NftClassDto.Companion.toNftClassUiModel
 import com.babylon.wallet.android.domain.AssetType
 import com.babylon.wallet.android.presentation.model.AccountUi
+import com.babylon.wallet.android.presentation.model.TokenUi
 import java.text.NumberFormat
 import java.util.Locale
 
@@ -26,11 +27,11 @@ data class AccountDto(
             hash = hash,
             amount = amountToUiFormat(value, currency),
             currencySymbol = currency,
-            tokens = assets.filter { assetDto ->
-                assetDto.type == AssetType.TOKEN.typeId
-            }.map { assetDto ->
-                assetDto.toTokenUiModel()
-            },
+            tokens = sortTokens(
+                assets.filter { assetDto ->
+                    assetDto.type == AssetType.TOKEN.typeId
+                }
+            ),
             nfts = assets.filter { assetDto ->
                 assetDto.type == AssetType.NTF.typeId
             }.flatMap { assetDto ->
@@ -39,6 +40,49 @@ data class AccountDto(
                 }.orEmpty()
             }
         )
+    }
+
+    // 1. The Radix token (XRD), if the account holds any, is always shown at top of the list
+    // 2. Those that have a market price (= tokenValue) available are all displayed above those that do not have a one
+    // 3. Within the tokens that have a market price, sort them by the total value the user holds of
+    //    that token (not the price of 1 unit of the token), highest to lowest
+    //
+    // We might move this later to the domain layer and keep there the business logic!
+    // AccountDto => Account, Token, Nft + sorting => TokenUi, NftUi, ...
+    private fun sortTokens(tokensList: List<AssetDto>): List<TokenUi> {
+        val indexOfXrdToken = tokensList.indexOfFirst { assetDto ->
+            // TODO when we have the actual backend we'll update this
+            assetDto.symbol == "XRD" && assetDto.name == "Radix"
+        }
+
+        return if (indexOfXrdToken > 0) {
+            val tempTokensList = tokensList.toMutableList()
+            val xrdToken = tempTokensList.removeAt(indexOfXrdToken)
+
+            val tokensListWithoutXrd = tempTokensList.map { assetDto ->
+                assetDto.toTokenUiModel()
+            }.subList(0, tempTokensList.size)
+
+            val tokensListSortedByTokenValueWithoutXrd = tokensListWithoutXrd
+                .sortedWith(
+                    compareBy(nullsLast()) { tokenUi ->
+                        tokenUi.tokenValue
+                    }
+                )
+
+            val tokensListSortedByTokenValueWithXrdFirst = tokensListSortedByTokenValueWithoutXrd.toMutableList()
+            tokensListSortedByTokenValueWithXrdFirst.add(0, xrdToken.toTokenUiModel())
+            tokensListSortedByTokenValueWithXrdFirst
+        } else {
+            val tempTokensList = tokensList.map { assetDto ->
+                assetDto.toTokenUiModel()
+            }
+            tempTokensList.sortedWith(
+                compareBy(nullsLast()) { tokenUi ->
+                    tokenUi.tokenValue
+                }
+            )
+        }
     }
 
     // TODO the format api returns the symbol alongside the amount,
