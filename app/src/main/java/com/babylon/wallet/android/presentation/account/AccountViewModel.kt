9771e4b8c53ae35b06e4a4db62dad9b38d768c61
giannis.tsepas <john87ts@gmail.com>
2022-08-16 11:44:35 +0300

    removed uri decode in AccountViewModel

--- app/src/main/java/com/babylon/wallet/android/presentation/account/AccountViewModel.kt
+++ app/src/main/java/com/babylon/wallet/android/presentation/account/AccountViewModel.kt
@@ -2,7 +2,6 @@ package com.babylon.wallet.android.presentation.account
 
 import android.content.ClipData
 import android.content.ClipboardManager
-import android.net.Uri
 import android.util.Log
 import androidx.annotation.StringRes
 import androidx.lifecycle.SavedStateHandle
@@ -25,7 +24,7 @@ class AccountViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle
 ) : ViewModel() {
 
-    private val accountId: String = Uri.decode(savedStateHandle.get<String>(ARG_ACCOUNT_ID)).orEmpty()
+    private val accountId: String = savedStateHandle.get<String>(ARG_ACCOUNT_ID).orEmpty()
 
     private val _accountUiState: MutableStateFlow<AccountUiState> = MutableStateFlow(AccountUiState.Loading)
     val accountUiState = _accountUiState.asStateFlow()
