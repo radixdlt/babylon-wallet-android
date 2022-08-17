6cc783fe5de2928e7d92cce4dac3924796095274
Rafal Wesolowski <rafal.wesolowski@rdx.works>
2022-08-17 14:24:25 +0200

    ABW-367 - Wallet fiat amount formatted.

--- app/src/main/java/com/babylon/wallet/android/presentation/ui/extensions/StringExtensions.kt
+++ app/src/main/java/com/babylon/wallet/android/presentation/ui/extensions/StringExtensions.kt
@@ -1,8 +1,20 @@
 @file:Suppress("MagicNumber")
 package com.babylon.wallet.android.presentation.ui.extensions
 
+import java.text.DecimalFormat
+
 fun String.truncatedHash(): String {
     val first = substring(IntRange(0, 3))
     val last = substring(IntRange(length - 9, length - 1))
     return "$first...$last"
+}
+
+fun String.formatDecimalSeparator(): String {
+    if (isEmpty()) return this
+    val formatter = DecimalFormat("#,###.00")
+    return try {
+        formatter.format(toDouble())
+    } catch (e: NumberFormatException) {
+        ""
+    }
 }
