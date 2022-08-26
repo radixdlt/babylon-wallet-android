600871ec9d6eec1fae0a74e8f2359d8f5d1eea6e
Rafal Wesolowski <rafal.wesolowski@rdx.works>
2022-08-26 12:09:44 +0200

    ABW-281 / ABW-269 - Unit tests fixed.

--- app/src/test/java/com/babylon/wallet/android/presentation/OnboardingViewModelTest.kt
+++ app/src/test/java/com/babylon/wallet/android/presentation/OnboardingViewModelTest.kt
@@ -2,13 +2,20 @@ package com.babylon.wallet.android.presentation
 
 import com.babylon.wallet.android.data.DataStoreManager
 import com.babylon.wallet.android.presentation.onboarding.OnboardingViewModel
+import com.babylon.wallet.android.utils.SecurityHelper
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
+import org.junit.Assert
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mockito
 import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
 
 @ExperimentalCoroutinesApi
 class OnboardingViewModelTest {
@@ -18,26 +25,55 @@ class OnboardingViewModelTest {
 
     private val dataStoreManager = Mockito.mock(DataStoreManager::class.java)
 
+    private val securityHelper = Mockito.mock(SecurityHelper::class.java)
+
     @Test
-    fun `when set to show onboarding verify shown`() = runTest {
+    fun `given device security is setup, when proceeding next, verify biometric auth happens`() = runTest {
         // given
-        val viewModel = OnboardingViewModel(dataStoreManager)
+        whenever(securityHelper.isDeviceSecure()).thenReturn(true)
+        val event = mutableListOf<OnboardingViewModel.OnboardingUiAction>()
+        val viewModel = OnboardingViewModel(dataStoreManager, securityHelper)
 
         // when
-        viewModel.setShowOnboarding(true)
+        viewModel.onboardingUiAction
+            .onEach { event.add(it) }
+            .launchIn(CoroutineScope(UnconfinedTestDispatcher(testScheduler)))
+
+        viewModel.onProceedClick()
+
         advanceUntilIdle()
 
         // then
-        verify(dataStoreManager).setShowOnboarding(true)
+        Assert.assertEquals(event.first(), OnboardingViewModel.OnboardingUiAction.AuthenticateWithBiometric)
     }
 
     @Test
-    fun `when set to not show onboarding, verify not shown`() = runTest {
+    fun `given device security is not setup, when proceeding next, verify security warning shown`() = runTest {
         // given
-        val viewModel = OnboardingViewModel(dataStoreManager)
+        whenever(securityHelper.isDeviceSecure()).thenReturn(false)
+        val event = mutableListOf<OnboardingViewModel.OnboardingUiAction>()
+        val viewModel = OnboardingViewModel(dataStoreManager, securityHelper)
 
         // when
-        viewModel.setShowOnboarding(false)
+        viewModel.onboardingUiAction
+            .onEach { event.add(it) }
+            .launchIn(CoroutineScope(UnconfinedTestDispatcher(testScheduler)))
+
+        viewModel.onProceedClick()
+
+        advanceUntilIdle()
+
+        // then
+        Assert.assertEquals(event.first(), OnboardingViewModel.OnboardingUiAction.ShowSecurityWarning)
+    }
+
+    @Test
+    fun `when user authenticated, verify onboarding not shown anymore`() = runTest {
+        // given
+        val viewModel = OnboardingViewModel(dataStoreManager, securityHelper)
+
+        // when
+        viewModel.onUserAuthenticated()
         advanceUntilIdle()
 
         // then
