ff49ab6d5e39e234c4e1b1b4d76b8aeef7340a38
Rafal Wesolowski <rafal.wesolowski@rdx.works>
2022-10-10 16:01:45 +0200

    ABW-308 - dApp connection request screen implemented.

--- app/src/main/java/com/babylon/wallet/android/presentation/dapp/DAppConnectionRequestScreen.kt
+++ app/src/main/java/com/babylon/wallet/android/presentation/dapp/DAppConnectionRequestScreen.kt
@@ -1,13 +1,13 @@
-package com.babylon.wallet.android.presentation.createaccount
+package com.babylon.wallet.android.presentation.dapp
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -16,18 +16,13 @@ import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
-import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -35,6 +30,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.rememberAsyncImagePainter
 import com.babylon.wallet.android.R
 import com.babylon.wallet.android.presentation.ui.theme.RadixBackground
 import com.babylon.wallet.android.presentation.ui.theme.RadixButtonBackground
@@ -42,14 +38,13 @@ import com.babylon.wallet.android.presentation.ui.theme.RadixGrey2
 import com.babylon.wallet.android.presentation.ui.theme.White
 
 @Composable
-fun CreateAccountScreen(
+fun DAppConnectionRequestScreen(
     onBackClick: () -> Unit,
-    modifier: Modifier = Modifier,
-    onContinueClick: (accountId: String, accountName: String) -> Unit = { _: String, _: String -> },
+    onContinueClick: () -> Unit,
+    imageUrl: String,
+    labels: List<String>,
+    modifier: Modifier = Modifier
 ) {
-    var buttonEnabled by rememberSaveable { mutableStateOf(false) }
-    var accountName by rememberSaveable { mutableStateOf("") }
-    val maxLength = 20
 
     Column(
         modifier = modifier.fillMaxSize()
@@ -63,63 +58,60 @@ fun CreateAccountScreen(
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(horizontal = 30.dp, vertical = 16.dp)
+                .padding(horizontal = 50.dp, vertical = 16.dp)
                 .verticalScroll(rememberScrollState()),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Image(
-                painter = painterResource(R.drawable.img_account_creation),
-                contentDescription = "account_creation_image"
-            )
             Spacer(modifier = Modifier.height(16.dp))
             Text(
-                text = stringResource(id = R.string.create_new_account),
+                text = stringResource(id = R.string.dapp_connection_request),
                 textAlign = TextAlign.Center,
-                fontSize = 24.sp,
+                fontSize = 18.sp,
                 fontWeight = FontWeight.SemiBold
             )
             Spacer(modifier = Modifier.height(40.dp))
+            Image(
+                painter = rememberAsyncImagePainter(
+                    model = imageUrl,
+                    placeholder = painterResource(id = R.drawable.img_placeholder),
+                    error = painterResource(id = R.drawable.img_placeholder)
+                ),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(110.dp)
+                    .clip(RoundedCornerShape(8.dp))
+            )
+            Spacer(modifier = Modifier.height(40.dp))
             Text(
-                text = stringResource(id = R.string.account_creation_text),
+                text = stringResource(id = R.string.radaswap_wants_to_connect_wallet),
                 textAlign = TextAlign.Center,
                 fontSize = 18.sp,
+                fontWeight = FontWeight.SemiBold
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                color = RadixGrey2,
+                text = stringResource(id = R.string.for_this_dapp_to_function),
+                textAlign = TextAlign.Center,
+                fontSize = 16.sp,
                 fontWeight = FontWeight.Normal
             )
-            Spacer(modifier = Modifier.height(30.dp))
-            Column(modifier = Modifier.fillMaxWidth()) {
-                TextField(
-                    value = accountName,
-                    onValueChange = {
-                        buttonEnabled = it.isNotEmpty()
-                        accountName = it.take(maxLength)
-                    },
-                    modifier = Modifier
-                        .border(width = 1.dp, color = Color.Black, shape = RoundedCornerShape(4.dp))
-                        .fillMaxWidth(),
-                    label = { Text(stringResource(id = R.string.account_name)) },
-                    shape = RoundedCornerShape(4.dp),
-                    colors = TextFieldDefaults.textFieldColors(
-                        focusedIndicatorColor = Color.Transparent,
-                        disabledIndicatorColor = Color.Transparent,
-                        unfocusedIndicatorColor = Color.Transparent,
-                        backgroundColor = Color.LightGray,
-                    )
-                )
+            Spacer(modifier = Modifier.height(34.dp))
+            labels.forEach { labelTitleResource ->
                 Text(
-                    text = stringResource(id = R.string.this_can_be_changed_any_time),
+                    text = labelTitleResource,
+                    textAlign = TextAlign.Start,
                     fontSize = 16.sp,
-                    fontWeight = FontWeight.Normal,
-                    color = RadixGrey2
+                    fontWeight = FontWeight.Normal
                 )
-                Spacer(modifier = Modifier.height(16.dp))
             }
             Spacer(Modifier.weight(1f))
             Button(
-                modifier = Modifier.fillMaxWidth(),
-                // TODO Im gonna revist this to handle that from viewmodel nicely.
-                //  In the meantime, we dont have account generation so i put hardcoded stuff here
-                onClick = { onContinueClick("di20ejdnd2e20e2", accountName) },
-                enabled = buttonEnabled,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 0.dp, vertical = 30.dp),
+                onClick = { onContinueClick() },
                 colors = ButtonDefaults.buttonColors(
                     backgroundColor = RadixButtonBackground,
                     disabledBackgroundColor = RadixBackground
@@ -138,9 +130,14 @@ fun CreateAccountScreen(
 @Preview(showBackground = true)
 @Preview("large font", fontScale = 2f, showBackground = true)
 @Composable
-fun CreateAccountPreview() {
-    CreateAccountScreen(
+fun DAppConnectionRequestScreenPreview() {
+    DAppConnectionRequestScreen(
         onBackClick = {},
-        onContinueClick = { _, _ -> }
+        onContinueClick = { },
+        imageUrl = "",
+        labels = listOf(
+            stringResource(id = R.string.dapp_condition1),
+            stringResource(id = R.string.dapp_condition2)
+        )
     )
 }
