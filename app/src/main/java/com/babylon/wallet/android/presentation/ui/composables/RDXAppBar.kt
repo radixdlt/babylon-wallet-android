cf8d533194669f6ded1114a0f3df0fe27760b356
giannis.tsepas <john87ts@gmail.com>
2022-11-22 11:49:21 +0200

    minor refactoring in RDXAppBar

--- app/src/main/java/com/babylon/wallet/android/presentation/ui/composables/RDXAppBar.kt
+++ app/src/main/java/com/babylon/wallet/android/presentation/ui/composables/RDXAppBar.kt
@@ -1,27 +1,30 @@
 package com.babylon.wallet.android.presentation.ui.composables
 
 import android.content.res.Configuration
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Badge
-import androidx.compose.material.BadgedBox
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.babylon.wallet.android.R
+import com.babylon.wallet.android.presentation.ui.theme.BabylonWalletTheme
 import androidx.compose.ui.unit.dp
 import com.babylon.wallet.android.designsystem.theme.BabylonWalletTheme
 import com.babylon.wallet.android.designsystem.theme.RadixTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RDXAppBar(
     toolbarTitle: String,
     modifier: Modifier = Modifier,
-    onMenuItemClicked: () -> Unit
+    onMenuClick: () -> Unit
 ) {
     TopAppBar(
         modifier = modifier,
@@ -44,8 +47,7 @@ fun RDXAppBar(
                     )
                 }
             }
-        },
-        elevation = 0.dp
+        }
     )
 }
 
@@ -58,7 +60,7 @@ fun RDXAppBarPreview() {
     BabylonWalletTheme {
         RDXAppBar(
             toolbarTitle = "Account",
-            onMenuItemClicked = {}
+            onMenuClick = {}
         )
     }
 }
