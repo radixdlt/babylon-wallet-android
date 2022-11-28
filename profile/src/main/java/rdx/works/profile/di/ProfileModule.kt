499d5b4a357dd53f8e1bc6e91315528c880c4920
Rafal Wesolowski <rafal.wesolowski@rdx.works>
2022-11-28 12:57:27 +0100

    ABW-494 - Binding in the module added.

--- profile/src/main/java/rdx/works/profile/di/ProfileModule.kt
+++ profile/src/main/java/rdx/works/profile/di/ProfileModule.kt
@@ -1,13 +1,24 @@
 package rdx.works.profile.di
 
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import rdx.works.profile.Crypto
-import rdx.works.profile.CryptoImpl
+import rdx.works.profile.data.crypto.Crypto
+import rdx.works.profile.data.crypto.CryptoImpl
+import rdx.works.profile.data.repository.ProfileRepository
+import rdx.works.profile.data.repository.ProfileRepositoryImpl
+import rdx.works.profile.domain.GetMnemonicUseCase
+import rdx.works.profile.domain.GenerateProfileUseCase
 import rdx.works.profile.enginetoolkit.EngineToolkit
 import rdx.works.profile.enginetoolkit.EngineToolkitImpl
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -22,5 +33,10 @@ interface ProfileModule {
     fun bindEngineToolkit(
         engineToolkitImpl: EngineToolkitImpl
     ): EngineToolkit
+
+    @Binds
+    fun bindProfileRepository(
+        profileRepositoryImpl: ProfileRepositoryImpl
+    ): ProfileRepository
 
 }
