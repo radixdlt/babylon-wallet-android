8a7c287dc028dffa4099875eb179abd821c5d8b9
Jakub Porzuczek <jakub.porzuczek@rdx.works>
2022-11-30 12:29:43 +0100

    add empty states for tokens and NFTs

--- app/src/main/java/com/babylon/wallet/android/presentation/ui/composables/NftTokenList.kt
+++ app/src/main/java/com/babylon/wallet/android/presentation/ui/composables/NftTokenList.kt
@@ -1,6 +1,7 @@
 package com.babylon.wallet.android.presentation.ui.composables
 
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -9,10 +10,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.babylon.wallet.android.R
 import com.babylon.wallet.android.data.mockdata.mockNftUiList
 import com.babylon.wallet.android.designsystem.theme.BabylonWalletTheme
+import com.babylon.wallet.android.presentation.account.AssetEmptyState
 import com.babylon.wallet.android.presentation.model.NftUiModel
 
 @Suppress("MutableParams", "UnstableCollections")
@@ -22,42 +26,64 @@ fun NftTokenList(
     item: List<NftUiModel>,
     modifier: Modifier = Modifier
 ) {
-    LazyColumn(modifier) {
-        item.forEachIndexed { i, dataItem ->
-            val collapsed = collapsedState[i]
-            item(key = "header_$i") {
-                NftTokenHeaderItem(
-                    nftImageUrl = dataItem.iconUrl,
-                    nftName = dataItem.name,
-                    nftsInCirculation = "?",
-                    nftsInPossession = dataItem.nft.size.toString(),
-                    nftChildCount = dataItem.nft.size,
-                    collapsed = collapsed
-                ) {
-                    collapsedState[i] = !collapsed
-                }
-            }
-            if (!collapsed) {
-                items(
-                    dataItem.nft,
-                    key = { nft -> nft.id }
-                ) { item ->
-                    var bottomCornersRounded = false
-                    if (dataItem.nft.last() == item) {
-                        bottomCornersRounded = true
+    if (item.isEmpty()) {
+        AssetEmptyState(
+            modifier = Modifier.fillMaxSize(),
+            title = stringResource(id = R.string.you_have_no_nfts),
+            subtitle = stringResource(
+                R.string.what_are_nfts
+            ),
+            onInfoClick = {}
+        )
+    } else {
+        LazyColumn(modifier) {
+            item.forEachIndexed { i, dataItem ->
+                val collapsed = collapsedState[i]
+                item(key = "header_$i") {
+                    NftTokenHeaderItem(
+                        nftImageUrl = dataItem.iconUrl,
+                        nftName = dataItem.name,
+                        nftsInCirculation = "?",
+                        nftsInPossession = dataItem.nft.size.toString(),
+                        nftChildCount = dataItem.nft.size,
+                        collapsed = collapsed
+                    ) {
+                        collapsedState[i] = !collapsed
                     }
-                    NftTokenDetailItem(
-                        nftId = item.id,
-                        imageUrl = null, // TODO do we have image per ntf?
-                        bottomCornersRounded = bottomCornersRounded,
-                        nftMetadata = item.nftsMetadata
-                    )
+                }
+                if (!collapsed) {
+                    items(
+                        dataItem.nft,
+                        key = { nft -> nft.id }
+                    ) { item ->
+                        var bottomCornersRounded = false
+                        if (dataItem.nft.last() == item) {
+                            bottomCornersRounded = true
+                        }
+                        NftTokenDetailItem(
+                            nftId = item.id,
+                            imageUrl = null, // TODO do we have image per ntf?
+                            bottomCornersRounded = bottomCornersRounded,
+                            nftMetadata = item.nftsMetadata
+                        )
+                    }
                 }
             }
+            item {
+                Spacer(modifier = Modifier.height(100.dp))
+            }
         }
-        item {
-            Spacer(modifier = Modifier.height(100.dp))
-        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun NftTokenListEmpty() {
+    BabylonWalletTheme {
+        NftTokenList(
+            item = emptyList(),
+            collapsedState = remember(mockNftUiList) { mockNftUiList.map { true }.toMutableStateList() }
+        )
     }
 }
 
