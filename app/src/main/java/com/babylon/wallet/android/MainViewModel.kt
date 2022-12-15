dac4c3a7bb24ef33bc41f6e1acf8ad8b7766fdf3
Rafal Wesolowski <rafal.wesolowski@rdx.works>
2022-12-15 18:27:05 +0100

    Proguard rules added for kotlin serialization. Navigation fix added for account creation.

--- app/src/main/java/com/babylon/wallet/android/MainViewModel.kt
+++ app/src/main/java/com/babylon/wallet/android/MainViewModel.kt
@@ -4,9 +4,10 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.babylon.wallet.android.data.DataStoreManager
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import rdx.works.profile.data.repository.ProfileRepository
 import javax.inject.Inject
 
@@ -16,25 +17,25 @@ class MainViewModel @Inject constructor(
     preferencesManager: DataStoreManager
 ) : ViewModel() {
 
-    val state = preferencesManager.showOnboarding.map { showOnboarding ->
-        MainUiState(
-            loading = false,
-            hasProfile = profileRepository.readProfileSnapshot() != null,
-            showOnboarding = showOnboarding
-        )
-    }.stateIn(
-        viewModelScope,
-        SharingStarted.WhileSubscribed(STOP_TIMEOUT_MILLIS),
-        MainUiState()
-    )
+    private val _state: MutableStateFlow<MainUiState> = MutableStateFlow(MainUiState())
+    val state = _state.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            val hasProfile = profileRepository.readProfileSnapshot() != null
+            _state.update { state ->
+                state.copy(
+                    loading = false,
+                    hasProfile = hasProfile,
+                    showOnboarding = preferencesManager.showOnboarding()
+                )
+            }
+        }
+    }
 
     data class MainUiState(
         val loading: Boolean = true,
         val hasProfile: Boolean = false,
         val showOnboarding: Boolean = false
     )
-
-    companion object {
-        private const val STOP_TIMEOUT_MILLIS = 5000L
-    }
 }
