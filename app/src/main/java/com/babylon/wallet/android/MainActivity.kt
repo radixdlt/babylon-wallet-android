cfb3582c453041a4fa868ab4a3ebaeb87fc65044
Jakub Porzuczek <jakub.porzuczek@rdx.works>
2023-01-04 22:27:03 +0100

    add splash screen

--- app/src/main/java/com/babylon/wallet/android/MainActivity.kt
+++ app/src/main/java/com/babylon/wallet/android/MainActivity.kt
@@ -1,9 +1,16 @@
 package com.babylon.wallet.android
 
+import android.R
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.view.View
+import android.view.ViewTreeObserver
+import android.view.animation.AnticipateInterpolator
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.runtime.getValue
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
@@ -23,10 +30,10 @@ class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
+        prepareSplashScreen()
         setContent {
             RadixWalletTheme {
                 val state by viewModel.state.collectAsStateWithLifecycle()
-
                 WalletApp(
                     showOnboarding = state.showOnboarding,
                     hasProfile = state.hasProfile,
@@ -34,5 +41,43 @@ class MainActivity : FragmentActivity() {
                 )
             }
         }
+    }
+
+    private fun prepareSplashScreen() {
+        val splashScreen = installSplashScreen()
+        splashScreen.setOnExitAnimationListener { splashScreenView ->
+            val fadeIn = ObjectAnimator.ofFloat(
+                splashScreenView.view,
+                View.ALPHA,
+                1f,
+                0f
+            )
+            fadeIn.interpolator = AnticipateInterpolator()
+            fadeIn.duration = splashExitAnimDurationMs
+            fadeIn.doOnEnd { splashScreenView.remove() }
+            // Run your animation.
+            fadeIn.start()
+        }
+        setUpSplashScreenDismissCondition()
+    }
+
+    private fun setUpSplashScreenDismissCondition() {
+        val content: View = findViewById(R.id.content)
+        content.viewTreeObserver.addOnPreDrawListener(
+            object : ViewTreeObserver.OnPreDrawListener {
+                override fun onPreDraw(): Boolean {
+                    return if (!viewModel.state.value.loading) {
+                        content.viewTreeObserver.removeOnPreDrawListener(this)
+                        true
+                    } else {
+                        false
+                    }
+                }
+            }
+        )
+    }
+
+    companion object {
+        private const val splashExitAnimDurationMs = 250L
     }
 }
