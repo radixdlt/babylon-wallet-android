938eab9b513e9407c30a2a196e9dd5a9dd23a9dc
giannis.tsepas <john87ts@gmail.com>
2023-01-08 13:35:51 +0200

    removed DAppRepository

--- app/src/main/java/com/babylon/wallet/android/domain/dapp/DAppRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.babylon.wallet.android.domain.dapp
-
-import com.babylon.wallet.android.data.dapp.DAppResult
-import com.babylon.wallet.android.domain.common.Result
-
-interface DAppRepository {
-
-    suspend fun verifyDApp(): Result<DAppResult>
-}
