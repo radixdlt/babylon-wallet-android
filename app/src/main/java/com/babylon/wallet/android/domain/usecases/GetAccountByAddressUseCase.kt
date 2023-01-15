c41f18c91822850419aa93dd840b588cb9041e36
giannis.tsepas <john87ts@gmail.com>
2023-01-15 12:40:08 +0200

    added GetAccountByAddressUseCase

---
+++ app/src/main/java/com/babylon/wallet/android/domain/usecases/GetAccountByAddressUseCase.kt
@@ -0,0 +1,22 @@
+package com.babylon.wallet.android.domain.usecases
+
+import com.babylon.wallet.android.domain.model.AccountSlim
+import rdx.works.profile.data.repository.ProfileRepository
+import javax.inject.Inject
+
+class GetAccountByAddressUseCase @Inject constructor(
+    private val profileRepository: ProfileRepository
+) {
+
+    suspend operator fun invoke(address: String): AccountSlim {
+        val account = profileRepository.getAccount(address)
+        requireNotNull(account) {
+            "account is null"
+        }
+        return AccountSlim(
+            address = account.entityAddress.address,
+            appearanceID = account.appearanceID,
+            displayName = account.displayName
+        )
+    }
+}
