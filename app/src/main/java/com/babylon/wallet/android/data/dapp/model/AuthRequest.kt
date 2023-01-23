5060f694f98e7a82724ff8e88f3d9a1bd8c59dd0
Jakub Porzuczek <jakub.porzuczek@rdx.works>
2023-01-23 14:11:40 +0100

    add auth request/response discriminators

--- app/src/main/java/com/babylon/wallet/android/data/dapp/model/AuthRequest.kt
+++ app/src/main/java/com/babylon/wallet/android/data/dapp/model/AuthRequest.kt
@@ -1,26 +1,24 @@
 package com.babylon.wallet.android.data.dapp.model
 
 import com.babylon.wallet.android.domain.model.MessageFromDataChannel
-import kotlinx.serialization.DeserializationStrategy
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.JsonContentPolymorphicSerializer
-import kotlinx.serialization.json.JsonElement
-import kotlinx.serialization.json.jsonObject
 
 @Serializable
+@SerialName("login")
 data class AuthLoginRequestItem(
     @SerialName("challenge")
     val challenge: String?
 ) : AuthRequestItem()
 
 @Serializable
+@SerialName("usePersona")
 data class AuthUsePersonaRequestItem(
     @SerialName("identityAddress")
     val identityAddress: String
 ) : AuthRequestItem()
 
-@Serializable(with = AuthRequestItemSerializer::class)
+@Serializable
 sealed class AuthRequestItem
 
 fun AuthLoginRequestItem.toDomainModel(): MessageFromDataChannel.AuthRequest.LoginRequest {
@@ -29,14 +27,4 @@ fun AuthLoginRequestItem.toDomainModel(): MessageFromDataChannel.AuthRequest.Log
 
 fun AuthUsePersonaRequestItem.toDomainModel(): MessageFromDataChannel.AuthRequest.UsePersonaRequest {
     return MessageFromDataChannel.AuthRequest.UsePersonaRequest(identityAddress)
-}
-
-class AuthRequestItemSerializer : JsonContentPolymorphicSerializer<AuthRequestItem>(AuthRequestItem::class) {
-    override fun selectDeserializer(element: JsonElement): DeserializationStrategy<out AuthRequestItem> {
-        return if (element.jsonObject["challenge"] != null) {
-            AuthLoginRequestItem.serializer()
-        } else {
-            AuthUsePersonaRequestItem.serializer()
-        }
-    }
 }
