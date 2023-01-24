e5d2b6339d97c6821199b47d3e05e86c560f07b6
Jakub Porzuczek <jakub.porzuczek@rdx.works>
2023-01-24 14:55:32 +0100

    well known file check

--- app/src/main/java/com/babylon/wallet/android/data/gateway/DynamicUrlApi.kt
+++ app/src/main/java/com/babylon/wallet/android/data/gateway/DynamicUrlApi.kt
@@ -1,12 +1,17 @@
 package com.babylon.wallet.android.data.gateway
 
 import com.babylon.wallet.android.data.gateway.generated.model.GatewayInfoResponse
+import com.babylon.wallet.android.data.gateway.model.WellKnownDappResponse
 import retrofit2.Response
+import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Url
 
-interface GatewayInfoApi {
+interface DynamicUrlApi {
 
     @POST
     suspend fun gatewayInfo(@Url gatewayUrl: String): Response<GatewayInfoResponse>
+
+    @GET
+    suspend fun wellKnownDappDefinition(@Url dappWellKnownUrl: String): Response<WellKnownDappResponse>
 }
