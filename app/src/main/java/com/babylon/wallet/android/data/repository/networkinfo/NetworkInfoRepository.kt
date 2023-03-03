56ee48f353e1bb1e8a082f5ee00e23c4afdae8d3
micbakos-rdx <michael.bakogiannis@rdx.works>
2023-03-03 13:32:36 +0200

    Add 5' of cache timeout in gateway info

--- app/src/main/java/com/babylon/wallet/android/data/repository/networkinfo/NetworkInfoRepository.kt
+++ app/src/main/java/com/babylon/wallet/android/data/repository/networkinfo/NetworkInfoRepository.kt
@@ -1,6 +1,9 @@
 package com.babylon.wallet.android.data.repository.networkinfo
 
 import com.babylon.wallet.android.data.gateway.DynamicUrlApi
+import com.babylon.wallet.android.data.repository.cache.CacheParameters
+import com.babylon.wallet.android.data.repository.cache.HttpCache
+import com.babylon.wallet.android.data.repository.cache.TimeoutDuration
 import com.babylon.wallet.android.data.repository.execute
 import com.babylon.wallet.android.domain.common.Result
 import javax.inject.Inject
@@ -10,11 +13,18 @@ interface NetworkInfoRepository {
 }
 
 class NetworkInfoRepositoryImpl @Inject constructor(
-    private val dynamicUrlApi: DynamicUrlApi
+    private val dynamicUrlApi: DynamicUrlApi,
+    private val cache: HttpCache
 ) : NetworkInfoRepository {
 
     override suspend fun getNetworkInfo(networkUrl: String): Result<String> {
         return dynamicUrlApi.gatewayInfo("$networkUrl/gateway/information")
-            .execute(map = { it.ledgerState.network })
+            .execute(
+                cacheParameters = CacheParameters(
+                    httpCache = cache,
+                    timeoutDuration = TimeoutDuration.FIVE_MINUTES
+                ),
+                map = { it.ledgerState.network }
+            )
     }
 }
