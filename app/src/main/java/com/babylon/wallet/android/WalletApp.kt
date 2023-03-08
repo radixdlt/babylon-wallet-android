407ca282b7465476f6f68356c7cf5e02915ce3a0
giannis.tsepas <john87ts@gmail.com>
2023-03-08 12:19:09 +0200

    refactored walletapp with new AppNavigationState

--- app/src/main/java/com/babylon/wallet/android/WalletApp.kt
+++ app/src/main/java/com/babylon/wallet/android/WalletApp.kt
@@ -1,17 +1,10 @@
 package com.babylon.wallet.android
 
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import com.babylon.wallet.android.designsystem.theme.RadixTheme
 import com.babylon.wallet.android.domain.model.MessageFromDataChannel
+import com.babylon.wallet.android.presentation.ROUTE_INCOMPATIBLE_PROFILE
 import com.babylon.wallet.android.presentation.createaccount.ROUTE_CREATE_ACCOUNT
 import com.babylon.wallet.android.presentation.dapp.accountonetime.chooseAccountsOneTime
 import com.babylon.wallet.android.presentation.dapp.login.dAppLogin
@@ -19,7 +12,6 @@ import com.babylon.wallet.android.presentation.dapp.requestsuccess.requestSucces
 import com.babylon.wallet.android.presentation.navigation.NavigationHost
 import com.babylon.wallet.android.presentation.navigation.Screen
 import com.babylon.wallet.android.presentation.transaction.transactionApproval
-import com.babylon.wallet.android.presentation.ui.composables.BasicPromptAlertDialog
 import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import com.google.accompanist.pager.ExperimentalPagerApi
 import kotlinx.coroutines.flow.Flow
@@ -29,43 +21,42 @@ import kotlinx.coroutines.flow.Flow
 @Composable
 @Suppress("ModifierMissing")
 fun WalletApp(
-    initialAppState: InitialAppState,
-    onDeleteProfile: () -> Unit,
-    onDeleteProfileDeclined: () -> Unit,
+    appNavigationState: AppNavigationState,
     oneOffEvent: Flow<MainEvent>,
 ) {
     val navController = rememberAnimatedNavController()
-    when (initialAppState) {
-        InitialAppState.CreateAccount -> {
+    when (appNavigationState) {
+        AppNavigationState.CreateAccount -> {
             NavigationHost(
-                ROUTE_CREATE_ACCOUNT,
-                navController
+                startDestination = ROUTE_CREATE_ACCOUNT,
+                navController = navController
             )
         }
-        InitialAppState.Dashboard -> {
+        AppNavigationState.Wallet -> {
             NavigationHost(
-                Screen.WalletDestination.route,
-                navController
+                startDestination = Screen.WalletDestination.route,
+                navController = navController
             )
         }
-        is InitialAppState.IncompatibleProfile -> {
-            IncompatibleProfileContent(
-                onDeleteProfile = onDeleteProfile,
-                onDeleteProfileDeclined = onDeleteProfileDeclined,
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(RadixTheme.colors.blue1)
-                    .padding(horizontal = RadixTheme.dimensions.paddingDefault)
+        is AppNavigationState.IncompatibleProfile -> {
+            NavigationHost(
+                startDestination = ROUTE_INCOMPATIBLE_PROFILE,
+                navController = navController
             )
         }
-        InitialAppState.Onboarding
-        -> {
+        AppNavigationState.Onboarding -> {
             NavigationHost(
-                Screen.OnboardingDestination.route,
-                navController
+                startDestination = Screen.OnboardingDestination.route,
+                navController = navController
             )
+        }
+        AppNavigationState.Init -> {
+            // TODO this doesn't seem to escalate well and as documentation suggests
+            //  we should better have fixed start destination and, in our case the Wallet,
+            //  and let the Wallet screen to decide where and when to navigate.
         }
     }
+
     LaunchedEffect(Unit) {
         oneOffEvent.collect { event ->
             when (event) {
@@ -89,42 +80,5 @@ fun WalletApp(
                 }
             }
         }
-    }
-}
-
-@Composable
-private fun IncompatibleProfileContent(
-    onDeleteProfile: () -> Unit,
-    onDeleteProfileDeclined: () -> Unit,
-    modifier: Modifier = Modifier
-) {
-    Box(
-        modifier = modifier
-    ) {
-        BasicPromptAlertDialog(
-            finish = {
-                if (it) {
-                    onDeleteProfile()
-                } else {
-                    onDeleteProfileDeclined()
-                }
-            },
-            title = {
-                Text(
-                    text = stringResource(id = R.string.wallet_data_incompatible),
-                    style = RadixTheme.typography.body2Header,
-                    color = RadixTheme.colors.gray1
-                )
-            },
-            text = {
-                Text(
-                    text = stringResource(id = R.string.for_this_preview_wallet_version),
-                    style = RadixTheme.typography.body2Regular,
-                    color = RadixTheme.colors.gray1
-                )
-            },
-            confirmText = stringResource(id = R.string.delete_wallet_data),
-            confirmTextColor = RadixTheme.colors.red1
-        )
     }
 }
