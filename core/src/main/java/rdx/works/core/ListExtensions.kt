774ffb1f1e5d3d6a1ab4db236179e63d722cae91
micbakos-rdx <michael.bakogiannis@rdx.works>
2023-03-17 12:51:39 +0200

    Make mapWhen be an inline function

--- core/src/main/java/rdx/works/core/ListExtensions.kt
+++ core/src/main/java/rdx/works/core/ListExtensions.kt
@@ -1,6 +1,16 @@
 package rdx.works.core
 
-fun <T> List<T>.mapWhen(predicate: (T) -> Boolean, mutation: (T) -> T): List<T> = map { value ->
+/**
+ * Used when in need to map a certain value of the list to the new value
+ * provided by the [mutation] lambda.
+ *
+ * Be aware that since this method only changes one element of the list, the type of the mutated
+ * item cannot change, as the original [map] function does, which can alter the type of the list.
+ */
+inline fun <T> Iterable<T>.mapWhen(
+    predicate: (T) -> Boolean,
+    mutation: (T) -> T
+): List<T> = map { value ->
     if (predicate(value)) {
         mutation(value)
     } else {
