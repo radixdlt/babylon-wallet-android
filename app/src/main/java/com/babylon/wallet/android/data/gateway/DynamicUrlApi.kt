30cf2c8c7195c677f434ae10759de3bb392c8a70
micbakos-rdx <michael.bakogiannis@rdx.works>
2023-03-23 15:24:46 +0200

    Change gateway info to gateway status

--- app/src/main/java/com/babylon/wallet/android/data/gateway/DynamicUrlApi.kt
+++ app/src/main/java/com/babylon/wallet/android/data/gateway/DynamicUrlApi.kt
@@ -1,6 +1,6 @@
 package com.babylon.wallet.android.data.gateway
 
-import com.babylon.wallet.android.data.gateway.generated.models.GatewayInformationResponse
+import com.babylon.wallet.android.data.gateway.generated.models.GatewayStatusResponse
 import com.babylon.wallet.android.data.gateway.model.DappMetadataResponse
 import retrofit2.Call
 import retrofit2.http.GET
@@ -10,8 +10,14 @@ import retrofit2.http.Url
 interface DynamicUrlApi {
 
     @POST
-    fun gatewayInfo(@Url gatewayUrl: String): Call<GatewayInformationResponse>
+    fun gatewayStatus(@Url gatewayUrl: String): Call<GatewayStatusResponse>
 
     @GET
     fun wellKnownDappDefinition(@Url dappWellKnownUrl: String): Call<DappMetadataResponse>
+
+    companion object {
+        private const val GATEWAY_STATUS_API = "status/gateway-status"
+
+        fun gatewayStatusUrl(fromBaseUrl: String) = "$fromBaseUrl/$GATEWAY_STATUS_API"
+    }
 }
