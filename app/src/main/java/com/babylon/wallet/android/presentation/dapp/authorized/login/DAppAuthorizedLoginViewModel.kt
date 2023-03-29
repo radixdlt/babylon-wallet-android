39847b34b0869025293500320c01960e50a292be
jakub-rdx <118203440+jakub-rdx@users.noreply.github.com>
2023-03-29 21:00:33 +0200

    ABW-1210 show login request only for auth of type login (#190)
    * show login request only for auth of type login
    
    * ABW-1209 simplified handling of initial screen setup for usePersona request

--- app/src/main/java/com/babylon/wallet/android/presentation/dapp/authorized/login/DAppAuthorizedLoginViewModel.kt
+++ app/src/main/java/com/babylon/wallet/android/presentation/dapp/authorized/login/DAppAuthorizedLoginViewModel.kt
@@ -110,19 +110,14 @@ class DAppAuthorizedLoginViewModel @Inject constructor(
     private suspend fun setInitialDappLoginRoute() {
         val isLoginRequest = request.authRequest is AuthorizedRequest.AuthRequest.LoginRequest
         val usePersonaRequest = request.isUsePersonaAuth()
-        val resetPersonaData = request.resetRequestItem?.personaData == true
-        val resetAccounts = request.resetRequestItem?.accounts == true
-        val showSelectPersona = isLoginRequest || resetPersonaData
-
-        if (showSelectPersona) {
+        if (isLoginRequest) {
             _state.update { it.copy(initialAuthorizedLoginRoute = InitialAuthorizedLoginRoute.SelectPersona(args.requestId)) }
         } else if (usePersonaRequest) {
             val dapp = authorizedDapp
             if (dapp != null) {
                 setInitialDappLoginRouteForUsePersonaRequest(
                     dapp,
-                    request.authRequest as AuthorizedRequest.AuthRequest.UsePersonaRequest,
-                    resetAccounts
+                    request.authRequest as AuthorizedRequest.AuthRequest.UsePersonaRequest
                 )
             } else {
                 onAbortDappLogin(WalletErrorType.InvalidPersona)
@@ -135,65 +130,64 @@ class DAppAuthorizedLoginViewModel @Inject constructor(
     @Suppress("LongMethod")
     private suspend fun setInitialDappLoginRouteForUsePersonaRequest(
         dapp: Network.AuthorizedDapp,
-        authRequest: AuthorizedRequest.AuthRequest.UsePersonaRequest,
-        resetAccounts: Boolean
+        authRequest: AuthorizedRequest.AuthRequest.UsePersonaRequest
     ) {
         val hasAuthorizedPersona = dapp.hasAuthorizedPersona(
             authRequest.personaAddress
         )
         if (hasAuthorizedPersona) {
+            val resetAccounts = request.resetRequestItem?.accounts == true
+            val resetPersonaData = request.resetRequestItem?.personaData == true
             val persona = checkNotNull(personaRepository.getPersonaByAddress(authRequest.personaAddress))
             onSelectPersona(persona)
             val ongoingAccountsRequestItem = request.ongoingAccountsRequestItem
             val oneTimeAccountsRequestItem = request.oneTimeAccountsRequestItem
             val ongoingPersonaDataRequestItem = request.ongoingPersonaDataRequestItem
             val oneTimePersonaDataRequestItem = request.oneTimePersonaDataRequestItem
-            if (ongoingAccountsRequestItem != null && (
-                !requestedAccountsPermissionAlreadyGranted(
-                        authRequest.personaAddress,
-                        ongoingAccountsRequestItem
-                    ) || resetAccounts
-                )
-            ) {
-                _state.update {
-                    it.copy(
-                        initialAuthorizedLoginRoute = InitialAuthorizedLoginRoute.Permission(
-                            ongoingAccountsRequestItem.numberOfAccounts,
-                            isExactAccountsCount = ongoingAccountsRequestItem.quantifier.exactly()
+            val requestedAccountsAlreadyGranted = requestedAccountsPermissionAlreadyGranted(
+                authRequest.personaAddress,
+                ongoingAccountsRequestItem
+            )
+            val requestedDataAlreadyGranted = personaDataAccessAlreadyGranted(ongoingPersonaDataRequestItem, persona.address)
+            when {
+                ongoingAccountsRequestItem != null && (!requestedAccountsAlreadyGranted || resetAccounts) -> {
+                    _state.update {
+                        it.copy(
+                            initialAuthorizedLoginRoute = InitialAuthorizedLoginRoute.Permission(
+                                ongoingAccountsRequestItem.numberOfAccounts,
+                                isExactAccountsCount = ongoingAccountsRequestItem.quantifier.exactly()
+                            )
                         )
-                    )
+                    }
                 }
-            } else {
-                when {
-                    ongoingPersonaDataRequestItem != null -> {
-                        _state.update { state ->
-                            state.copy(
-                                initialAuthorizedLoginRoute = InitialAuthorizedLoginRoute.OngoingPersonaData(
-                                    authRequest.personaAddress,
-                                    ongoingPersonaDataRequestItem.fields.map { it.toKind() }.encodeToString()
-                                )
+                ongoingPersonaDataRequestItem != null && (!requestedDataAlreadyGranted || resetPersonaData) -> {
+                    _state.update { state ->
+                        state.copy(
+                            initialAuthorizedLoginRoute = InitialAuthorizedLoginRoute.OngoingPersonaData(
+                                authRequest.personaAddress,
+                                ongoingPersonaDataRequestItem.fields.map { it.toKind() }.encodeToString()
                             )
-                        }
+                        )
                     }
-                    oneTimeAccountsRequestItem != null -> {
-                        _state.update {
-                            it.copy(
-                                initialAuthorizedLoginRoute = InitialAuthorizedLoginRoute.ChooseAccount(
-                                    oneTimeAccountsRequestItem.numberOfAccounts,
-                                    isExactAccountsCount = oneTimeAccountsRequestItem.quantifier.exactly(),
-                                    oneTime = true
-                                )
+                }
+                oneTimeAccountsRequestItem != null -> {
+                    _state.update {
+                        it.copy(
+                            initialAuthorizedLoginRoute = InitialAuthorizedLoginRoute.ChooseAccount(
+                                oneTimeAccountsRequestItem.numberOfAccounts,
+                                isExactAccountsCount = oneTimeAccountsRequestItem.quantifier.exactly(),
+                                oneTime = true
                             )
-                        }
+                        )
                     }
-                    oneTimePersonaDataRequestItem != null -> {
-                        _state.update { state ->
-                            state.copy(
-                                initialAuthorizedLoginRoute = InitialAuthorizedLoginRoute.OneTimePersonaData(
-                                    oneTimePersonaDataRequestItem.fields.map { it.toKind() }.encodeToString()
-                                )
+                }
+                oneTimePersonaDataRequestItem != null -> {
+                    _state.update { state ->
+                        state.copy(
+                            initialAuthorizedLoginRoute = InitialAuthorizedLoginRoute.OneTimePersonaData(
+                                oneTimePersonaDataRequestItem.fields.map { it.toKind() }.encodeToString()
                             )
-                        }
+                        )
                     }
                 }
             }
@@ -281,8 +275,9 @@ class DAppAuthorizedLoginViewModel @Inject constructor(
 
     private suspend fun requestedAccountsPermissionAlreadyGranted(
         personaAddress: String,
-        accountsRequestItem: AccountsRequestItem
+        accountsRequestItem: AccountsRequestItem?
     ): Boolean {
+        if (accountsRequestItem == null) return false
         return authorizedDapp?.let { dapp ->
             val potentialOngoingAddresses = dAppConnectionRepository.dAppAuthorizedPersonaAccountAddresses(
                 dapp.dAppDefinitionAddress,
@@ -339,9 +334,10 @@ class DAppAuthorizedLoginViewModel @Inject constructor(
     }
 
     private suspend fun personaDataAccessAlreadyGranted(
-        requestItem: MessageFromDataChannel.IncomingRequest.PersonaRequestItem,
+        requestItem: MessageFromDataChannel.IncomingRequest.PersonaRequestItem?,
         personaAddress: String
     ): Boolean {
+        if (requestItem == null) return false
         val dapp = requireNotNull(editedDapp)
         val requestedFieldsCount = requestItem.fields.size
         val requestedFieldKinds = requestItem.fields.map { it.toKind() }
