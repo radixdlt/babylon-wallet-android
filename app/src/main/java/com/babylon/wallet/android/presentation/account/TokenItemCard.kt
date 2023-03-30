54016334f653abe017608384f91d89e69aac7424
Rafal Wesolowski <rafal.wesolowski@rdx.works>
2023-03-30 10:39:16 +0200

    ABW-1019 - Engine toolkit bumped. Some detekt issues fixed.

--- app/src/main/java/com/babylon/wallet/android/presentation/account/TokenItemCard.kt
+++ app/src/main/java/com/babylon/wallet/android/presentation/account/TokenItemCard.kt
@@ -4,7 +4,6 @@ import android.graphics.drawable.ColorDrawable
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
