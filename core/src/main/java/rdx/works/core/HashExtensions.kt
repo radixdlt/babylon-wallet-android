55a0c9baaab3791c48bed48f14baeb44ca0fa7bd
giannis.tsepas <john87ts@gmail.com>
2023-04-12 14:21:46 +0300

    moved hash extensions from peerdroid to core module

--- core/src/main/java/rdx/works/core/HashExtensions.kt
+++ core/src/main/java/rdx/works/core/HashExtensions.kt
@@ -1,12 +1,7 @@
 package rdx.works.core
 
-import com.radixdlt.crypto.hash.sha256.extensions.sha256
-import com.radixdlt.hex.extensions.toHexString
-
-fun ByteArray.sha256Hash(): ByteArray = this.sha256()
-
 fun ByteArray.blake2Hash(): ByteArray = com.radixdlt.toolkit.hash(data = this)
 
 fun String.blake2Hash(): ByteArray = toByteArray().blake2Hash()
 
-fun ByteArray.toHexString(): String = this.toHexString()
+fun ByteArray.toHexString(): String = joinToString(separator = "") { eachByte -> "%02x".format(eachByte) }
