51016d77fc3c1f674611210a1f61b99babbc8f44
micbakos-rdx <michael.bakogiannis@rdx.works>
2023-04-20 12:42:56 +0300

    Change account and identity derivation paths based on the latest changes to CAP-26

--- profile/src/main/java/rdx/works/profile/derivation/AccountHDDerivationPath.kt
+++ profile/src/main/java/rdx/works/profile/derivation/AccountHDDerivationPath.kt
@@ -8,14 +8,14 @@ import rdx.works.profile.derivation.model.NetworkId
 
 /**
  * The **default** derivation path used to derive `Account` keys for signing off transactions or for
- * signing authentication,at a certain account index (`ENTITY_INDEX`) and **unique per network** (`NETWORK_ID`)
- * as per [CAP-26][cap26].
+ * signing authentication, at a certain account index (`ENTITY_INDEX`) and **unique per network** (`NETWORK_ID`)
+ * as per CAP-26.
  *
  * Note that users can choose to use custom derivation path instead of this default
  * one when deriving keys for accounts.
  *
  * The format is:
- *          `m/44'/1022'/<NETWORK_ID>'/525'/<ENTITY_INDEX>'/<KEY_TYPE>'`
+ *          `m/44'/1022'/<NETWORK_ID>'/525'/<KEY_TYPE>'/<ENTITY_INDEX>'`
  *
  */
 data class AccountHDDerivationPath(
@@ -27,6 +27,5 @@ data class AccountHDDerivationPath(
     private val entityType: EntityType = EntityType.Account
 
     val path: String
-        get() = "$BIP44_PREFIX/44H/${coinType.value}H/${networkId.value}H/${entityType.value}H/${accountIndex}H/" +
-            "${keyType.value}H"
+        get() = "$BIP44_PREFIX/44H/${coinType.value}H/${networkId.value}H/${entityType.value}H/${keyType.value}H/${accountIndex}H"
 }
--- profile/src/main/java/rdx/works/profile/derivation/IdentityHDDerivationPath.kt
+++ profile/src/main/java/rdx/works/profile/derivation/IdentityHDDerivationPath.kt
@@ -8,13 +8,13 @@ import rdx.works.profile.derivation.model.NetworkId
 
 /**
  * The **default** derivation path used to derive `Identity` (Persona) keys for signing authentication,
- * at a certain (Persona) index (`ENTITY_INDEX`) and **unique per network** (`NETWORK_ID`) as per [CAP-26][cap26].
+ * at a certain (Persona) index (`ENTITY_INDEX`) and **unique per network** (`NETWORK_ID`) as per [CAP-26].
  *
  * Note that users can choose to use custom derivation path instead of this default one
  * when deriving keys for identities (personas).
  *
  * The format is:
- *          `m/44'/1022'/<NETWORK_ID>'/618'/<ENTITY_INDEX>'/<KEY_TYPE>'`
+ *          `m/44'/1022'/<NETWORK_ID>'/618'/<KEY_TYPE>'/<ENTITY_INDEX>'`
  *
  */
 data class IdentityHDDerivationPath(
@@ -26,6 +26,5 @@ data class IdentityHDDerivationPath(
     private val entityType: EntityType = EntityType.Identity
 
     val path: String
-        get() = "$BIP44_PREFIX/44H/${coinType.value}H/${networkId.value}H/${entityType.value}H/${identityIndex}H/" +
-            "${keyType.value}H"
+        get() = "$BIP44_PREFIX/44H/${coinType.value}H/${networkId.value}H/${entityType.value}H/${keyType.value}H/${identityIndex}H"
 }
