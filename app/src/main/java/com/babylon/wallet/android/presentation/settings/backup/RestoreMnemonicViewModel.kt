50d099091511100e7bddb84578108f3b1b34a910
micbakos-rdx <michael.bakogiannis@rdx.works>
2023-05-08 14:34:06 +0300

    Add tests for restore mnemonic view model

--- app/src/main/java/com/babylon/wallet/android/presentation/settings/backup/RestoreMnemonicViewModel.kt
+++ app/src/main/java/com/babylon/wallet/android/presentation/settings/backup/RestoreMnemonicViewModel.kt
@@ -144,6 +144,8 @@ class RestoreMnemonicViewModel @Inject constructor(
 
 enum class SeedPhraseLength(val words: Int) {
     TWELVE(12),
+    FIFTEEN(15),
     EIGHTEEN(18),
+    TWENTY_ONE(21),
     TWENTY_FOUR(24)
 }
---
+++ app/src/test/java/com/babylon/wallet/android/presentation/settings/backup/RestoreMnemonicViewModelTest.kt
@@ -0,0 +1,138 @@
+package com.babylon.wallet.android.presentation.settings.backup
+
+import androidx.lifecycle.SavedStateHandle
+import app.cash.turbine.test
+import com.babylon.wallet.android.presentation.StateViewModelTest
+import com.babylon.wallet.android.utils.AppEventBus
+import io.mockk.every
+import io.mockk.mockk
+import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertFalse
+import junit.framework.TestCase.assertTrue
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+import rdx.works.profile.data.model.MnemonicWithPassphrase
+import rdx.works.profile.data.model.apppreferences.Radix
+import rdx.works.profile.data.model.factorsources.FactorSource
+import rdx.works.profile.data.model.pernetwork.Network
+import rdx.works.profile.domain.GetProfileUseCase
+import rdx.works.profile.domain.backup.RestoreMnemonicUseCase
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class RestoreMnemonicViewModelTest : StateViewModelTest<RestoreMnemonicViewModel>() {
+
+    private val savedStateHandle = mockk<SavedStateHandle>()
+    private val getProfileUseCase = mockk<GetProfileUseCase>()
+    private val restoreMnemonicUseCase = mockk<RestoreMnemonicUseCase>()
+    private val appEventBus = mockk<AppEventBus>()
+
+    private val mnemonicWithPassphrase = MnemonicWithPassphrase(
+        mnemonic = "zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo vote",
+        bip39Passphrase = ""
+    )
+
+    private val sampleProfile = sampleDataProvider.sampleProfile(
+        mnemonicWithPassphrase = mnemonicWithPassphrase
+    ).copy(
+        networks = listOf(
+            Network(
+                networkID = Radix.Gateway.default.network.id,
+                accounts = listOf(sampleDataProvider.sampleAccount(
+                    address = ACCOUNT_ADDRESS,
+                    factorSourceId = FactorSource.factorSourceId(mnemonicWithPassphrase = mnemonicWithPassphrase)
+                )),
+                personas = listOf(),
+                authorizedDapps = listOf()
+            )
+        ),
+        factorSources = listOf(
+            FactorSource.babylon(mnemonicWithPassphrase = mnemonicWithPassphrase)
+        )
+    )
+
+    override fun initVM(): RestoreMnemonicViewModel = RestoreMnemonicViewModel(
+        savedStateHandle = savedStateHandle,
+        getProfileUseCase = getProfileUseCase,
+        restoreMnemonicUseCase = restoreMnemonicUseCase,
+        appEventBus = appEventBus
+    )
+
+    @Before
+    override fun setUp() {
+        super.setUp()
+        every { savedStateHandle.get<String>(ARG_ACCOUNT_ADDRESS) } returns ACCOUNT_ADDRESS
+        every { getProfileUseCase() } returns flowOf(sampleProfile)
+    }
+
+    @Test
+    fun `when is initialised, the hint and the account exist in state`() = runTest {
+        val vm = vm.value
+        advanceUntilIdle()
+        vm.state.test {
+            val item = expectMostRecentItem()
+
+            assertEquals(sampleProfile.networks.first().accounts.first(), item.accountOnNetwork)
+            assertEquals(sampleProfile.factorSources.first().hint, item.factorSourceHint)
+        }
+    }
+
+    @Test
+    fun `when user types mnemonic, the words array is filled correctly`() = runTest {
+        val vm = vm.value
+        vm.onMnemonicWordsTyped("zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo vote    ")
+        advanceUntilIdle()
+        vm.state.test {
+            val item = expectMostRecentItem()
+
+            assertEquals("zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo vote ", item.wordsPhrase)
+            assertTrue(item.isSubmitButtonEnabled)
+        }
+    }
+
+    @Test
+    fun `when words are typed, the button is enabled only on valid word counts`() = runTest {
+
+
+        val vm = vm.value
+        advanceUntilIdle()
+        vm.state.test {
+            vm.onMnemonicWordsTyped("zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo vote")
+            assertTrue(expectMostRecentItem().isSubmitButtonEnabled)
+            vm.onMnemonicWordsTyped("zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo")
+            assertFalse(expectMostRecentItem().isSubmitButtonEnabled)
+
+            vm.onChangeSeedPhraseLength(SeedPhraseLength.TWELVE)
+            vm.onMnemonicWordsTyped("zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo")
+            assertTrue(expectMostRecentItem().isSubmitButtonEnabled)
+            vm.onMnemonicWordsTyped("zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo")
+            assertFalse(expectMostRecentItem().isSubmitButtonEnabled)
+
+            vm.onChangeSeedPhraseLength(SeedPhraseLength.FIFTEEN)
+            vm.onMnemonicWordsTyped("zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo")
+            assertTrue(expectMostRecentItem().isSubmitButtonEnabled)
+            vm.onMnemonicWordsTyped("zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo")
+            assertFalse(expectMostRecentItem().isSubmitButtonEnabled)
+
+            vm.onChangeSeedPhraseLength(SeedPhraseLength.EIGHTEEN)
+            vm.onMnemonicWordsTyped("zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo")
+            assertTrue(expectMostRecentItem().isSubmitButtonEnabled)
+            vm.onMnemonicWordsTyped("zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo")
+            assertFalse(expectMostRecentItem().isSubmitButtonEnabled)
+
+            vm.onChangeSeedPhraseLength(SeedPhraseLength.TWENTY_ONE)
+            vm.onMnemonicWordsTyped("zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo")
+            assertTrue(expectMostRecentItem().isSubmitButtonEnabled)
+            vm.onMnemonicWordsTyped("zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo")
+            assertFalse(expectMostRecentItem().isSubmitButtonEnabled)
+        }
+    }
+
+
+    private companion object {
+        private const val ACCOUNT_ADDRESS = "account_rdx_abcd"
+    }
+}
