d8071a911765bc367766997e18e215b56441838d
giannis.tsepas <giannis.tsepas@rdx.works>
2023-05-09 20:01:52 +0300

    updated accounts response with discriminator, renamed file and moved AccountDto to this file

---
+++ app/src/main/java/com/babylon/wallet/android/data/dapp/model/AccountsResponseItem.kt
@@ -0,0 +1,69 @@
+@file:OptIn(ExperimentalSerializationApi::class)
+
+package com.babylon.wallet.android.data.dapp.model
+
+import com.babylon.wallet.android.presentation.dapp.authorized.account.AccountItemUiModel
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonClassDiscriminator
+
+@Serializable
+data class AccountWithProofOfOwnership(
+    @SerialName("account") val accountDto: AccountDto,
+    @SerialName("proof") val proof: ProofDto
+) {
+
+    @Serializable
+    data class AccountDto(
+        @SerialName("address") val address: String,
+        @SerialName("label") val label: String,
+        @SerialName("appearanceId") val appearanceId: Int,
+    )
+}
+
+@Serializable
+@JsonClassDiscriminator("discriminator")
+sealed class OneTimeAccountsRequestResponseItem
+
+@Serializable
+@SerialName("oneTimeAccountsWithProofOfOwnership")
+data class OneTimeAccountsWithProofOfOwnershipRequestResponseItem(
+    @SerialName("challenge")
+    val challenge: String,
+    @SerialName("accounts")
+    val accounts: List<AccountWithProofOfOwnership>
+) : OneTimeAccountsRequestResponseItem()
+
+@Serializable
+@SerialName("oneTimeAccountsWithoutProofOfOwnership")
+data class OneTimeAccountsWithoutProofOfOwnershipRequestResponseItem(
+    @SerialName("accounts")
+    val accounts: List<AccountWithProofOfOwnership.AccountDto>
+) : OneTimeAccountsRequestResponseItem()
+
+@Serializable
+@JsonClassDiscriminator("discriminator")
+sealed class OngoingAccountsRequestResponseItem
+
+@Serializable
+@SerialName("ongoingAccountsWithProofOfOwnership")
+data class OngoingAccountsWithProofOfOwnershipRequestResponseItem(
+    @SerialName("accounts")
+    val accounts: List<AccountWithProofOfOwnership>
+) : OngoingAccountsRequestResponseItem()
+
+@Serializable
+@SerialName("ongoingAccountsWithoutProofOfOwnership")
+data class OngoingAccountsWithoutProofOfOwnershipRequestResponseItem(
+    @SerialName("accounts")
+    val accounts: List<AccountWithProofOfOwnership.AccountDto>
+) : OngoingAccountsRequestResponseItem()
+
+fun List<AccountItemUiModel>.toDataModel() = map { accountResources ->
+    AccountWithProofOfOwnership.AccountDto(
+        address = accountResources.address,
+        label = accountResources.displayName.orEmpty(),
+        appearanceId = accountResources.appearanceID
+    )
+}
