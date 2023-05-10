aed65d7fefea553159c3d0b48f81a60aef03f14c
giannis.tsepas <giannis.tsepas@rdx.works>
2023-05-10 14:26:42 +0300

    added new domain model AccountWithResources

---
+++ app/src/main/java/com/babylon/wallet/android/domain/model/AccountWithResources.kt
@@ -0,0 +1,74 @@
+package com.babylon.wallet.android.domain.model
+
+import android.net.Uri
+import com.babylon.wallet.android.domain.model.metadata.DescriptionMetadataItem
+import com.babylon.wallet.android.domain.model.metadata.IconUrlMetadataItem
+import com.babylon.wallet.android.domain.model.metadata.NameMetadataItem
+import com.babylon.wallet.android.domain.model.metadata.SymbolMetadataItem
+import rdx.works.profile.data.model.pernetwork.Network
+import java.math.BigDecimal
+
+data class AccountWithResources(
+    val account: Network.Account,
+    val fungibleResources: List<FungibleResource>,
+    val nonFungibleResources: List<NonFungibleResource>,
+    private val factorSourceState: FactorSourceState = FactorSourceState.Valid
+) {
+
+    data class FungibleResource(
+        val resourceAddress: String,
+        val amount: BigDecimal,
+        private val nameMetadataItem: NameMetadataItem? = null,
+        private val symbolMetadataItem: SymbolMetadataItem? = null,
+        private val descriptionMetadataItem: DescriptionMetadataItem? = null,
+        private val iconUrlMetadataItem: IconUrlMetadataItem? = null,
+    ) {
+        val name: String?
+            get() = nameMetadataItem?.name // .orEmpty()
+
+        val symbol: String
+            get() = symbolMetadataItem?.symbol.orEmpty()
+
+        val description: String
+            get() = descriptionMetadataItem?.description.orEmpty()
+
+        val iconUrl: Uri?
+            get() = iconUrlMetadataItem?.url
+    }
+
+    data class NonFungibleResource(
+        val resourceAddress: String,
+        val amount: Long,
+        private val nameMetadataItem: NameMetadataItem? = null,
+        private val descriptionMetadataItem: DescriptionMetadataItem? = null,
+        val nftIds: List<String>, // TODO when gateway is ready
+    ) {
+        val name: String
+            get() = nameMetadataItem?.name.orEmpty()
+
+        val description: String
+            get() = descriptionMetadataItem?.description.orEmpty()
+    }
+
+    fun hasXrdWithEnoughBalance(minimumBalance: Long): Boolean {
+        return fungibleResources.any {
+            it.symbol == MetadataConstants.SYMBOL_XRD && it.amount >= BigDecimal(minimumBalance)
+        }
+    }
+
+    fun needMnemonicBackup(): Boolean {
+        return hasXrdWithEnoughBalance(1L) && factorSourceState == FactorSourceState.NeedMnemonicBackup
+    }
+
+    fun needMnemonicRecovery(): Boolean {
+        return factorSourceState == FactorSourceState.NeedMnemonicRecovery
+    }
+
+    enum class FactorSourceState {
+        NeedMnemonicRecovery, NeedMnemonicBackup, Valid
+    }
+}
+
+fun List<AccountWithResources>.findAccountWithEnoughXRDBalance(minimumBalance: Long) = find {
+    it.hasXrdWithEnoughBalance(minimumBalance)
+}
