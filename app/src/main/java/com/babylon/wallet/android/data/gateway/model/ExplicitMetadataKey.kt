166957b4a2254444433e274378e7cae321a15e07
giannis.tsepas <giannis.tsepas@rdx.works>
2023-05-10 14:37:23 +0300

    added bew metadata keys in ExplicitMetadataKey class

--- app/src/main/java/com/babylon/wallet/android/data/gateway/model/ExplicitMetadataKey.kt
+++ app/src/main/java/com/babylon/wallet/android/data/gateway/model/ExplicitMetadataKey.kt
@@ -7,10 +7,12 @@ import com.babylon.wallet.android.domain.model.metadata.DAppDefinitionMetadataIt
 import com.babylon.wallet.android.domain.model.metadata.DescriptionMetadataItem
 import com.babylon.wallet.android.domain.model.metadata.DomainMetadataItem
 import com.babylon.wallet.android.domain.model.metadata.IconUrlMetadataItem
+import com.babylon.wallet.android.domain.model.metadata.InfoUrlMetadataItem
 import com.babylon.wallet.android.domain.model.metadata.NameMetadataItem
 import com.babylon.wallet.android.domain.model.metadata.RelatedWebsiteMetadataItem
 import com.babylon.wallet.android.domain.model.metadata.StandardMetadataItem
 import com.babylon.wallet.android.domain.model.metadata.SymbolMetadataItem
+import com.babylon.wallet.android.domain.model.metadata.TagsMetadataItem
 
 /**
  * Common metadata keys used in the wallet app and defined
@@ -24,7 +26,11 @@ enum class ExplicitMetadataKey(val key: String) {
     DAPP_DEFINITION("dapp_definition"),
     RELATED_WEBSITES("related_websites"),
     ACCOUNT_TYPE("account_type"),
-    ICON_URL("icon_url");
+    TAGS("tags"),
+    KEY_IMAGE_URL("key_image_url"),
+    ICON_URL("icon_url"),
+    INFO_URL("info_url"),
+    URL("url"); // TODO it should be info_url ?
 
     fun toStandardMetadataItem(value: String): StandardMetadataItem = when (this) {
         DESCRIPTION -> DescriptionMetadataItem(description = value)
@@ -34,10 +40,29 @@ enum class ExplicitMetadataKey(val key: String) {
         DAPP_DEFINITION -> DAppDefinitionMetadataItem(address = value)
         RELATED_WEBSITES -> RelatedWebsiteMetadataItem(website = value)
         ACCOUNT_TYPE -> AccountTypeMetadataItem(type = AccountType.valueOf(value))
+        TAGS -> TagsMetadataItem(tags = listOf(value))
+        KEY_IMAGE_URL -> IconUrlMetadataItem(url = Uri.parse(value))
+        INFO_URL -> InfoUrlMetadataItem(url = Uri.parse(value))
         ICON_URL -> IconUrlMetadataItem(url = Uri.parse(value))
+        URL -> InfoUrlMetadataItem(url = Uri.parse(value))
     }
 
     companion object {
+
+        val forAssets: Set<ExplicitMetadataKey>
+            get() = setOf(
+                NAME,
+                SYMBOL,
+                DESCRIPTION,
+                KEY_IMAGE_URL,
+                ACCOUNT_TYPE,
+                RELATED_WEBSITES,
+                ICON_URL,
+                INFO_URL,
+                URL,
+                TAGS
+            )
+
         val forDapp: Set<ExplicitMetadataKey>
             get() = setOf(
                 NAME,
