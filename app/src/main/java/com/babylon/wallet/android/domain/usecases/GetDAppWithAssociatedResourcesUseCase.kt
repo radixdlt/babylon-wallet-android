516ac96d716c7cb2b3d5b8cd7e7ca38f0123ad35
Rafal Wesolowski <rafal.wesolowski@rdx.works>
2023-06-02 12:46:56 +0200

    ABW-1341 - Well known file check added for claimed websites.

--- app/src/main/java/com/babylon/wallet/android/domain/usecases/GetDAppWithAssociatedResourcesUseCase.kt
+++ app/src/main/java/com/babylon/wallet/android/domain/usecases/GetDAppWithAssociatedResourcesUseCase.kt
@@ -5,6 +5,7 @@ import com.babylon.wallet.android.data.repository.entity.EntityRepository
 import com.babylon.wallet.android.domain.common.Result
 import com.babylon.wallet.android.domain.common.map
 import com.babylon.wallet.android.domain.common.switchMap
+import com.babylon.wallet.android.domain.common.value
 import com.babylon.wallet.android.domain.model.DAppWithAssociatedResources
 import javax.inject.Inject
 
@@ -19,10 +20,22 @@ class GetDAppWithAssociatedResourcesUseCase @Inject constructor(
         definitionAddress = definitionAddress,
         needMostRecentData = false
     ).switchMap { dAppMetadata ->
+        // If well known file verification fails, we dont want claimed websites
+        val isWebsiteAuthentic = dAppMetadata.claimedWebsite?.let { website ->
+            dAppMetadataRepository.verifyDapp(
+                origin = website,
+                dAppDefinitionAddress = dAppMetadata.dAppAddress
+            ).value() == true
+        } ?: false
+
+        val updatedDAppMetadata = dAppMetadata.copy(
+            claimedWebsiteItem = if (isWebsiteAuthentic) dAppMetadata.claimedWebsiteItem else null
+        )
+
         entityRepository.getDAppResources(dAppMetadata = dAppMetadata, needMostRecentData)
             .map { resources ->
                 DAppWithAssociatedResources(
-                    dAppWithMetadata = dAppMetadata,
+                    dAppWithMetadata = updatedDAppMetadata,
                     resources = resources
                 )
             }
