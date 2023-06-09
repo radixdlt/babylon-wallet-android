1ad25fc5c390b5bb01b1f073f6583cfa1e034e7c
Jakub Porzuczek <jakub.porzuczek@rdx.works>
2023-06-09 10:47:40 +0200

    remove unused methods

--- app/src/main/java/com/babylon/wallet/android/presentation/dapp/InitialAuthorizedLoginRoute.kt
+++ app/src/main/java/com/babylon/wallet/android/presentation/dapp/InitialAuthorizedLoginRoute.kt
@@ -1,10 +1,4 @@
-package com.babylon.wallet.android.presentation.dapp
-
-import com.babylon.wallet.android.presentation.dapp.authorized.account.ROUTE_CHOOSE_ACCOUNTS
-import com.babylon.wallet.android.presentation.dapp.authorized.permission.ROUTE_DAPP_PERMISSION
-import com.babylon.wallet.android.presentation.dapp.authorized.personaonetime.ROUTE_PERSONA_DATA_ONETIME_AUTHORIZED
-import com.babylon.wallet.android.presentation.dapp.authorized.personaongoing.ROUTE_PERSONA_DATA_ONGOING
-import com.babylon.wallet.android.presentation.dapp.authorized.selectpersona.ROUTE_SELECT_PERSONA
+//package com.babylon.wallet.android.presentation.dapp
 
 sealed interface InitialAuthorizedLoginRoute {
     data class SelectPersona(val reqId: String) : InitialAuthorizedLoginRoute
@@ -31,13 +25,4 @@ sealed interface InitialAuthorizedLoginRoute {
         val showBack: Boolean = false
     ) : InitialAuthorizedLoginRoute
 
-    fun toRouteString(): String {
-        return when (this) {
-            is ChooseAccount -> ROUTE_CHOOSE_ACCOUNTS
-            is Permission -> ROUTE_DAPP_PERMISSION
-            is SelectPersona -> ROUTE_SELECT_PERSONA
-            is OngoingPersonaData -> ROUTE_PERSONA_DATA_ONGOING
-            is OneTimePersonaData -> ROUTE_PERSONA_DATA_ONETIME_AUTHORIZED
-        }
-    }
 }
--- app/src/main/java/com/babylon/wallet/android/presentation/dapp/InitialUnauthorizedLoginRoute.kt
+++ app/src/main/java/com/babylon/wallet/android/presentation/dapp/InitialUnauthorizedLoginRoute.kt
@@ -1,8 +1,5 @@
 package com.babylon.wallet.android.presentation.dapp
 
-import com.babylon.wallet.android.presentation.dapp.unauthorized.accountonetime.ROUTE_CHOOSE_ACCOUNTS_ONETIME
-import com.babylon.wallet.android.presentation.dapp.unauthorized.personaonetime.ROUTE_PERSONA_DATA_ONETIME_UNAUTHORIZED
-
 sealed interface InitialUnauthorizedLoginRoute {
     data class OnetimePersonaData(
         val requestedFieldsEncoded: String
@@ -13,10 +10,4 @@ sealed interface InitialUnauthorizedLoginRoute {
         val isExactAccountsCount: Boolean
     ) : InitialUnauthorizedLoginRoute
 
-    fun toRouteString(): String {
-        return when (this) {
-            is ChooseAccount -> ROUTE_CHOOSE_ACCOUNTS_ONETIME
-            is OnetimePersonaData -> ROUTE_PERSONA_DATA_ONETIME_UNAUTHORIZED
-        }
-    }
 }
