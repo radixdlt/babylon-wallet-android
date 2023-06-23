5273c6cbda37ac34b7430e7d491199a19a2e4bd1
giannis.tsepas <giannis.tsepas@rdx.works>
2023-06-23 14:46:44 +0300

    refactored FactorSource: now id contains factor source kind and id value

--- profile/src/main/java/rdx/works/profile/data/model/factorsources/FactorSource.kt
+++ profile/src/main/java/rdx/works/profile/data/model/factorsources/FactorSource.kt
@@ -1,326 +1,131 @@
 package rdx.works.profile.data.model.factorsources
 
-import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.JsonClassDiscriminator
-import rdx.works.core.InstantGenerator
 import rdx.works.profile.data.model.MnemonicWithPassphrase
 import rdx.works.profile.data.model.compressedPublicKey
-import rdx.works.profile.data.model.factorsources.DerivationPathScheme.BIP_44_OLYMPIA
-import rdx.works.profile.data.model.factorsources.DerivationPathScheme.CAP_26
-import rdx.works.profile.data.model.factorsources.FactorSource.Parameters.Companion.babylon
-import rdx.works.profile.data.model.factorsources.FactorSource.Parameters.Companion.olympia
-import rdx.works.profile.data.model.factorsources.Slip10Curve.CURVE_25519
-import rdx.works.profile.data.model.factorsources.Slip10Curve.SECP_256K1
 import rdx.works.profile.data.model.pernetwork.DerivationPath
-import rdx.works.profile.data.model.pernetwork.Network
 import rdx.works.profile.data.model.serialisers.InstantSerializer
 import rdx.works.profile.data.utils.hashToFactorId
-import rdx.works.profile.derivation.model.NetworkId
 import java.time.Instant
 
-/**
- * A FactorSource is the source of FactorInstance(s)
- */
 @Serializable
-data class FactorSource(
-    /**
-     * The kind of the factor source.
-     */
-    @SerialName("kind")
-    val kind: FactorSourceKind,
+sealed interface FactorSource {
 
-    /**
-     * Canonical identifier which uniquely identifies this factor source.
-     */
     @SerialName("id")
-    val id: ID,
+    val id: FactorSourceID
 
-    /**
-     * A user facing **label** about this FactorSource which is displayed
-     * to the user when she is prompted for this FactorSource during
-     * for example transaction signing. For most FactorSource kinds
-     * this value will be a *name*, here are some examples:
-     *
-     * * `.device`: "iPhone RED"
-     * * `.ledgerHQHardwareWallet`: "Ledger MOON Edition"
-     * * `.trustedEntity`: "Sarah"
-     * * `.offDeviceMnemonic`: "Story about a horse and a battery"
-     * * `.securityQuestion`: ""
-     *
-     * The reason why this is mutable (`var`) instead of immutable `let` is
-     * an implementation detailed on iOS, where reading the device name
-     * and model is `async` but we want to be able to `sync` create the
-     * profile, thus this property at a later point in time where an async
-     * context is available.
-     */
-    @SerialName("label")
-    val label: String,
+    @SerialName("common")
+    val common: Common
 
-    /** A user facing **description** about this FactorSource which is displayed
-     * to the user when she is prompted for this FactorSource during
-     * for example transaction signing. For most FactorSource kinds
-     * this value will be a *model*, here are some examples:
-     *
-     * * `.device`: "iPhone SE 2nd gen"
-     * * `.ledgerHQHardwareWallet`: "nanoS+"
-     * * `.trustedEntity`: "Friend"
-     * * `.offDeviceMnemonic`: "Stored in the place where I played often with my friend A***"
-     * * `.securityQuestion`: ""
-     *
-     * The reason why this is mutable (`var`) instead of immutable `let` is
-     * an implementation detailed on iOS, where reading the device name
-     * and model is `async` but we want to be able to `sync` create the
-     * profile, thus this property at a later point in time where an async
-     * context is available.
-     */
-    @SerialName("description")
-    val description: String,
-
-    /**
-     * Curve/Derivation scheme
-     */
-    @SerialName("parameters")
-    val parameters: Parameters,
-
-    /**
-     * When this factor source for originally added by the user.
-     */
-    @Serializable(with = InstantSerializer::class)
-    @SerialName("addedOn")
-    val addedOn: Instant,
-
-    /**
-     * Date of last usage of this factor source
-     */
-    @Serializable(with = InstantSerializer::class)
-    @SerialName("lastUsedOn")
-    val lastUsedOn: Instant,
-
-    /**
-     * Some factor source requires extra stored properties, e.g.
-     * [FactorSourceKind.SECURITY_QUESTIONS] kind which requires storage of:
-     * * which questions user chose
-     * * the encryptions of the mnemonic
-     *
-     * Rather than letting ALL factor source contain ALL possible
-     * extra stored properties as optionals, which will be `null`
-     * for most FactorSource, we model this with one single optional
-     * being a sealed class modelling all possible required extra stored
-     * properties.
-     */
-    @SerialName("storage")
-    val storage: Storage? = null
-) {
-
-    fun getNextAccountDerivationIndex(forNetworkId: NetworkId): Int {
-        val entityCreatingStorage = storage as? Storage.EntityCreating ?: throw WasNotDeviceFactorSource()
-
-        return entityCreatingStorage.nextDerivationIndicesPerNetwork.find {
-            it.networkId == forNetworkId.value
-        }?.forAccount ?: 0
-    }
-
-    fun getNextIdentityDerivationIndex(forNetworkId: NetworkId): Int {
-        val entityCreatingStorage = storage as? Storage.EntityCreating ?: throw WasNotDeviceFactorSource()
-
-        return entityCreatingStorage.nextDerivationIndicesPerNetwork.find {
-            it.networkId == forNetworkId.value
-        }?.forIdentity ?: 0
-    }
-
-    fun supportsCurve(curve: Slip10Curve) = parameters.supportedCurves.contains(curve)
-
-    @JvmInline
     @Serializable
-    value class ID(
-        val value: String
+    sealed class FactorSourceID {
+        abstract val kind: FactorSourceKind
+
+        @Serializable
+        data class FromHash(
+            @SerialName("kind")
+            override val kind: FactorSourceKind,
+            @SerialName("body")
+            val body: HexCoded32Bytes
+        ) : FactorSourceID()
+
+        @Serializable
+        data class FromAddress(
+            @SerialName("kind")
+            override val kind: FactorSourceKind,
+            @SerialName("body")
+            val body: AccountAddress
+        ) : FactorSourceID()
+    }
+
+    @Serializable
+    @JvmInline
+    value class HexCoded32Bytes(val value: String)
+
+    // TODO move it to the top of the profile
+    @Serializable
+    @JvmInline
+    value class AccountAddress(val value: String)
+
+    @Serializable
+    data class Hint(
+        @SerialName("model")
+        val model: String,
+        @SerialName("name")
+        val name: String
     )
 
     @Serializable
-    data class Parameters(
-        @SerialName("supportedCurves")
-        val supportedCurves: LinkedHashSet<Slip10Curve>,
+    data class Common(
+        @SerialName("cryptoParameters")
+        val cryptoParameters: CryptoParameters,
+
         /**
-         * Can be empty if the factor source does not support HD derivation
+         * When this factor source for originally added by the user.
          */
-        @SerialName("supportedDerivationPathSchemes")
-        val supportedDerivationPathSchemes: LinkedHashSet<DerivationPathScheme>
+        @Serializable(with = InstantSerializer::class)
+        @SerialName("addedOn")
+        val addedOn: Instant,
+
+        /**
+         * Date of last usage of this factor source
+         */
+        @Serializable(with = InstantSerializer::class)
+        @SerialName("lastUsedOn")
+        var lastUsedOn: Instant
     ) {
 
-        val supportsOlympia: Boolean
-            get() = supportedCurves.contains(SECP_256K1) &&
-                supportedDerivationPathSchemes.contains(BIP_44_OLYMPIA)
-
-        companion object {
-            val babylon = Parameters(
-                supportedCurves = linkedSetOf(CURVE_25519),
-                supportedDerivationPathSchemes = linkedSetOf(CAP_26)
-            )
-
-            val olympia = Parameters(
-                supportedCurves = linkedSetOf(SECP_256K1, CURVE_25519),
-                supportedDerivationPathSchemes = linkedSetOf(CAP_26, BIP_44_OLYMPIA)
-            )
-        }
-    }
-
-    @Serializable
-    @OptIn(ExperimentalSerializationApi::class)
-    @JsonClassDiscriminator(discriminator = "discriminator")
-    sealed class Storage {
-
         @Serializable
-        @SerialName("entityCreating")
-        data class EntityCreating(
-            @SerialName("nextDerivationIndicesPerNetwork")
-            val nextDerivationIndicesPerNetwork: List<Network.NextDerivationIndices>
-        ) : Storage() {
+        data class CryptoParameters(
+            @SerialName("supportedCurves")
+            val supportedCurves: LinkedHashSet<Slip10Curve>,
+            /**
+             * Can be empty if the factor source does not support HD derivation
+             */
+            @SerialName("supportedDerivationPathSchemes")
+            val supportedDerivationPathSchemes: LinkedHashSet<DerivationPathScheme>
+        ) {
 
-            fun incrementAccount(forNetworkId: NetworkId): EntityCreating {
-                val indicesForNetwork = nextDerivationIndicesPerNetwork.find {
-                    it.networkId == forNetworkId.value
-                }
-
-                val mutatedList = if (indicesForNetwork == null) {
-                    nextDerivationIndicesPerNetwork + Network.NextDerivationIndices(
-                        networkId = forNetworkId.value,
-                        forAccount = 1,
-                        forIdentity = 0
-                    )
-                } else {
-                    nextDerivationIndicesPerNetwork.map {
-                        if (it.networkId == forNetworkId.value) {
-                            it.copy(forAccount = it.forAccount + 1)
-                        } else {
-                            it
-                        }
-                    }
-                }
-
-                return copy(nextDerivationIndicesPerNetwork = mutatedList)
-            }
-
-            fun incrementIdentity(forNetworkId: NetworkId): EntityCreating {
-                val indicesForNetwork = nextDerivationIndicesPerNetwork.find {
-                    it.networkId == forNetworkId.value
-                }
-
-                val mutatedList = if (indicesForNetwork == null) {
-                    nextDerivationIndicesPerNetwork + Network.NextDerivationIndices(
-                        networkId = forNetworkId.value,
-                        forAccount = 0,
-                        forIdentity = 1
-                    )
-                } else {
-                    nextDerivationIndicesPerNetwork.map {
-                        if (it.networkId == forNetworkId.value) {
-                            it.copy(forIdentity = it.forIdentity + 1)
-                        } else {
-                            it
-                        }
-                    }
-                }
-
-                return copy(nextDerivationIndicesPerNetwork = mutatedList)
-            }
-        }
-    }
-
-    sealed class LedgerHardwareWallet {
-        enum class DeviceModel {
-            NanoS, NanoSPlus, NanoX;
-
-            fun description(): String {
-                return when (this) {
-                    NanoS -> "nanoS"
-                    NanoSPlus -> "nanoS+"
-                    NanoX -> "nanoX"
-                }
-            }
+            val supportsOlympia: Boolean
+                get() = supportedCurves.contains(Slip10Curve.SECP_256K1) &&
+                    supportedDerivationPathSchemes.contains(DerivationPathScheme.BIP_44_OLYMPIA)
 
             companion object {
-                fun fromDescription(value: String): DeviceModel? {
-                    return when (value) {
-                        "nanoS" -> NanoS
-                        "nanoS+" -> NanoSPlus
-                        "nanoX" -> NanoX
-                        else -> null
-                    }
-                }
+                val babylon = CryptoParameters(
+                    supportedCurves = linkedSetOf(Slip10Curve.CURVE_25519),
+                    supportedDerivationPathSchemes = linkedSetOf(DerivationPathScheme.CAP_26)
+                )
+
+                val trustedEntity = CryptoParameters(
+                    supportedCurves = linkedSetOf(Slip10Curve.CURVE_25519),
+                    supportedDerivationPathSchemes = linkedSetOf()
+                )
+
+                val olympiaBackwardsCompatible = CryptoParameters(
+                    supportedCurves = linkedSetOf(Slip10Curve.SECP_256K1, Slip10Curve.CURVE_25519),
+                    supportedDerivationPathSchemes = linkedSetOf(
+                        DerivationPathScheme.CAP_26,
+                        DerivationPathScheme.BIP_44_OLYMPIA
+                    )
+                )
+
+                val default = babylon
             }
         }
     }
 
     companion object {
-        fun babylon(
-            mnemonicWithPassphrase: MnemonicWithPassphrase,
-            label: String = "Android",
-            description: String = "babylon",
-        ) = device(
-            mnemonicWithPassphrase = mnemonicWithPassphrase,
-            label = label,
-            description = description,
-            olympiaCompatible = false
-        )
-
-        fun olympia(
-            mnemonicWithPassphrase: MnemonicWithPassphrase,
-            label: String = "Android",
-            description: String = "olympia",
-        ) = device(
-            mnemonicWithPassphrase = mnemonicWithPassphrase,
-            label = label,
-            description = description,
-            olympiaCompatible = true
-        )
-
-        fun ledger(
-            id: ID,
-            model: LedgerHardwareWallet.DeviceModel,
-            name: String?,
-            olympiaCompatible: Boolean = true
-        ): FactorSource {
-            return FactorSource(
-                kind = FactorSourceKind.LEDGER_HQ_HARDWARE_WALLET,
-                id = id,
-                label = name?.ifEmpty { "Unnamed ${model.description()}" } ?: "Unnamed ${model.description()}",
-                description = model.description(),
-                parameters = if (olympiaCompatible) olympia else babylon,
-                storage = Storage.EntityCreating(nextDerivationIndicesPerNetwork = listOf()),
-                addedOn = InstantGenerator(),
-                lastUsedOn = InstantGenerator()
-            )
-        }
-
-        private fun device(
-            mnemonicWithPassphrase: MnemonicWithPassphrase,
-            label: String,
-            description: String,
-            olympiaCompatible: Boolean
-        ) = FactorSource(
-            kind = FactorSourceKind.DEVICE,
-            id = factorSourceId(mnemonicWithPassphrase = mnemonicWithPassphrase),
-            label = label,
-            description = description,
-            parameters = if (olympiaCompatible) olympia else babylon,
-            storage = Storage.EntityCreating(nextDerivationIndicesPerNetwork = listOf()),
-            addedOn = InstantGenerator(),
-            lastUsedOn = InstantGenerator()
-        )
 
         fun factorSourceId(
             mnemonicWithPassphrase: MnemonicWithPassphrase,
-            curve: Slip10Curve = CURVE_25519,
+            curve: Slip10Curve = Slip10Curve.CURVE_25519,
             derivationPath: DerivationPath = DerivationPath.forFactorSource(),
-        ): ID {
-            return ID(
-                mnemonicWithPassphrase.compressedPublicKey(
-                    curve = curve,
-                    derivationPath = derivationPath
-                ).hashToFactorId()
-            )
+        ): String {
+            return mnemonicWithPassphrase.compressedPublicKey(
+                curve = curve,
+                derivationPath = derivationPath
+            ).hashToFactorId()
         }
     }
 }
