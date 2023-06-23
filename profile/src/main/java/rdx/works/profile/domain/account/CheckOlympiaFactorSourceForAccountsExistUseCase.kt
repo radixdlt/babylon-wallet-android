5d7962ffcf7e71cb7a2bfe78ca8134bd6d1bdb36
giannis.tsepas <giannis.tsepas@rdx.works>
2023-06-23 14:57:11 +0300

    updated account usecases with new FactorSourceID

--- profile/src/main/java/rdx/works/profile/domain/account/CheckOlympiaFactorSourceForAccountsExistUseCase.kt
+++ profile/src/main/java/rdx/works/profile/domain/account/CheckOlympiaFactorSourceForAccountsExistUseCase.kt
@@ -4,34 +4,36 @@ import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.withContext
 import rdx.works.profile.data.model.factorsources.FactorSource
-import rdx.works.profile.data.model.factorsources.FactorSourceKind
 import rdx.works.profile.data.model.validatePublicKeysOf
 import rdx.works.profile.data.repository.MnemonicRepository
-import rdx.works.profile.data.repository.ProfileRepository
-import rdx.works.profile.data.repository.profile
 import rdx.works.profile.di.coroutines.DefaultDispatcher
+import rdx.works.profile.domain.GetProfileUseCase
+import rdx.works.profile.domain.deviceFactorSources
 import rdx.works.profile.olympiaimport.OlympiaAccountDetails
 import javax.inject.Inject
 
 class CheckOlympiaFactorSourceForAccountsExistUseCase @Inject constructor(
     private val mnemonicRepository: MnemonicRepository,
-    private val profileRepository: ProfileRepository,
+    private val getProfileUseCase: GetProfileUseCase,
     @DefaultDispatcher private val defaultDispatcher: CoroutineDispatcher
 ) {
-    suspend operator fun invoke(
-        olympiaAccounts: List<OlympiaAccountDetails>
-    ): FactorSource.ID? {
+    suspend operator fun invoke(olympiaAccounts: List<OlympiaAccountDetails>): FactorSource.FactorSourceID.FromHash? {
         return withContext(defaultDispatcher) {
-            val profile = profileRepository.profile.first()
-            val factorSourceIDs = profile.factorSources.filter {
-                it.kind == FactorSourceKind.DEVICE && it.parameters.supportsOlympia
-            }.map { it.id }
-            factorSourceIDs.forEach { id ->
-                val mnemonic = requireNotNull(mnemonicRepository.readMnemonic(id))
-                if (mnemonic.validatePublicKeysOf(olympiaAccounts)) {
-                    return@withContext id
+            getProfileUseCase.deviceFactorSources
+                .first()
+                .filter { deviceFactorSource ->
+                    deviceFactorSource.common.cryptoParameters.supportsOlympia
                 }
-            }
+                .map { deviceFactorSource ->
+                    deviceFactorSource.id
+                }
+                .forEach { fromHashId ->
+                    val mnemonic = requireNotNull(mnemonicRepository.readMnemonic(fromHashId))
+                    if (mnemonic.validatePublicKeysOf(olympiaAccounts)) {
+                        return@withContext fromHashId
+                    }
+                }
+
             return@withContext null
         }
     }
