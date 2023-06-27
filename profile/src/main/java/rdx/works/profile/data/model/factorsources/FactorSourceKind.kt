d71ea86d7d5d9037f4fbdef096005cd66d75afc4
giannis.tsepas <giannis.tsepas@rdx.works>
2023-06-27 15:35:11 +0300

    removed unused code in FactorSourceKind

--- profile/src/main/java/rdx/works/profile/data/model/factorsources/FactorSourceKind.kt
+++ profile/src/main/java/rdx/works/profile/data/model/factorsources/FactorSourceKind.kt
@@ -48,28 +48,6 @@ enum class FactorSourceKind {
     LEDGER_HQ_HARDWARE_WALLET,
 
     /**
-     * A user owned hardware wallet by vendor YubiCo, which the user has to produce (connect)
-     * in order to use. Most common model might be YubiKey 5C NFC.
-     *
-     * Attributes:
-     * * Mine
-     * * Off device
-     * * Hardware (directly readable by wallet)
-     */
-//    @SerialName("yubiKey")
-//    YUBIKEY,
-
-    /**
-     * A user known single key which the user has to produce (input).
-     *
-     * Attributes:
-     * * Mine
-     * * Off Device
-     */
-//    @SerialName("offDeviceSingleKey")
-//    OFF_DEVICE_SINGLE_KEY,
-
-    /**
      * A user known mnemonic which the user has to produce (input).
      *
      * Attributes:
@@ -81,18 +59,6 @@ enum class FactorSourceKind {
     OFF_DEVICE_MNEMONIC,
 
     /**
-     * A user known secret acting as input key material (*IKM*) for some
-     * function which maps it to Entropy for a Mnemonic.
-     *
-     * Attributes:
-     * * Mine
-     * * Off Device
-     * * Hierarchical deterministic
-     */
-//    @SerialName("offDeviceInputKeyMaterialForMnemonic")
-//    OFF_DEVICE_INPUT_KEY_MATERIAL_FOR_MNEMONIC,
-
-    /**
      * Some individual the user knows and trust, e.g. a friend or family member,
      * typically used as a factor source for the recovery role.
      *
@@ -102,84 +68,4 @@ enum class FactorSourceKind {
      */
     @SerialName("trustedContact")
     TRUSTED_CONTACT;
-
-    /**
-     * Some entity the user knows and trust, e.g. a company,
-     * typically used as a factor source for the recovery role.
-     *
-     * Attributes:
-     * * *Not* mine
-     * * Off Device
-     */
-//    @SerialName("trustedEnterprise")
-//    TRUSTED_ENTERPRISE;
-
-    val isHierarchicalDeterministic: Boolean
-        get() = when (this) {
-            DEVICE,
-            LEDGER_HQ_HARDWARE_WALLET,
-            OFF_DEVICE_MNEMONIC -> true
-//            SECURITY_QUESTIONS,
-//            OFF_DEVICE_INPUT_KEY_MATERIAL_FOR_MNEMONIC -> true
-//            YUBIKEY,
-//            OFF_DEVICE_SINGLE_KEY,
-            TRUSTED_CONTACT -> false
-//            TRUSTED_ENTERPRISE -> false
-        }
-
-    val isOnDevice: Boolean
-        get() = when (this) {
-            DEVICE -> true
-//            SECURITY_QUESTIONS -> true
-            LEDGER_HQ_HARDWARE_WALLET -> false
-//            YUBIKEY,
-//            OFF_DEVICE_SINGLE_KEY,
-            OFF_DEVICE_MNEMONIC -> false
-//            OFF_DEVICE_INPUT_KEY_MATERIAL_FOR_MNEMONIC,
-            TRUSTED_CONTACT -> false
-//            TRUSTED_ENTERPRISE -> false
-        }
-
-    val isMine: Boolean
-        get() = when (this) {
-            DEVICE,
-//            SECURITY_QUESTIONS,
-            LEDGER_HQ_HARDWARE_WALLET -> true
-//            YUBIKEY,
-//            OFF_DEVICE_SINGLE_KEY,
-            OFF_DEVICE_MNEMONIC -> true
-//            OFF_DEVICE_INPUT_KEY_MATERIAL_FOR_MNEMONIC -> true
-            TRUSTED_CONTACT -> false
-//            TRUSTED_ENTERPRISE -> false
-        }
-
-    /**
-     * Returns the kind of a hardware a factor source uses. If the factor source is not
-     * of hardware type, then it returns null.
-     */
-    val hardwareKind: HardwareKind?
-        get() = when (this) {
-            LEDGER_HQ_HARDWARE_WALLET -> HardwareKind.REQUIRES_BROWSER_CONNECTOR_EXTENSION
-//            YUBIKEY -> HardwareKind.DIRECT
-            DEVICE -> null
-//            SECURITY_QUESTIONS,
-//            OFF_DEVICE_SINGLE_KEY,
-            OFF_DEVICE_MNEMONIC -> null
-//            OFF_DEVICE_INPUT_KEY_MATERIAL_FOR_MNEMONIC,
-            TRUSTED_CONTACT -> null
-//            TRUSTED_ENTERPRISE -> null
-        }
-
-    enum class HardwareKind {
-        /**
-         * A hardware that can directly communicate with the wallet
-         */
-        DIRECT,
-
-        /**
-         * A hardware that requires a browser with the connector extension
-         * in order to connect
-         */
-        REQUIRES_BROWSER_CONNECTOR_EXTENSION
-    }
 }
