bd582864a57dc223c94a7a7f9f97e63257537759
giannis.tsepas <giannis.tsepas@rdx.works>
2023-06-29 10:53:10 +0300

    bump up profile snapshot min version to 37

--- profile/src/main/java/rdx/works/profile/data/model/ProfileSnapshot.kt
+++ profile/src/main/java/rdx/works/profile/data/model/ProfileSnapshot.kt
@@ -46,7 +46,7 @@ internal data class ProfileSnapshot(
         /**
          * The minimum accepted snapshot version format. Lower versions are currently discarded.
          */
-        const val MINIMUM = 35
+        const val MINIMUM = 37
 
         fun fromJson(serialised: String) = Json.decodeFromString<ProfileSnapshot>(serialised)
     }
