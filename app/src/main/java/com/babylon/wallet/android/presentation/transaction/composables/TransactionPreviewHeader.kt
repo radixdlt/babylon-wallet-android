77ffed6f8cea17af63f0fb34515218dc63f1eec8
micbakos-rdx <michael.bakogiannis@rdx.works>
2023-07-13 17:12:01 +0300

    Fix toolbar animation temporarily

--- app/src/main/java/com/babylon/wallet/android/presentation/transaction/composables/TransactionPreviewHeader.kt
+++ app/src/main/java/com/babylon/wallet/android/presentation/transaction/composables/TransactionPreviewHeader.kt
@@ -3,7 +3,11 @@
 package com.babylon.wallet.android.presentation.transaction.composables
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeContent
 import androidx.compose.material.IconButton
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -12,10 +16,13 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.babylon.wallet.android.R
 import com.babylon.wallet.android.data.transaction.TransactionVersion
@@ -35,15 +42,31 @@ fun TransactionPreviewHeader(
     onRawManifestClick: () -> Unit,
     scrollBehavior: TopAppBarScrollBehavior
 ) {
-    val collapsed = scrollBehavior.state.collapsedFraction >= 0.5f
+    val fraction = scrollBehavior.state.collapsedFraction
     LargeTopAppBar(
         modifier = modifier,
         title = {
-            Text(
-                text = stringResource(R.string.transactionReview_title),
-                style = if (!collapsed) RadixTheme.typography.title else RadixTheme.typography.body1Header,
-                color = RadixTheme.colors.gray1,
-            )
+            Box {
+                Text(
+                    modifier = Modifier.alpha(1 - fraction * 2),
+                    text = stringResource(R.string.transactionReview_title),
+                    style = RadixTheme.typography.title,
+                    color = RadixTheme.colors.gray1,
+                    textAlign = TextAlign.Start,
+                    maxLines = 2
+                )
+
+                val smallAlpha = if (fraction in 0f..0.6f) 0f else fraction + fraction * 0.6f
+                Text(
+                    modifier = Modifier.alpha(smallAlpha).align(Alignment.Center),
+                    text = stringResource(R.string.transactionReview_title),
+                    style = RadixTheme.typography.body1Header,
+                    color = RadixTheme.colors.gray1,
+                    textAlign = TextAlign.Center,
+                    maxLines = 2
+                )
+            }
+
         },
         navigationIcon = {
             IconButton(
@@ -80,6 +103,7 @@ fun TransactionPreviewHeader(
                 }
             }
         },
+        windowInsets = WindowInsets(0, 0, 0, 0),
         colors = TopAppBarDefaults.largeTopAppBarColors(
             containerColor = RadixTheme.colors.defaultBackground,
             scrolledContainerColor = RadixTheme.colors.defaultBackground
