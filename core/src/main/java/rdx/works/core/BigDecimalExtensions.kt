5e4f1447bffb7f9d74e649634f0a8716aae743ac
Rafal Wesolowski <rafal.wesolowski@rdx.works>
2023-07-19 09:23:28 +0200

    ABW-1903 - Token formatting fixed.

--- core/src/main/java/rdx/works/core/BigDecimalExtensions.kt
+++ core/src/main/java/rdx/works/core/BigDecimalExtensions.kt
@@ -7,6 +7,11 @@ import java.text.DecimalFormat
 import java.util.Locale
 
 private const val MAX_TOKEN_DIGITS_LENGTH = 8
+private const val MILLION_DIGITS_LENGTH = 6
+
+// We still want to display 1,000 M so we catch 10 instead of 9 to not fall into B for thousands
+private const val BILLION_DIGITS_LENGTH = 10
+private const val TRILLION_DIGITS_LENGTH = 12
 
 /**
  * Algorithm that implements the rules of token amount display:
@@ -26,22 +31,40 @@ fun BigDecimal.displayableQuantity(): String {
     }
     val decimalFormat = DecimalFormat.getInstance(Locale.getDefault())
 
-    return if (integralPartLength >= MAX_TOKEN_DIGITS_LENGTH && decimalPartLength == 0) {
+    return if (integralPartLength > MAX_TOKEN_DIGITS_LENGTH) {
+        if (integralPartLength > TRILLION_DIGITS_LENGTH) {
+            // trillion
+            val wholeNumbers = this.divide(BigDecimal(10).pow(TRILLION_DIGITS_LENGTH))
+            val wholeDecimalPlaces = wholeNumbers.toBigInteger().toString().length
+            decimalFormat.maximumFractionDigits = MAX_TOKEN_DIGITS_LENGTH - wholeDecimalPlaces
+            decimalFormat.format(wholeNumbers).plus(" T")
+        } else if (integralPartLength > BILLION_DIGITS_LENGTH) {
+            // billion
+            val wholeNumbers = this.divide(BigDecimal(10).pow(BILLION_DIGITS_LENGTH-1))
+            val wholeDecimalPlaces = wholeNumbers.toBigInteger().toString().length
+            decimalFormat.maximumFractionDigits = MAX_TOKEN_DIGITS_LENGTH - wholeDecimalPlaces
+            decimalFormat.format(wholeNumbers).plus(" B")
+        } else {
+            // million
+            val wholeNumbers = this.divide(BigDecimal(10).pow(MILLION_DIGITS_LENGTH))
+            val wholeDecimalPlaces = wholeNumbers.toBigInteger().toString().length
+            decimalFormat.maximumFractionDigits = MAX_TOKEN_DIGITS_LENGTH - wholeDecimalPlaces
+            decimalFormat.format(wholeNumbers).plus(" M")
+        }
+    } else if (integralPartLength >= MAX_TOKEN_DIGITS_LENGTH && decimalPartLength == 0) {
         decimalFormat.format(this)
-    } else if (integralPartLength >= MAX_TOKEN_DIGITS_LENGTH && decimalPartLength > 0) {
-        decimalFormat.maximumFractionDigits = 1
+    } else if (integralPartLength >= MAX_TOKEN_DIGITS_LENGTH) {
+        decimalFormat.maximumFractionDigits = 0
         decimalFormat.format(this)
     } else if (integralPartLength + decimalPartLength <= MAX_TOKEN_DIGITS_LENGTH) {
         decimalFormat.minimumFractionDigits = decimalPartLength
         decimalFormat.format(this)
     } else if (integralPartLength == 0 && decimalPartLength >= MAX_TOKEN_DIGITS_LENGTH - 1) {
-        val roundTokenQuantity = this.setScale(MAX_TOKEN_DIGITS_LENGTH, RoundingMode.FLOOR)
-        val roundTokenQuantityStr = roundTokenQuantity.toPlainString()
-        val result = roundTokenQuantityStr.toDouble()
-        if (result == 0.0) {
+        val roundTokenQuantity = this.setScale(MAX_TOKEN_DIGITS_LENGTH - 1, RoundingMode.HALF_UP).stripTrailingZeros()
+        if (roundTokenQuantity == BigDecimal.ZERO) {
             "0"
         } else {
-            result.toString()
+            roundTokenQuantity.toPlainString()
         }
     } else {
         decimalFormat.maximumFractionDigits = MAX_TOKEN_DIGITS_LENGTH - integralPartLength
