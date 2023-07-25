aa445be9af004ac77c34aa7ab5861634b7e02ce4
micbakos-rdx <michael.bakogiannis@rdx.works>
2023-07-25 10:50:02 +0300

    Remove unused import

--- app/src/test/java/com/babylon/wallet/android/data/transaction/TransactionClientTest.kt
+++ app/src/test/java/com/babylon/wallet/android/data/transaction/TransactionClientTest.kt
@@ -38,7 +38,6 @@ import rdx.works.profile.data.model.apppreferences.Radix
 import rdx.works.profile.data.model.pernetwork.Network
 import rdx.works.profile.data.repository.ProfileRepository
 import rdx.works.profile.domain.GetProfileUseCase
-import java.math.BigDecimal
 
 internal class TransactionClientTest {
 
