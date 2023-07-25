f603f1fd515035e746d563611da55a663216202a
giannis.tsepas <giannis.tsepas@rdx.works>
2023-07-25 18:17:01 +0300

    created RedWarningText reusable composable

--- app/src/main/java/com/babylon/wallet/android/presentation/ui/composables/RedWarningText.kt
+++ app/src/main/java/com/babylon/wallet/android/presentation/ui/composables/RedWarningText.kt
@@ -7,29 +7,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.withStyle
-import com.babylon.wallet.android.R
+import androidx.compose.ui.text.AnnotatedString
 import com.babylon.wallet.android.designsystem.theme.RadixTheme
-import com.babylon.wallet.android.presentation.model.toDisplayResource
-import kotlinx.collections.immutable.ImmutableList
-import rdx.works.profile.data.model.pernetwork.PersonaData
 
 @Composable
-fun RequiredPersonaInformationInfo(
-    requiredFields: ImmutableList<PersonaData.PersonaDataField.Kind>,
-    modifier: Modifier = Modifier
+fun RedWarningText(
+    modifier: Modifier = Modifier,
+    text: AnnotatedString
 ) {
-    val text = stringResource(id = R.string.dAppRequest_personalDataBox_requiredInformation)
-    val finalText = buildAnnotatedString {
-        withStyle(SpanStyle(fontWeight = FontWeight.SemiBold)) {
-            append("$text ")
-        }
-        append(requiredFields.map { stringResource(id = it.toDisplayResource()) }.joinToString())
-    }
     Row(
         modifier = modifier,
         verticalAlignment = Alignment.CenterVertically,
@@ -43,7 +28,7 @@ fun RequiredPersonaInformationInfo(
             tint = RadixTheme.colors.red1
         )
         androidx.compose.material3.Text(
-            text = finalText,
+            text = text,
             style = RadixTheme.typography.body1StandaloneLink,
             color = RadixTheme.colors.red1
         )
