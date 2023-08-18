33bb3481b27ed318676bec2b7a35769a27e08998
micbakos-rdx <michael.bakogiannis@rdx.works>
2023-08-18 17:25:06 +0300

    Mark mnemonic as backed up when the user has restored it

--- profile/src/main/java/rdx/works/profile/domain/backup/RestoreMnemonicUseCase.kt
+++ profile/src/main/java/rdx/works/profile/domain/backup/RestoreMnemonicUseCase.kt
@@ -1,16 +1,21 @@
 package rdx.works.profile.domain.backup
 
+import rdx.works.core.preferences.PreferencesManager
 import rdx.works.profile.data.model.MnemonicWithPassphrase
 import rdx.works.profile.data.model.factorsources.FactorSource
 import rdx.works.profile.data.repository.MnemonicRepository
 import javax.inject.Inject
 
 class RestoreMnemonicUseCase @Inject constructor(
-    private val mnemonicRepository: MnemonicRepository
+    private val mnemonicRepository: MnemonicRepository,
+    private val preferencesManager: PreferencesManager
 ) {
 
     suspend operator fun invoke(
         factorSourceId: FactorSource.FactorSourceID.FromHash,
         mnemonicWithPassphrase: MnemonicWithPassphrase
-    ) = mnemonicRepository.saveMnemonic(factorSourceId, mnemonicWithPassphrase)
+    ) {
+        mnemonicRepository.saveMnemonic(factorSourceId, mnemonicWithPassphrase)
+        preferencesManager.markFactorSourceBackedUp(factorSourceId.body.value)
+    }
 }
