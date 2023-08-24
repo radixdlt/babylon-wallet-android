2aa1a54ab518c5a4df56215622ff0b9929aadc09
micbakos-rdx <michael.bakogiannis@rdx.works>
2023-08-24 13:15:10 +0300

    Add read from file ability to file repository

--- core/src/main/java/rdx/works/core/storage/FileRepository.kt
+++ core/src/main/java/rdx/works/core/storage/FileRepository.kt
@@ -3,11 +3,16 @@ package rdx.works.core.storage
 import android.content.Context
 import android.net.Uri
 import dagger.hilt.android.qualifiers.ApplicationContext
+import okio.buffer
+import okio.source
+import java.lang.NullPointerException
 import javax.inject.Inject
 
 interface FileRepository {
 
     fun save(toFile: Uri, data: String): Result<Unit>
+
+    fun read(fromFile: Uri): Result<String>
 
 }
 
@@ -22,6 +27,18 @@ class FileRepositoryImpl @Inject constructor(
             }
 
             Result.success(Unit)
+        } catch (exception: Exception) {
+            Result.failure(exception)
+        }
+    }
+
+    override fun read(fromFile: Uri): Result<String> {
+        return try {
+            val fileContent = context.contentResolver.openInputStream(fromFile)?.use { stream ->
+                stream.source().buffer().readUtf8()
+            } ?: return Result.failure(NullPointerException())
+
+            Result.success(fileContent)
         } catch (exception: Exception) {
             Result.failure(exception)
         }
