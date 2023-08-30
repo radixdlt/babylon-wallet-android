3e1496e26bcd50d6b695af556bb255fddabdd748
micbakos-rdx <michael.bakogiannis@rdx.works>
2023-08-30 18:15:03 +0300

    Fix app settings' insets

--- app/src/main/java/com/babylon/wallet/android/presentation/settings/appsettings/AppSettingsScreen.kt
+++ app/src/main/java/com/babylon/wallet/android/presentation/settings/appsettings/AppSettingsScreen.kt
@@ -2,12 +2,15 @@ package com.babylon.wallet.android.presentation.settings.appsettings
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Divider
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
@@ -34,10 +37,7 @@ fun AppSettingsScreen(
     AppSettingsContent(
         onBackClick = onBackClick,
         appSettings = state.settings,
-        modifier = modifier
-            .navigationBarsPadding()
-            .fillMaxSize()
-            .background(RadixTheme.colors.defaultBackground),
+        modifier = modifier,
         onDeveloperModeToggled = viewModel::onDeveloperModeToggled
     )
 }
@@ -49,17 +49,23 @@ private fun AppSettingsContent(
     modifier: Modifier = Modifier,
     onDeveloperModeToggled: (Boolean) -> Unit
 ) {
-    Column(
-        modifier = modifier.background(RadixTheme.colors.defaultBackground),
-        horizontalAlignment = Alignment.Start
-    ) {
-        RadixCenteredTopAppBar(
-            title = stringResource(R.string.appSettings_title),
-            onBackClick = onBackClick,
-            contentColor = RadixTheme.colors.gray1
-        )
-        Divider(color = RadixTheme.colors.gray5)
-        LazyColumn(modifier = Modifier.fillMaxSize()) {
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            RadixCenteredTopAppBar(
+                title = stringResource(R.string.appSettings_title),
+                onBackClick = onBackClick,
+                windowInsets = WindowInsets.statusBars
+            )
+        },
+        containerColor = RadixTheme.colors.defaultBackground
+    ) { padding ->
+        Column(
+            modifier = modifier.padding(padding),
+            horizontalAlignment = Alignment.Start
+        ) {
+            Divider(color = RadixTheme.colors.gray5)
+            LazyColumn(modifier = Modifier.fillMaxSize()) {
 //            item { // TODO when we can really customize it
 //                Text(
 //                    modifier = Modifier.padding(all = RadixTheme.dimensions.paddingDefault),
@@ -68,25 +74,26 @@ private fun AppSettingsContent(
 //                    color = RadixTheme.colors.gray2
 //                )
 //            }
-            appSettings.forEachIndexed { index, settingsItem ->
-                item {
-                    when (settingsItem) {
-                        is SettingsItem.AppSettings.DeveloperMode -> {
-                            SwitchSettingsItem(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(all = RadixTheme.dimensions.paddingDefault),
-                                titleRes = settingsItem.descriptionRes(),
-                                subtitleRes = settingsItem.subtitleRes(),
-                                iconResource = settingsItem.getIcon(),
-                                checked = settingsItem.enabled,
-                                onCheckedChange = onDeveloperModeToggled
-                            )
+                appSettings.forEachIndexed { index, settingsItem ->
+                    item {
+                        when (settingsItem) {
+                            is SettingsItem.AppSettings.DeveloperMode -> {
+                                SwitchSettingsItem(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(all = RadixTheme.dimensions.paddingDefault),
+                                    titleRes = settingsItem.descriptionRes(),
+                                    subtitleRes = settingsItem.subtitleRes(),
+                                    iconResource = settingsItem.getIcon(),
+                                    checked = settingsItem.enabled,
+                                    onCheckedChange = onDeveloperModeToggled
+                                )
+                            }
                         }
-                    }
 
-                    if (index < appSettings.count() - 1) {
-                        Divider(color = RadixTheme.colors.gray5)
+                        if (index < appSettings.count() - 1) {
+                            Divider(color = RadixTheme.colors.gray5)
+                        }
                     }
                 }
             }
