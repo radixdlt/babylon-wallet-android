847b95dfc52a55af231d054be6feee2a63c98f98
Jakub Porzuczek <jakub.porzuczek@rdx.works>
2023-09-22 10:20:06 +0200

    check if profile is initialized before validating mnemonic integrity

--- profile/src/main/java/rdx/works/profile/data/model/ProfileState.kt
+++ profile/src/main/java/rdx/works/profile/data/model/ProfileState.kt
@@ -7,20 +7,20 @@ sealed class ProfileState {
      * that describes the saved state of the [ProfileSnapshot]. This is needed until the
      * repository owning this flow, initialises and reads it from internal storage.
      */
-    object NotInitialised : ProfileState()
+    data object NotInitialised : ProfileState()
 
     /**
      * The repository has tried to query the [ProfileSnapshot] from internal storage, but
      * there is nothing there. The user has not created a [Profile] yet.
      */
-    object None : ProfileState()
+    data object None : ProfileState()
 
     /**
      * The [ProfileSnapshot]'s version saved in the internal storage is lower than the
      * [ProfileSnapshot.MINIMUM], so it is incompatible. Currently the user can only
      * create a new profile.
      */
-    object Incompatible : ProfileState()
+    data object Incompatible : ProfileState()
 
     /**
      * A compatible [ProfileSnapshot] exists and the user can derive the [Profile].
