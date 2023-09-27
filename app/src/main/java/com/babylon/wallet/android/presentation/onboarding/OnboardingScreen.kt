a48cf7b3b923a20c4b6916634432c734329ab219
micbakos-rdx <michael.bakogiannis@rdx.works>
2023-09-27 20:44:28 +0300

    Fix detekt issues

--- app/src/main/java/com/babylon/wallet/android/presentation/onboarding/OnboardingScreen.kt
+++ app/src/main/java/com/babylon/wallet/android/presentation/onboarding/OnboardingScreen.kt
@@ -21,8 +21,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Density
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
 import com.babylon.wallet.android.R
 import com.babylon.wallet.android.designsystem.composable.RadixPrimaryButton
