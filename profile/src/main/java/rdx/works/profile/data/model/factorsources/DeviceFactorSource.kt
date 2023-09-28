160518b1de2503c01be52263e378561a505f653c
Jakub Porzuczek <jakub.porzuczek@rdx.works>
2023-09-28 21:41:29 +0200

    remove equals

--- profile/src/main/java/rdx/works/profile/data/model/factorsources/DeviceFactorSource.kt
+++ profile/src/main/java/rdx/works/profile/data/model/factorsources/DeviceFactorSource.kt
@@ -89,19 +89,4 @@ data class DeviceFactorSource(
             )
         )
     }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as DeviceFactorSource
-
-        if (id != other.id) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        return id.hashCode()
-    }
 }
