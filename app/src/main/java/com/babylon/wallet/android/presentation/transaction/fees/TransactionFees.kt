ff6f226e7bd9f91e681a0eaa647fc7181a518d24
Rafal Wesolowski <rafal.wesolowski@rdx.works>
2023-10-03 12:11:05 +0200

    ABW-2398 - Tip not accepting decimals, only whole numbers.

--- app/src/main/java/com/babylon/wallet/android/presentation/transaction/fees/TransactionFees.kt
+++ app/src/main/java/com/babylon/wallet/android/presentation/transaction/fees/TransactionFees.kt
@@ -113,7 +113,7 @@ data class TransactionFees(
      */
     @Suppress("MagicNumber")
     val effectiveTip: BigDecimal
-        get() = tipPercentageBigDecimal?.divide(
+        get() = tipPercentageNumber?.toBigDecimal()?.divide(
             BigDecimal(100)
         )?.multiply(
             totalExecutionCost.add(
@@ -143,8 +143,8 @@ data class TransactionFees(
             )
         )
 
-    private val tipPercentageBigDecimal: BigDecimal?
-        get() = tipPercentage?.toBigDecimalOrNull()
+    private val tipPercentageNumber: Int?
+        get() = tipPercentage?.toIntOrNull()
 
     @Suppress("MagicNumber")
     val feePaddingAmountToDisplay: String
@@ -165,8 +165,7 @@ data class TransactionFees(
         get() = tipPercentage ?: "0"
 
     val tipPercentageForTransaction: UShort
-        get() = tipPercentage?.toLong()?.toUShort()
-            ?: TransactionConfig.TIP_PERCENTAGE
+        get() = tipPercentageNumber?.toUShort() ?: TransactionConfig.TIP_PERCENTAGE
 
     companion object {
         private val PERCENT_15 = BigDecimal(0.15)
