628541ac545b137bca3b4162e073f7e8c2beefb7
micbakos-rdx <michael.bakogiannis@rdx.works>
2023-10-04 17:26:19 +0300

    Implement round-trip tests with JSONAssert

--- profile/src/test/java/rdx/works/profile/snapshot/SnapshotEncodingTests.kt
+++ profile/src/test/java/rdx/works/profile/snapshot/SnapshotEncodingTests.kt
@@ -2,10 +2,13 @@ package rdx.works.profile.snapshot
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
+import org.skyscreamer.jsonassert.JSONAssert
+import rdx.works.core.HexCoded32Bytes
 import rdx.works.profile.data.model.EncryptedProfileSnapshot
 import rdx.works.profile.data.model.MnemonicWithPassphrase
 import rdx.works.profile.data.model.ProfileSnapshot
@@ -18,7 +21,6 @@ import rdx.works.profile.data.model.pernetwork.validateAgainst
 import rdx.works.profile.di.SerializerModule
 import java.io.File
 import kotlin.test.assertEquals
-import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
 @RunWith(Parameterized::class)
@@ -27,7 +29,7 @@ class SnapshotEncodingTests(private val versionUnderTest: Int) {
     private val serializer = SerializerModule.provideProfileSerializer()
 
     @Test
-    fun `multiple snapshots version `() {
+    fun `test round-trip for multiple `() {
         val multiple = File("$TEST_VECTOR_DIR/$versionUnderTest/$MULTI_SNAPSHOT_VECTOR_FILE").readText()
         val testVector = serializer.decodeFromString<TestVector>(multiple)
 
@@ -39,6 +41,22 @@ class SnapshotEncodingTests(private val versionUnderTest: Int) {
         )
         // Validate plaintext snapshot has the correct version
         assertEquals(versionUnderTest, testVector.plaintext.toProfile().header.snapshotVersion)
+
+        val expected = File("$TEST_VECTOR_DIR/$versionUnderTest/$BASE_SNAPSHOT").readText()
+        decryptedSnapshots.forEach { snapshot ->
+            val actual = serializer.encodeToString(snapshot)
+
+            JSONAssert.assertEquals(expected, actual, false)
+        }
+    }
+
+    @Test
+    fun `test round-trip for snapshot `() {
+        val expected = File("$TEST_VECTOR_DIR/$versionUnderTest/$BASE_SNAPSHOT").readText()
+        val snapshot = serializer.decodeFromString<ProfileSnapshot>(expected)
+
+        val actual = serializer.encodeToString(snapshot)
+        JSONAssert.assertEquals(expected, actual, false)
     }
 
     @Serializable
@@ -115,11 +133,75 @@ class SnapshotEncodingTests(private val versionUnderTest: Int) {
                 fun toDomain() = MnemonicWithPassphrase(mnemonic, passphrase)
             }
         }
+
+        companion object {
+
+            fun from(
+                serializer: Json,
+                snapshot: ProfileSnapshot,
+                mnemonics: List<MnemonicWithPassphrase>,
+                passwords: List<String>
+            ): TestVector {
+                val profile = snapshot.toProfile()
+
+                return TestVector(
+                    snapshotVersion = profile.header.snapshotVersion,
+                    encryptedSnapshots = passwords.map {
+                        EncryptedMnemonicWithPassword(
+                            password = it,
+                            snapshot = EncryptedProfileSnapshot.from(
+                                serializer = serializer,
+                                snapshot = snapshot,
+                                password = it
+                            )
+                        )
+                    },
+                    mnemonics = mnemonics.map {
+                        MnemonicWithFactorSourceId(
+                            factorSourceID = FactorSource.FactorSourceID.FromHash(
+                                kind = FactorSourceKind.DEVICE,
+                                body = HexCoded32Bytes(FactorSource.factorSourceId(mnemonicWithPassphrase = it))
+                            ),
+                            mnemonicWithPassphrase = MnemonicWithFactorSourceId.Mnemonic(
+                                mnemonic = it.mnemonic,
+                                passphrase = it.bip39Passphrase
+                            )
+                        )
+                    },
+                    plaintext = snapshot
+                )
+            }
+
+        }
     }
 
     companion object {
         private const val TEST_VECTOR_DIR = "src/test/resources/version"
         private const val MULTI_SNAPSHOT_VECTOR_FILE = "multi_profile_snapshots.json"
+        private const val BASE_SNAPSHOT = "base_profile_snapshot.json"
+
+        private val MNEMONICS = listOf(
+            MnemonicWithPassphrase(
+                mnemonic = "alley urge tag valid execute hat little funny armed salute orient hurt balcony urban found clip tennis wrong " +
+                        "turtle canoe castle exist pledge test",
+                bip39Passphrase = ""
+            ),
+            MnemonicWithPassphrase(
+                mnemonic = "gentle hawk winner rain embrace erosion call update photo frost fatal wrestle",
+                bip39Passphrase = ""
+            ),
+            MnemonicWithPassphrase(
+                mnemonic = "smile entry satisfy shed margin rubber disorder hungry foot error ribbon cradle aim round october blind " +
+                        "lab spend",
+                bip39Passphrase = ""
+            )
+        )
+
+        private val PASSWORDS = listOf(
+            "",
+            "Radix... just imagine!",
+            "babylon"
+        )
 
         @JvmStatic
         @Parameterized.Parameters(name = "{0}")
