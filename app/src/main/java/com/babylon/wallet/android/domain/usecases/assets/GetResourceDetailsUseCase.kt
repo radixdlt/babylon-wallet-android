8014735e907eac4872ecb378919bb25cfbb368aa
micbakos-rdx <michael.bakogiannis@rdx.works>
2023-10-30 13:57:56 +0200

    Add some simple documentation

--- app/src/main/java/com/babylon/wallet/android/domain/usecases/assets/GetResourceDetailsUseCase.kt
+++ app/src/main/java/com/babylon/wallet/android/domain/usecases/assets/GetResourceDetailsUseCase.kt
@@ -5,6 +5,10 @@ import com.babylon.wallet.android.domain.model.resources.Resource
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
+/**
+ * Fetches details regarding this fungible/non-fungible by looking it up through its address.
+ * If an account address is provided, the repository will try to also fetch amount information.
+ */
 class GetResourceDetailsUseCase @Inject constructor(
     private val stateRepository: StateRepository
 ) {
