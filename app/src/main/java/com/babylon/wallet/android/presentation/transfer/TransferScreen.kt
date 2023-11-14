0b24e7d685c3610d40b307d8e0ff30d5d24104ee
Rafal Wesolowski <rafal.wesolowski@rdx.works>
2023-11-14 14:30:33 +0100

    ABW-2498 - Temp detekt fix.

--- app/src/main/java/com/babylon/wallet/android/presentation/transfer/TransferScreen.kt
+++ app/src/main/java/com/babylon/wallet/android/presentation/transfer/TransferScreen.kt
@@ -142,7 +142,9 @@ fun TransferContent(
         BasicPromptAlertDialog(
             finish = onMaxAmountApplied,
             title = "Sending All XRD", // TODO Crowdin
-            text = "Sending the full amount of XRD in this account will require you to pay the transaction fee from a different account. Or, the wallet can reduce the amount transferred so the fee can be paid from this account. Choose the amount to transfer:",
+            text = "Sending the full amount of XRD in this account will require you to pay the transaction " +
+                    "fee from a different account. Or, the wallet can reduce the amount transferred so the fee can be " +
+                    "paid from this account. Choose the amount to transfer:",
             confirmText = "${error.amountWithoutFees.displayableQuantity()} (send all XRD)",
             dismissText = "${error.maxAccountAmount.displayableQuantity()} (save 1 XRD for fee)"
         )
