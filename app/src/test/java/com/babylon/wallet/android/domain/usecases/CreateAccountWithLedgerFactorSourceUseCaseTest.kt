2ce09caef798659ae1d86033b850e47b5998df95
Jakub Porzuczek <jakub.porzuczek@rdx.works>
2023-12-14 14:37:08 +0100

    update test

--- app/src/test/java/com/babylon/wallet/android/domain/usecases/CreateAccountWithLedgerFactorSourceUseCaseTest.kt
+++ app/src/test/java/com/babylon/wallet/android/domain/usecases/CreateAccountWithLedgerFactorSourceUseCaseTest.kt
@@ -1,70 +1,80 @@
 package com.babylon.wallet.android.domain.usecases
 
 import com.babylon.wallet.android.data.repository.ResolveAccountsLedgerStateRepository
-import com.babylon.wallet.android.designsystem.theme.AccountGradientList
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.withContext
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.TestScope
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+import rdx.works.profile.data.model.MnemonicWithPassphrase
+import rdx.works.profile.data.model.ProfileState
 import rdx.works.profile.data.model.apppreferences.Radix
-import rdx.works.profile.data.model.currentNetwork
-import rdx.works.profile.data.model.factorsources.FactorSource
-import rdx.works.profile.data.model.factorsources.LedgerHardwareWalletFactorSource
+import rdx.works.profile.data.model.factorsources.DerivationPathScheme
 import rdx.works.profile.data.model.pernetwork.DerivationPath
-import rdx.works.profile.data.model.pernetwork.Network
 import rdx.works.profile.data.model.pernetwork.addAccounts
 import rdx.works.profile.data.model.pernetwork.nextAccountIndex
 import rdx.works.profile.data.repository.ProfileRepository
-import rdx.works.profile.data.repository.profile
-import rdx.works.profile.derivation.model.NetworkId
-import rdx.works.profile.di.coroutines.DefaultDispatcher
-import javax.inject.Inject
+import rdx.works.profile.derivation.model.KeyType
+import rdx.works.profile.domain.TestData
 
-class CreateAccountWithLedgerFactorSourceUseCase @Inject constructor(
-    private val profileRepository: ProfileRepository,
-    private val resolveAccountsLedgerStateRepository: ResolveAccountsLedgerStateRepository,
-    @DefaultDispatcher private val defaultDispatcher: CoroutineDispatcher
-) {
-    suspend operator fun invoke(
-        displayName: String,
-        derivedPublicKeyHex: String,
-        ledgerFactorSourceID: FactorSource.FactorSourceID.FromHash,
-        derivationPath: DerivationPath,
-        networkID: NetworkId? = null
-    ): Network.Account {
-        return withContext(defaultDispatcher) {
-            val profile = profileRepository.profile.first()
+internal class CreateAccountWithLedgerFactorSourceUseCaseTest {
+    private val testDispatcher = StandardTestDispatcher()
+    private val testScope = TestScope(testDispatcher)
+    private val resolveAccountsLedgerStateRepository = mockk<ResolveAccountsLedgerStateRepository>()
 
-            val ledgerHardwareWalletFactorSource = profile.factorSources
-                .first {
-                    it.id == ledgerFactorSourceID
-                } as LedgerHardwareWalletFactorSource
-            // Construct new account
-            val networkId = networkID ?: profile.currentNetwork?.knownNetworkId ?: Radix.Gateway.default.network.networkId()
-            val totalAccountsOnNetwork = profile.networks.find { it.networkID == networkId.value }?.accounts?.size ?: 0
-            val newAccount = Network.Account.initAccountWithLedgerFactorSource(
-                entityIndex = profile.nextAccountIndex(derivationPath.scheme, networkId, ledgerFactorSourceID),
-                displayName = displayName,
-                derivedPublicKeyHex = derivedPublicKeyHex,
-                ledgerFactorSource = ledgerHardwareWalletFactorSource,
-                networkId = networkId,
+    @Before
+    fun setUp() {
+        coEvery { resolveAccountsLedgerStateRepository.invoke(any()) } returns Result.failure(Exception(""))
+    }
+
+    @Test
+    fun `given profile already exists, creating new ledger account adds it to profile`() {
+        testScope.runTest {
+            // given
+            val mnemonicWithPassphrase = MnemonicWithPassphrase(
+                mnemonic = "noodle question hungry sail type offer grocery clay nation hello mixture forum",
+                bip39Passphrase = ""
+            )
+            val accountName = "First account"
+            val network = Radix.Gateway.hammunet.network
+            val profile = TestData.testProfile2Networks2AccountsEach(mnemonicWithPassphrase)
+
+            val profileRepository = Mockito.mock(ProfileRepository::class.java)
+            whenever(profileRepository.profileState).thenReturn(flowOf(ProfileState.Restored(profile)))
+
+            val createAccountWithLedgerFactorSourceUseCase = CreateAccountWithLedgerFactorSourceUseCase(
+                profileRepository = profileRepository,
+                resolveAccountsLedgerStateRepository = resolveAccountsLedgerStateRepository,
+                testDispatcher
+            )
+            val derivationPath = DerivationPath.forAccount(
+                networkId = network.networkId(),
+                accountIndex = profile.nextAccountIndex(
+                    derivationPathScheme = DerivationPathScheme.CAP_26,
+                    forNetworkId = network.networkId(),
+                    factorSourceID = TestData.ledgerFactorSource.id
+                ),
+                keyType = KeyType.TRANSACTION_SIGNING
+            )
+            val account = createAccountWithLedgerFactorSourceUseCase(
+                displayName = accountName,
+                derivedPublicKeyHex = "7229e3b98ffa35a4ce28b891ff0a9f95c9d959eff58d0e61015fab3a3b2d18f9",
                 derivationPath = derivationPath,
-                appearanceID = totalAccountsOnNetwork % AccountGradientList.count()
+                ledgerFactorSourceID = TestData.ledgerFactorSource.id
             )
-            val resolveResult = resolveAccountsLedgerStateRepository.invoke(listOf(newAccount))
-            // Add account to the profile
-            val accountToAdd = if (resolveResult.isSuccess) {
-                resolveResult.getOrThrow().first().account
-            } else {
-                newAccount
-            }
+
             val updatedProfile = profile.addAccounts(
-                accounts = listOf(accountToAdd),
-                onNetwork = networkId
+                accounts = listOf(account),
+                onNetwork = network.networkId()
             )
-            // Save updated profile
-            profileRepository.saveProfile(updatedProfile)
-            // Return new account
-            newAccount
+
+            verify(profileRepository).saveProfile(updatedProfile)
         }
     }
 }
