0affa9424b022ac01bda51a596d77f82139ad0c2
raf-rdx <108684750+raf-rdx@users.noreply.github.com>
2024-01-04 09:54:33 +0100

    ABW-2526 - Navigation bar padding added to fix Android 14 issue. (#728)

--- app/src/main/java/com/babylon/wallet/android/presentation/account/AccountScreen.kt
+++ app/src/main/java/com/babylon/wallet/android/presentation/account/AccountScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
@@ -20,8 +21,8 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.Icon
-import androidx.compose.material.Surface
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
 import androidx.compose.material.pullrefresh.PullRefreshIndicator
 import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.material.pullrefresh.rememberPullRefreshState
@@ -175,6 +176,7 @@ private fun AccountScreenContent(
     Box(
         modifier = modifier
             .pullRefresh(pullToRefreshState)
+            .navigationBarsPadding()
             .background(Brush.horizontalGradient(gradient))
             .statusBarsPadding()
     ) {
