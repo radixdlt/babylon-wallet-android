502bc22e1eaee4fdbb69422427eb527e6332971e
Rafal Wesolowski <rafal.wesolowski@rdx.works>
2024-01-08 12:22:39 +0100

    ABW-2691 - Fix for bottom sheet title added.

--- app/src/main/java/com/babylon/wallet/android/presentation/settings/authorizeddapps/dappdetail/UnknownDAppComponentsSheetContent.kt
+++ app/src/main/java/com/babylon/wallet/android/presentation/settings/authorizeddapps/dappdetail/UnknownDAppComponentsSheetContent.kt
@@ -1,5 +1,6 @@
 package com.babylon.wallet.android.presentation.settings.authorizeddapps.dappdetail
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -12,12 +13,10 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -26,8 +25,7 @@ import com.babylon.wallet.android.R
 import com.babylon.wallet.android.designsystem.theme.RadixTheme
 import com.babylon.wallet.android.designsystem.theme.RadixWalletTheme
 import com.babylon.wallet.android.presentation.ui.composables.ActionableAddressView
-import com.babylon.wallet.android.presentation.ui.composables.BackIconType
-import com.babylon.wallet.android.presentation.ui.composables.RadixCenteredTopAppBar
+import com.babylon.wallet.android.presentation.ui.composables.BottomDialogDragHandle
 import com.babylon.wallet.android.presentation.ui.composables.Thumbnail
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.persistentListOf
@@ -40,11 +38,13 @@ fun UnknownDAppComponentsSheetContent(
 ) {
     Column(modifier = modifier) {
         val title = stringResource(id = R.string.transactionReview_unknownComponents, unknownDAppComponents.size)
-        RadixCenteredTopAppBar(
+        BottomDialogDragHandle(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(RadixTheme.colors.defaultBackground, shape = RadixTheme.shapes.roundedRectTopDefault)
+                .padding(vertical = RadixTheme.dimensions.paddingSmall),
             title = title,
-            onBackClick = onBackClick,
-            contentColor = RadixTheme.colors.gray1,
-            backIconType = BackIconType.Close
+            onDismissRequest = onBackClick
         )
         HorizontalDivider(color = RadixTheme.colors.gray5)
         val lastItem = unknownDAppComponents.last()
@@ -84,12 +84,6 @@ fun UnknownDAppComponentsSheetContent(
                                 textColor = RadixTheme.colors.gray1
                             )
                         }
-                        Spacer(modifier = Modifier.weight(1f))
-                        Icon(
-                            painter = painterResource(id = com.babylon.wallet.android.designsystem.R.drawable.ic_link_out),
-                            tint = RadixTheme.colors.gray2,
-                            contentDescription = null
-                        )
                     }
 
                     if (lastItem != unknownComponentAddress) {
