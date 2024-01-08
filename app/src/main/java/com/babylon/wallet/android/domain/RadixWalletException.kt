4ff174b90e85c938de3ee2244462eccebe9c7e5c
micbakos-rdx <michael.bakogiannis@rdx.works>
2024-01-08 14:30:57 +0200

    Add rate limit error message

--- app/src/main/java/com/babylon/wallet/android/domain/RadixWalletException.kt
+++ app/src/main/java/com/babylon/wallet/android/domain/RadixWalletException.kt
@@ -10,6 +10,22 @@ import rdx.works.profile.data.model.apppreferences.Radix
 import rdx.works.profile.domain.ProfileException
 
 sealed class RadixWalletException(cause: Throwable? = null) : Throwable(cause = cause) {
+
+    sealed class GatewayException(cause: Throwable? = null): RadixWalletException(cause) {
+
+        data class ClientError(override val cause: Throwable? = null): GatewayException(cause)
+        data class ServiceError(
+            val code: Int?,
+            override val message: String
+        ): GatewayException(null) {
+
+            companion object {
+                const val RATE_LIMIT_REACHED = 429
+            }
+
+        }
+    }
+
     data object DappMetadataEmpty : RadixWalletException()
     data object SignatureCancelled : RadixWalletException()
     data object FailedToCollectLedgerSignature : RadixWalletException()
@@ -345,6 +361,11 @@ fun RadixWalletException.toUserFriendlyMessage(context: Context): String {
         is RadixWalletException.LedgerCommunicationException -> toUserFriendlyMessage(context)
         is RadixWalletException.PrepareTransactionException -> toUserFriendlyMessage(context)
         is RadixWalletException.TransactionSubmitException -> toUserFriendlyMessage(context)
+        is RadixWalletException.GatewayException.ClientError -> cause?.message.orEmpty()
+        is RadixWalletException.GatewayException.ServiceError -> when (code) {
+            RadixWalletException.GatewayException.ServiceError.RATE_LIMIT_REACHED -> context.getString(R.string.common_rateLimitReached)
+            else -> message
+        }
     }
 }
 
