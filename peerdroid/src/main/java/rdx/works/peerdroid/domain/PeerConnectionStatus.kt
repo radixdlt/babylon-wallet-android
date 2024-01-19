a4d0df0ac1ef3d8bcf22b40449c16d63013efeef
giannis.tsepas <giannis.tsepas@rdx.works>
2024-01-19 17:54:07 +0200

    added PeerConnectionStatus in PeerdroidConnector that informs the client for the status of each available peer connection

---
+++ peerdroid/src/main/java/rdx/works/peerdroid/domain/PeerConnectionStatus.kt
@@ -0,0 +1,6 @@
+package rdx.works.peerdroid.domain
+
+// it indicates the PeerConnection state that can be used as a simple indicator in the UI
+enum class PeerConnectionStatus {
+    OPEN, CLOSED, CONNECTING
+}
