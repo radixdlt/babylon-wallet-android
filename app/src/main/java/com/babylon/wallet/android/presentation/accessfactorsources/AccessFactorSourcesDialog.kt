e44f2800f99ed6c927c115324831aa23d2b27d5c
giannis.tsepas <giannis.tsepas@rdx.works>
2024-01-23 11:30:49 +0200

    implemented logic for AccessFactorSourceBottomSheet and its viewmodel

--- app/src/main/java/com/babylon/wallet/android/presentation/accessfactorsources/AccessFactorSourcesDialog.kt
+++ app/src/main/java/com/babylon/wallet/android/presentation/accessfactorsources/AccessFactorSourcesDialog.kt
@@ -1,21 +1,35 @@
 package com.babylon.wallet.android.presentation.accessfactorsources
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import com.babylon.wallet.android.R
 import com.babylon.wallet.android.designsystem.theme.RadixTheme
+import com.babylon.wallet.android.presentation.accessfactorsource.AccessFactorSourceViewModel.AccessFactorSourceUiState
 import com.babylon.wallet.android.presentation.ui.composables.BottomSheetDialogWrapper
+import com.babylon.wallet.android.presentation.ui.composables.LedgerListItem
 import com.babylon.wallet.android.utils.biometricAuthenticate
+import com.babylon.wallet.android.utils.formattedSpans
 
 @Composable
 fun AccessFactorSourcesDialog(
@@ -23,6 +37,7 @@ fun AccessFactorSourcesDialog(
     viewModel: AccessFactorSourcesViewModel,
     onDismiss: () -> Unit
 ) {
+    val state by viewModel.state.collectAsState()
     val context = LocalContext.current
 
     LaunchedEffect(Unit) {
@@ -36,6 +51,9 @@ fun AccessFactorSourcesDialog(
 
     AccessFactorSourceBottomSheetContent(
         modifier = modifier,
+        isAccessingFactorSourceInProgress = state.isAccessingFactorSourceInProgress,
+        isAccessingFactorSourceCompleted = state.isAccessingFactorSourceCompleted,
+        showContentForFactorSource = state.showContentFor,
         onDismiss = onDismiss
     )
 }
@@ -43,8 +61,15 @@ fun AccessFactorSourcesDialog(
 @Composable
 private fun AccessFactorSourceBottomSheetContent(
     modifier: Modifier = Modifier,
+    isAccessingFactorSourceInProgress: Boolean,
+    isAccessingFactorSourceCompleted: Boolean,
+    showContentForFactorSource: AccessFactorSourceUiState.ShowContentFor,
     onDismiss: () -> Unit
 ) {
+    if (isAccessingFactorSourceCompleted) {
+        onDismiss()
+    }
+
     BottomSheetDialogWrapper(
         modifier = modifier,
         onDismiss = {
@@ -58,18 +83,50 @@ private fun AccessFactorSourceBottomSheetContent(
                 .background(RadixTheme.colors.defaultBackground),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            Image(
+                painter = painterResource(id = com.babylon.wallet.android.designsystem.R.drawable.ic_create_account),
+                contentDescription = null
+            )
+            Spacer(modifier = Modifier.height(RadixTheme.dimensions.paddingDefault))
             Text(
                 style = RadixTheme.typography.title,
                 text = stringResource(id = R.string.derivePublicKeys_titleCreateAccount)
             )
+            Spacer(modifier = Modifier.height(RadixTheme.dimensions.paddingDefault))
+            when (showContentForFactorSource) {
+                AccessFactorSourceUiState.ShowContentFor.Device -> {
+                    Text(
+                        style = RadixTheme.typography.body1Regular,
+                        text = stringResource(id = R.string.derivePublicKeys_subtitleDevice)
+                    )
+                }
+
+                is AccessFactorSourceUiState.ShowContentFor.Ledger -> {
+                    Text(
+                        style = RadixTheme.typography.body1Regular,
+                        text = stringResource(id = R.string.derivePublicKeys_subtitleLedger)
+                            .formattedSpans(SpanStyle(fontWeight = FontWeight.Bold))
+                    )
+                    Spacer(modifier = Modifier.height(RadixTheme.dimensions.paddingLarge))
+                    LedgerListItem(
+                        ledgerFactorSource = showContentForFactorSource.selectedLedgerDevice,
+                        modifier = Modifier
+                            .shadow(elevation = 4.dp, shape = RadixTheme.shapes.roundedRectSmall)
+                            .fillMaxWidth()
+                            .background(RadixTheme.colors.defaultBackground, shape = RadixTheme.shapes.roundedRectSmall)
+                            .padding(RadixTheme.dimensions.paddingLarge),
+
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.height(RadixTheme.dimensions.paddingXLarge))
+            if (isAccessingFactorSourceInProgress) {
+                CircularProgressIndicator(
+                    modifier = Modifier.size(24.dp),
+                    color = RadixTheme.colors.gray1
+                )
+            }
+            Spacer(Modifier.height(70.dp))
         }
     }
-}
-
-@Preview
-@Composable
-fun AccessFactorSourcesDialogPreview() {
-    AccessFactorSourceBottomSheetContent(
-        onDismiss = {}
-    )
 }
--- app/src/main/java/com/babylon/wallet/android/presentation/accessfactorsources/AccessFactorSourcesViewModel.kt
+++ app/src/main/java/com/babylon/wallet/android/presentation/accessfactorsources/AccessFactorSourcesViewModel.kt
@@ -1,26 +1,154 @@
 package com.babylon.wallet.android.presentation.accessfactorsources
 
+import androidx.lifecycle.viewModelScope
+import com.babylon.wallet.android.data.dapp.LedgerMessenger
+import com.babylon.wallet.android.data.dapp.model.Curve
+import com.babylon.wallet.android.data.dapp.model.LedgerInteractionRequest
+import com.babylon.wallet.android.presentation.accessfactorsource.AccessFactorSourceOutput.PublicKeyAndDerivationPath
+import com.babylon.wallet.android.presentation.accessfactorsource.AccessFactorSourceViewModel.AccessFactorSourceUiState.ShowContentFor
 import com.babylon.wallet.android.presentation.common.StateViewModel
 import com.babylon.wallet.android.presentation.common.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import rdx.works.core.UUIDGenerator
+import rdx.works.core.decodeHex
+import rdx.works.profile.data.model.extensions.mainBabylonFactorSource
+import rdx.works.profile.data.model.factorsources.DeviceFactorSource
+import rdx.works.profile.data.model.factorsources.LedgerHardwareWalletFactorSource
+import rdx.works.profile.data.repository.AccessFactorSourceProvider
+import rdx.works.profile.derivation.model.NetworkId
+import rdx.works.profile.domain.EnsureBabylonFactorSourceExistUseCase
 import javax.inject.Inject
 
 @HiltViewModel
-class AccessFactorSourcesViewModel @Inject constructor() : StateViewModel<AccessFactorSourcesViewModel.AccessFactorSourceUiState>() {
+class AccessFactorSourcesViewModel @Inject constructor(
+    private val accessFactorSourceProvider: AccessFactorSourceProvider,
+    private val accessFactorSourceUiProxy: AccessFactorSourceUiProxy,
+    private val ensureBabylonFactorSourceExistUseCase: EnsureBabylonFactorSourceExistUseCase,
+    private val ledgerMessenger: LedgerMessenger
+) : StateViewModel<AccessFactorSourcesViewModel.AccessFactorSourceUiState>() {
 
     override fun initialState(): AccessFactorSourceUiState = AccessFactorSourceUiState()
 
-    @Suppress("UnusedParameter") // will be used later
     fun biometricAuthenticationCompleted(isAuthenticated: Boolean) {
-        // todo ...
+        viewModelScope.launch {
+            if (isAuthenticated) {
+                _state.update { uiState ->
+                    uiState.copy(isAccessingFactorSourceInProgress = true)
+                }
+                when (val input = accessFactorSourceUiProxy.getInput()) {
+                    AccessFactorSourceInput.Init -> { /* do nothing */ }
+
+                    is AccessFactorSourceInput.ToCreateAccount -> {
+                        derivePublicKey(input)
+                    }
+
+                    is AccessFactorSourceInput.ToSign -> { /* TBD */ }
+                }
+
+                _state.update { uiState ->
+                    uiState.copy(
+                        isAccessingFactorSourceInProgress = false,
+                        isAccessingFactorSourceCompleted = true
+                    )
+                }
+            } else {
+                biometricAuthenticationDismissed()
+            }
+        }
     }
 
-    @Suppress("UnusedPrivateMember") // will be used later
+    private suspend fun derivePublicKey(input: AccessFactorSourceInput.ToCreateAccount) {
+        if (input.factorSource == null) { // device factor source
+            val profile = ensureBabylonFactorSourceExistUseCase()
+            val deviceFactorSource = profile.mainBabylonFactorSource() ?: error("Babylon factor source is not present")
+            derivePublicKeyFromDeviceFactorSource(
+                forNetworkId = input.forNetworkId,
+                deviceFactorSource = deviceFactorSource
+            )
+        } else { // ledger factor source
+            val ledgerFactorSource = input.factorSource as LedgerHardwareWalletFactorSource
+            _state.update { uiState ->
+                uiState.copy(
+                    showContentFor = ShowContentFor.Ledger(selectedLedgerDevice = ledgerFactorSource)
+                )
+            }
+            derivePublicKeyFromLedgerFactorSource(
+                forNetworkId = input.forNetworkId,
+                ledgerFactorSource = ledgerFactorSource
+            )
+        }
+    }
+
+    private suspend fun derivePublicKeyFromDeviceFactorSource(
+        forNetworkId: NetworkId,
+        deviceFactorSource: DeviceFactorSource
+    ) {
+        val derivationPath = accessFactorSourceProvider.getDerivationPathForFactorSource(
+            forNetworkId = forNetworkId,
+            factorSource = deviceFactorSource
+        )
+        val compressedPublicKey = accessFactorSourceProvider.derivePublicKeyForDeviceFactorSource(
+            deviceFactorSource = deviceFactorSource,
+            derivationPath = derivationPath
+        )
+        val output = PublicKeyAndDerivationPath(
+            compressedPublicKey = compressedPublicKey,
+            derivationPath = derivationPath
+        )
+
+        accessFactorSourceUiProxy.setOutput(output)
+    }
+
+    private suspend fun derivePublicKeyFromLedgerFactorSource(
+        forNetworkId: NetworkId,
+        ledgerFactorSource: LedgerHardwareWalletFactorSource
+    ) {
+        val derivationPath = accessFactorSourceProvider.getDerivationPathForFactorSource(
+            forNetworkId = forNetworkId,
+            factorSource = ledgerFactorSource
+        )
+        val derivePublicKeyResponse = ledgerMessenger.sendDerivePublicKeyRequest(
+            interactionId = UUIDGenerator.uuid().toString(),
+            keyParameters = listOf(LedgerInteractionRequest.KeyParameters(Curve.Curve25519, derivationPath.path)),
+            ledgerDevice = LedgerInteractionRequest.LedgerDevice.from(ledgerFactorSource = ledgerFactorSource)
+        )
+
+        if (derivePublicKeyResponse.isSuccess) {
+            val publicKeyResponse = derivePublicKeyResponse.getOrNull()
+            if (publicKeyResponse == null) {
+                accessFactorSourceUiProxy.setOutput(null)
+                return
+            }
+
+            val publicKey = publicKeyResponse.publicKeysHex.first().publicKeyHex.decodeHex() // TODO CHECK ABOUT IT
+            val publicKeyAndDerivationPath = PublicKeyAndDerivationPath(
+                compressedPublicKey = publicKey,
+                derivationPath = derivationPath
+            )
+            accessFactorSourceUiProxy.setOutput(publicKeyAndDerivationPath)
+        } else {
+            accessFactorSourceUiProxy.setOutput(null)
+        }
+    }
+
     private fun biometricAuthenticationDismissed() {
-        // todo ...
+        viewModelScope.launch {
+            accessFactorSourceUiProxy.setOutput(null)
+            accessFactorSourceUiProxy.reset()
+        }
     }
 
     data class AccessFactorSourceUiState(
-        val loading: Boolean = false
-    ) : UiState
+        val isAccessingFactorSourceInProgress: Boolean = false,
+        val isAccessingFactorSourceCompleted: Boolean = false,
+        val showContentFor: ShowContentFor = ShowContentFor.Device
+    ) : UiState {
+
+        sealed interface ShowContentFor {
+            data object Device : ShowContentFor
+            data class Ledger(val selectedLedgerDevice: LedgerHardwareWalletFactorSource) : ShowContentFor
+        }
+    }
 }
