c19d81a29541c5453ab014f965e929f1b470da3c
Rafal Wesolowski <rafal.wesolowski@rdx.works>
2024-01-26 15:11:10 +0100

    ABW-2820 - Review your transaction title changed.

--- app/src/main/java/com/babylon/wallet/android/presentation/transaction/composables/TransactionPreviewHeader.kt
+++ app/src/main/java/com/babylon/wallet/android/presentation/transaction/composables/TransactionPreviewHeader.kt
@@ -50,10 +50,8 @@ fun TransactionPreviewHeader(
     scrollState: ScrollState
 ) {
     val context = LocalContext.current
-    val title = when (state.previewType) {
-        is PreviewType.Transfer -> stringResource(R.string.transactionReview_transferTitle)
-        else -> stringResource(R.string.transactionReview_title)
-    }
+    val title = stringResource(R.string.transactionReview_title)
+
     // TODO improve this at later time.
     // AnimationRangePx is the threshold which scrollState.value hits and then transition of the motion layout starts.
     // When its relatively low i.e. 40.dp we start transition early and if content is large enough scroll state continue
