570c5df8864b4416fd376a7ed5c774adec869abb
giannis.tsepas <giannis.tsepas@rdx.works>
2024-02-14 11:41:03 +0200

    updated AccessFactorSourcesDialog UI

--- app/src/main/java/com/babylon/wallet/android/presentation/accessfactorsources/AccessFactorSourcesDialog.kt
+++ app/src/main/java/com/babylon/wallet/android/presentation/accessfactorsources/AccessFactorSourcesDialog.kt
@@ -1,14 +1,16 @@
 package com.babylon.wallet.android.presentation.accessfactorsources
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -16,20 +18,21 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.babylon.wallet.android.R
 import com.babylon.wallet.android.designsystem.theme.RadixTheme
+import com.babylon.wallet.android.designsystem.theme.RadixWalletTheme
 import com.babylon.wallet.android.presentation.accessfactorsources.AccessFactorSourcesViewModel.AccessFactorSourcesUiState
 import com.babylon.wallet.android.presentation.ui.composables.BottomSheetDialogWrapper
-import com.babylon.wallet.android.presentation.ui.composables.LedgerListItem
 import com.babylon.wallet.android.utils.biometricAuthenticate
 import com.babylon.wallet.android.utils.formattedSpans
+import rdx.works.profile.domain.TestData.ledgerFactorSource
 
 @Composable
 fun AccessFactorSourcesDialog(
@@ -89,16 +92,21 @@ private fun AccessFactorSourcesBottomSheetContent(
                 .background(RadixTheme.colors.defaultBackground),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Image(
-                painter = painterResource(id = com.babylon.wallet.android.designsystem.R.drawable.ic_create_account),
-                contentDescription = null
+            Spacer(Modifier.height(40.dp))
+            Icon(
+                modifier = Modifier.size(80.dp),
+                painter = painterResource(
+                    id = com.babylon.wallet.android.designsystem.R.drawable.ic_security_key
+                ),
+                contentDescription = null,
+                tint = RadixTheme.colors.gray3
             )
             Spacer(modifier = Modifier.height(RadixTheme.dimensions.paddingDefault))
             Text(
                 style = RadixTheme.typography.title,
                 text = stringResource(id = R.string.derivePublicKeys_titleCreateAccount)
             )
-            Spacer(modifier = Modifier.height(RadixTheme.dimensions.paddingDefault))
+            Spacer(modifier = Modifier.height(RadixTheme.dimensions.paddingSemiLarge))
             when (showContentForFactorSource) {
                 AccessFactorSourcesUiState.ShowContentFor.Device -> {
                     Text(
@@ -113,16 +121,8 @@ private fun AccessFactorSourcesBottomSheetContent(
                         text = stringResource(id = R.string.derivePublicKeys_subtitleLedger)
                             .formattedSpans(SpanStyle(fontWeight = FontWeight.Bold))
                     )
-                    Spacer(modifier = Modifier.height(RadixTheme.dimensions.paddingLarge))
-                    LedgerListItem(
-                        ledgerFactorSource = showContentForFactorSource.selectedLedgerDevice,
-                        modifier = Modifier
-                            .shadow(elevation = 4.dp, shape = RadixTheme.shapes.roundedRectSmall)
-                            .fillMaxWidth()
-                            .background(RadixTheme.colors.defaultBackground, shape = RadixTheme.shapes.roundedRectSmall)
-                            .padding(RadixTheme.dimensions.paddingLarge),
-
-                    )
+                    Spacer(modifier = Modifier.height(RadixTheme.dimensions.paddingXXLarge))
+                    RoundLedgerItem(ledgerName = ledgerFactorSource.hint.name)
                 }
             }
             Spacer(modifier = Modifier.height(RadixTheme.dimensions.paddingXLarge))
@@ -132,7 +132,59 @@ private fun AccessFactorSourcesBottomSheetContent(
                     color = RadixTheme.colors.gray1
                 )
             }
-            Spacer(Modifier.height(70.dp))
+            Spacer(Modifier.height(120.dp))
         }
+    }
+}
+
+@Composable
+private fun RoundLedgerItem(ledgerName: String) {
+    Row(
+        modifier = Modifier
+            .background(RadixTheme.colors.gray5, RadixTheme.shapes.circle)
+            .padding(RadixTheme.dimensions.paddingDefault),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(RadixTheme.dimensions.paddingSmall)
+    ) {
+        Icon(
+            painter = painterResource(
+                id = com.babylon.wallet.android.designsystem.R.drawable.ic_security_key
+            ),
+            contentDescription = null,
+            tint = RadixTheme.colors.gray3
+        )
+        Text(
+            text = ledgerName,
+            style = RadixTheme.typography.secondaryHeader,
+            color = RadixTheme.colors.gray1
+        )
+    }
+}
+
+@Preview(showBackground = false)
+@Composable
+fun AccessFactorSourcesDialogDevicePreview() {
+    RadixWalletTheme {
+        AccessFactorSourcesBottomSheetContent(
+            isAccessingFactorSourceInProgress = false,
+            isAccessingFactorSourceCompleted = false,
+            showContentForFactorSource = AccessFactorSourcesUiState.ShowContentFor.Device,
+            onDismiss = {}
+        )
+    }
+}
+
+@Preview(showBackground = false)
+@Composable
+fun AccessFactorSourcesDialogLedgerPreview() {
+    RadixWalletTheme {
+        AccessFactorSourcesBottomSheetContent(
+            isAccessingFactorSourceInProgress = false,
+            isAccessingFactorSourceCompleted = false,
+            showContentForFactorSource = AccessFactorSourcesUiState.ShowContentFor.Ledger(
+                selectedLedgerDevice = ledgerFactorSource
+            ),
+            onDismiss = {}
+        )
     }
 }
