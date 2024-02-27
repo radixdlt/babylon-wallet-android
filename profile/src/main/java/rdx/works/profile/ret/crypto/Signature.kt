b6e2feb2c87500232dc5c27fde0e285ee1780472
micbakos-rdx <michael.bakogiannis@rdx.works>
2024-02-27 13:02:14 +0200

    Wrap public key and signature

---
+++ profile/src/main/java/rdx/works/profile/ret/crypto/Signature.kt
@@ -0,0 +1,55 @@
+package rdx.works.profile.ret.crypto
+
+
+private typealias EngineSignatureEd25519 = com.radixdlt.ret.Signature.Ed25519
+private typealias EngineSignatureSecp256k1 = com.radixdlt.ret.Signature.Secp256k1
+
+interface Signature {
+
+    class Ed25519(
+        value: ByteArray
+    ): Signature {
+
+        private val signature = EngineSignatureEd25519(value)
+
+        val value: ByteArray
+            get() = signature.value
+
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+
+            other as Ed25519
+
+            return signature == other.signature
+        }
+
+        override fun hashCode(): Int {
+            return signature.hashCode()
+        }
+    }
+
+    class Secp256k1(
+        value: ByteArray
+    ): Signature {
+
+        private val signature = EngineSignatureSecp256k1(value)
+
+        val value: ByteArray
+            get() = signature.value
+
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+
+            other as Secp256k1
+
+            return signature == other.signature
+        }
+
+        override fun hashCode(): Int {
+            return signature.hashCode()
+        }
+    }
+
+}
