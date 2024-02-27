7d48ebae2ec58e154c8cf0f434824a5969213d8d
micbakos-rdx <michael.bakogiannis@rdx.works>
2024-02-27 13:39:03 +0200

    Make private key to also return the wrapped objects

--- profile/src/main/java/rdx/works/profile/ret/crypto/Signature.kt
+++ profile/src/main/java/rdx/works/profile/ret/crypto/Signature.kt
@@ -1,8 +1,8 @@
 package rdx.works.profile.ret.crypto
 
-
-private typealias EngineSignatureEd25519 = com.radixdlt.ret.Signature.Ed25519
-private typealias EngineSignatureSecp256k1 = com.radixdlt.ret.Signature.Secp256k1
+internal typealias EngineSignature = com.radixdlt.ret.Signature
+internal typealias EngineSignatureEd25519 = com.radixdlt.ret.Signature.Ed25519
+internal typealias EngineSignatureSecp256k1 = com.radixdlt.ret.Signature.Secp256k1
 
 interface Signature {
 
