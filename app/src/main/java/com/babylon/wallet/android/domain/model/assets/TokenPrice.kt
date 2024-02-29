afe6a194eb464065af9de6194f978a1e5b8e03fe
giannis.tsepas <giannis.tsepas@rdx.works>
2024-02-29 21:21:11 +0200

    added TokenPrice domain model

---
+++ app/src/main/java/com/babylon/wallet/android/domain/model/assets/TokenPrice.kt
@@ -0,0 +1,9 @@
+package com.babylon.wallet.android.domain.model.assets
+
+import java.math.BigDecimal
+
+data class TokenPrice(
+    val resourceAddress: String,
+    val price: BigDecimal,
+    val currency: String
+)
