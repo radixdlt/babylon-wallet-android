8a1040bdb33249f6d14da8fe74c480459b0c2e1e
giannis.tsepas <giannis.tsepas@rdx.works>
2024-03-01 18:32:06 +0200

    added serial names

--- app/src/main/java/com/babylon/wallet/android/data/gateway/model/TokensAndLsusPricesRequest.kt
+++ app/src/main/java/com/babylon/wallet/android/data/gateway/model/TokensAndLsusPricesRequest.kt
@@ -1,10 +1,14 @@
 package com.babylon.wallet.android.data.gateway.model
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class TokensAndLsusPricesRequest(
+    @SerialName("currency")
     val currency: String,
+    @SerialName("lsus")
     val lsus: List<String>,
+    @SerialName("tokens")
     val tokens: List<String>
 )
