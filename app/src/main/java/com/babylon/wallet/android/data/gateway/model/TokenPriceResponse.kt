ba8a43477839561092b8df1382479b37fd88a1f8
giannis.tsepas <giannis.tsepas@rdx.works>
2024-03-01 18:32:06 +0200

    added serial names

--- app/src/main/java/com/babylon/wallet/android/data/gateway/model/TokenPriceResponse.kt
+++ app/src/main/java/com/babylon/wallet/android/data/gateway/model/TokenPriceResponse.kt
@@ -7,18 +7,23 @@ import rdx.works.core.InstantGenerator
 
 @Serializable
 data class TokenPriceResponse(
+    @SerialName(value = "id")
     val id: String,
     @SerialName(value = "resource_address")
     val resourceAddress: String,
+    @SerialName(value = "symbol")
     val symbol: String,
+    @SerialName(value = "name")
     val name: String,
+    @SerialName(value = "price")
     val price: Double,
+    @SerialName(value = "currency")
     val currency: String
 ) {
 
     companion object {
 
-        fun List<TokenPriceResponse>.asEntity(): List<TokenPriceEntity> {
+        fun List<TokenPriceResponse>.asEntities(): List<TokenPriceEntity> {
             return map { tokenPrice ->
                 TokenPriceEntity(
                     resourceAddress = tokenPrice.resourceAddress,
