72d0edb1be92e46a3c1cd081462b1ec7b7abd1a7
micbakos-rdx <michael.bakogiannis@rdx.works>
2024-03-14 18:53:41 +0200

    Remove unnecessary bigdecimal currency formatter

--- app/src/main/java/com/babylon/wallet/android/utils/BigDecimalExtensions.kt
+++ app/src/main/java/com/babylon/wallet/android/utils/BigDecimalExtensions.kt
@@ -4,12 +4,5 @@ package com.babylon.wallet.android.utils
 
 import java.math.BigDecimal
 import java.math.RoundingMode
-import java.text.NumberFormat
-import java.util.Locale
 
 fun BigDecimal.toRETDecimalString(): String = setScale(18, RoundingMode.HALF_UP).toPlainString()
-
-fun BigDecimal.toLocaleNumberFormat(): String {
-    val numberFormat: NumberFormat = NumberFormat.getNumberInstance(Locale.getDefault())
-    return numberFormat.format(this)
-}
