b798c898b46a11dddb425c95b01055ea05a82ad0
giannis.tsepas <giannis.tsepas@rdx.works>
2024-03-15 20:14:56 +0200

    improvement in StakingTab and AssetsViewData

--- app/src/main/java/com/babylon/wallet/android/presentation/ui/composables/assets/StakingTab.kt
+++ app/src/main/java/com/babylon/wallet/android/presentation/ui/composables/assets/StakingTab.kt
@@ -138,17 +138,7 @@ private fun StakingSummary(
         }
 
         val oneXrdPrice = remember(assetsViewData) {
-            val oneXrdPrice = (
-                assetsViewData.prices?.values?.find {
-                    (it as? AssetPrice.LSUPrice)?.oneXrdPrice != null
-                } as? AssetPrice.LSUPrice
-                )?.oneXrdPrice
-            oneXrdPrice
-                ?: (
-                    assetsViewData.prices?.values?.find {
-                        (it as? AssetPrice.StakeClaimPrice)?.oneXrdPrice != null
-                    } as? AssetPrice.StakeClaimPrice
-                    )?.oneXrdPrice
+            assetsViewData.oneXrdPrice
         }
 
         val stakedFiatPrice = remember(stakeSummary, oneXrdPrice) {
