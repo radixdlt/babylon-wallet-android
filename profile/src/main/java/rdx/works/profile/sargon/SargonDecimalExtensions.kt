3f432c3466a5405f56c123cb70ec95750d1d00b8
micbakos-rdx <michael.bakogiannis@rdx.works>
2024-03-26 15:54:02 +0200

    Modify manifest with lock fee using sargon

--- profile/src/main/java/rdx/works/profile/sargon/SargonDecimalExtensions.kt
+++ profile/src/main/java/rdx/works/profile/sargon/SargonDecimalExtensions.kt
@@ -1,8 +1,13 @@
 package rdx.works.profile.sargon
 
+import com.radixdlt.sargon.CommonException
 import com.radixdlt.sargon.Decimal192
 import com.radixdlt.sargon.extensions.toDecimal192
 import java.math.BigDecimal
 
 // Temporary until all values are replaced with Decimal192
-fun BigDecimal.toDecimal192(): Decimal192 = toPlainString().toDecimal192()
+fun BigDecimal.toDecimal192(): Decimal192 = try {
+    toPlainString().toDecimal192()
+} catch (exception: CommonException.DecimalException) {
+    toFloat().toDecimal192()
+}
