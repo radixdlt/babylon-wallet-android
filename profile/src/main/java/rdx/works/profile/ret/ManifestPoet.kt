f1c25b25702682fdc6ca630701562122f546ae8d
micbakos-rdx <michael.bakogiannis@rdx.works>
2024-03-26 13:25:37 +0200

    Build claim manifest through sargon

--- profile/src/main/java/rdx/works/profile/ret/ManifestPoet.kt
+++ profile/src/main/java/rdx/works/profile/ret/ManifestPoet.kt
@@ -5,12 +5,9 @@ import com.radixdlt.ret.Decimal
 import com.radixdlt.ret.Instruction
 import com.radixdlt.ret.Instructions
 import com.radixdlt.ret.ManifestAddress
-import com.radixdlt.ret.ManifestBuilder
-import com.radixdlt.ret.ManifestBuilderBucket
 import com.radixdlt.ret.ManifestValue
 import com.radixdlt.ret.TransactionHeader
 import com.radixdlt.ret.TransactionManifest
-import com.radixdlt.ret.nonFungibleLocalIdFromStr
 import rdx.works.core.AddressHelper
 import rdx.works.profile.data.model.pernetwork.FactorInstance
 import rdx.works.profile.data.model.pernetwork.Network
@@ -26,49 +23,6 @@ object ManifestPoet {
     ) = BabylonManifestBuilder()
         .setOwnerKeys(entityAddress, publicKeyHashes)
         .buildSafely(AddressHelper.networkId(entityAddress))
-
-    fun buildClaim(
-        fromAccount: Network.Account,
-        claims: List<Claim>
-    ): Result<TransactionManifestData> {
-        var builder = ManifestBuilder()
-        var bucketCounter = 0
-
-        claims.forEach { claim ->
-            val claimBucket = ManifestBuilderBucket("bucket$bucketCounter").also {
-                bucketCounter += 1
-            }
-
-            val depositBucket = ManifestBuilderBucket("bucket$bucketCounter").also {
-                bucketCounter += 1
-            }
-
-            val totalClaimValue = claim.claimNFTs.values.sumOf { it }
-            val xrdAddress = AddressHelper.xrdAddress(forNetworkId = fromAccount.networkID)
-
-            builder = builder
-                .accountWithdrawNonFungibles(
-                    address = Address(fromAccount.address),
-                    resourceAddress = Address(claim.resourceAddress),
-                    ids = claim.claimNFTs.keys.map { nonFungibleLocalIdFromStr(it) }
-                ).takeAllFromWorktop(
-                    resourceAddress = Address(claim.resourceAddress),
-                    intoBucket = claimBucket
-                ).validatorClaimXrd(
-                    address = Address(claim.validatorAddress),
-                    bucket = claimBucket
-                ).takeFromWorktop(
-                    resourceAddress = Address(xrdAddress),
-                    amount = totalClaimValue.toRETDecimal(roundingMode = RoundingMode.HALF_DOWN),
-                    intoBucket = depositBucket
-                ).accountDeposit(
-                    address = Address(fromAccount.address),
-                    bucket = depositBucket
-                )
-        }
-
-        return builder.buildSafely(fromAccount.networkID)
-    }
 
     fun buildThirdPartyDeposits(
         settings: ThirdPartyDepositSettings
@@ -110,12 +64,6 @@ object ManifestPoet {
         }
     }.buildSafely(
         AddressHelper.networkId(settings.accountAddress)
-    )
-
-    data class Claim(
-        val resourceAddress: String,
-        val validatorAddress: String,
-        val claimNFTs: Map<String, BigDecimal>
     )
 
     data class ThirdPartyDepositSettings(
