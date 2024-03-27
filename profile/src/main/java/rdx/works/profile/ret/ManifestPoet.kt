4f34058280745757013a068a3c8358c05832b424
micbakos-rdx <michael.bakogiannis@rdx.works>
2024-03-27 10:34:28 +0200

    Modify manifest with guarantees using sargon

--- profile/src/main/java/rdx/works/profile/ret/ManifestPoet.kt
+++ profile/src/main/java/rdx/works/profile/ret/ManifestPoet.kt
@@ -1,16 +1,7 @@
 package rdx.works.profile.ret
 
-import com.radixdlt.ret.Address
-import com.radixdlt.ret.Decimal
-import com.radixdlt.ret.Instruction
-import com.radixdlt.ret.Instructions
-import com.radixdlt.ret.TransactionHeader
-import com.radixdlt.ret.TransactionManifest
 import rdx.works.core.AddressHelper
 import rdx.works.profile.data.model.pernetwork.FactorInstance
-import rdx.works.profile.ret.transaction.TransactionManifestData
-import java.math.BigDecimal
-import java.math.RoundingMode
 
 object ManifestPoet {
 
@@ -20,60 +11,4 @@ object ManifestPoet {
     ) = BabylonManifestBuilder()
         .setOwnerKeys(entityAddress, publicKeyHashes)
         .buildSafely(AddressHelper.networkId(entityAddress))
-}
-
-fun TransactionManifestData.addGuaranteeInstructionToManifest(
-    address: String,
-    guaranteedAmount: BigDecimal,
-    index: Int
-): TransactionManifestData {
-    return TransactionManifestData.from(
-        manifest = TransactionManifest(
-            instructions = Instructions.fromInstructions(
-                instructions = manifest.instructions().instructionsList().toMutableList().apply {
-                    add(
-                        index = index,
-                        element = Instruction.AssertWorktopContains(
-                            resourceAddress = Address(address),
-                            amount = guaranteedAmount.toRETDecimal(roundingMode = RoundingMode.HALF_UP)
-                        )
-                    )
-                }.toList(),
-                networkId = manifest.instructions().networkId()
-            ),
-            blobs = manifest.blobs()
-        ),
-        message = message
-    )
-}
-
-@Suppress("MagicNumber")
-internal fun BigDecimal.toRETDecimal(roundingMode: RoundingMode): Decimal = Decimal(setScale(18, roundingMode).toPlainString())
-internal fun TransactionHeader.toPrettyString(): String = StringBuilder()
-    .appendLine("[Start Epoch]         => $startEpochInclusive")
-    .appendLine("[End Epoch]           => $endEpochExclusive")
-    .appendLine("[Network id]          => $networkId")
-    .appendLine("[Nonce]               => $nonce")
-    .appendLine("[Notary is signatory] => $notaryIsSignatory")
-    .appendLine("[Tip %]               => $tipPercentage")
-    .toString()
-
-internal fun TransactionManifest.toPrettyString(): String {
-    val blobSeparator = "\n"
-    val blobPreamble = "BLOBS\n"
-    val blobLabel = "BLOB\n"
-
-    val instructionsFormatted = instructions().asStr()
-
-    val blobsByByteCount = blobs().mapIndexed { index, bytes ->
-        "$blobLabel[$index]: #${bytes.size} bytes"
-    }.joinToString(blobSeparator)
-
-    val blobsString = if (blobsByByteCount.isNotEmpty()) {
-        listOf(blobPreamble, blobsByByteCount).joinToString(separator = blobSeparator)
-    } else {
-        ""
-    }
-
-    return "$instructionsFormatted$blobsString"
 }
