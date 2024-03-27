2245099d735cd0b4fd97e31fe195a071456750c4
micbakos-rdx <michael.bakogiannis@rdx.works>
2024-03-27 11:36:46 +0200

    Fix detekt issues

--- profile/src/main/java/rdx/works/profile/sargon/SargonDecimalExtensions.kt
+++ profile/src/main/java/rdx/works/profile/sargon/SargonDecimalExtensions.kt
@@ -6,6 +6,7 @@ import com.radixdlt.sargon.extensions.toDecimal192
 import java.math.BigDecimal
 
 // Temporary until all values are replaced with Decimal192
+@Suppress("SwallowedException")
 fun BigDecimal.toDecimal192(): Decimal192 = try {
     toPlainString().toDecimal192()
 } catch (exception: CommonException.DecimalException) {
