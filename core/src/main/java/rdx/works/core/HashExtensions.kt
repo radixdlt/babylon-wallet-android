5a473cb294b99b2d73f1bb5cb5cf5bf1656889f4
micbakos-rdx <michael.bakogiannis@rdx.works>
2024-04-03 15:32:12 +0300

    Remove unused methods

--- core/src/main/java/rdx/works/core/HashExtensions.kt
+++ core/src/main/java/rdx/works/core/HashExtensions.kt
@@ -1,10 +1,6 @@
 package rdx.works.core
 
-import com.radixdlt.hex.extensions.toHexString
 import com.radixdlt.ret.hash
-
-@OptIn(ExperimentalUnsignedTypes::class)
-fun ByteArray.toUByteList() = toUByteArray().toTypedArray().toList()
 
 @OptIn(ExperimentalUnsignedTypes::class)
 fun List<UByte>.toByteArray() = toUByteArray().toByteArray()
@@ -14,9 +10,3 @@ fun ByteArray.blake2Hash(): ByteArray = hash(data = this).bytes()
 fun String.blake2Hash(): ByteArray = toByteArray().blake2Hash()
 
 fun ByteArray.toHexString(): String = joinToString(separator = "") { eachByte -> "%02x".format(eachByte) }
-
-fun String.compressedPublicKeyHash(): String = decodeHex().blake2Hash().takeLast(PUBLIC_KEY_HASH_LENGTH).toHexString()
-
-fun String.compressedPublicKeyHashBytes(): ByteArray = decodeHex().blake2Hash().takeLast(PUBLIC_KEY_HASH_LENGTH).toByteArray()
-
-const val PUBLIC_KEY_HASH_LENGTH = 29
