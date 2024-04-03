18cd7161de74b4501a1c1e20b5bb5c230f77ab8d
micbakos-rdx <michael.bakogiannis@rdx.works>
2024-04-03 15:31:49 +0300

    Use sargon to figure out if any account address was already imported from legacy olympia

--- profile/src/main/java/rdx/works/profile/domain/GetProfileUseCase.kt
+++ profile/src/main/java/rdx/works/profile/domain/GetProfileUseCase.kt
@@ -9,7 +9,6 @@ import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.mapNotNull
-import rdx.works.core.AddressHelper
 import rdx.works.profile.data.model.ProfileState
 import rdx.works.profile.data.model.currentNetwork
 import rdx.works.profile.data.model.extensions.factorSourceId
@@ -132,12 +131,6 @@ suspend fun GetProfileUseCase.accountOnCurrentNetwork(
     withAddress: AccountAddress
 ) = accountsOnCurrentNetwork().firstOrNull { account ->
     account.address == withAddress.string
-}
-
-suspend fun GetProfileUseCase.currentNetworkAccountHashes(): Set<ByteArray> {
-    return accountsOnCurrentNetwork().mapNotNull {
-        AddressHelper.publicKeyHash(it.address)
-    }.toSet()
 }
 
 /**
