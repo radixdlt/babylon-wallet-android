7efa8823b9c9b8548b20a4ddbbaef01ac8810355
giannis.tsepas <giannis.tsepas@rdx.works>
2024-04-11 12:19:46 +0300

    added GoogleAccount presentation model and GoogleSignInManager

---
+++ app/src/main/java/com/babylon/wallet/android/presentation/googlesignin/GoogleAccount.kt
@@ -0,0 +1,9 @@
+package com.babylon.wallet.android.presentation.googlesignin
+
+import android.net.Uri
+
+data class GoogleAccount(
+    val email: String?,
+    val name: String?,
+    val photoUrl: Uri?
+)
