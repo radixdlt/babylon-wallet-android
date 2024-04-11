7501744e8a83cf71573c2f63f3fbad60389cd300
giannis.tsepas <giannis.tsepas@rdx.works>
2024-04-11 12:24:16 +0300

    updated RestoreFromBackupScreen in onboarding flow and replaced old cloud backup with new google sign in flow

--- app/src/main/java/com/babylon/wallet/android/presentation/onboarding/OnboardingViewModel.kt
+++ app/src/main/java/com/babylon/wallet/android/presentation/onboarding/OnboardingViewModel.kt
@@ -1,21 +1,46 @@
 package com.babylon.wallet.android.presentation.onboarding
 
+import android.content.Intent
+import androidx.activity.result.ActivityResult
 import androidx.lifecycle.viewModelScope
 import com.babylon.wallet.android.presentation.common.OneOffEvent
 import com.babylon.wallet.android.presentation.common.OneOffEventHandler
 import com.babylon.wallet.android.presentation.common.OneOffEventHandlerImpl
 import com.babylon.wallet.android.presentation.common.StateViewModel
 import com.babylon.wallet.android.presentation.common.UiState
+import com.babylon.wallet.android.presentation.googlesignin.GoogleSignInManager
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
-class OnboardingViewModel @Inject constructor() :
-    StateViewModel<OnboardingViewModel.OnBoardingUiState>(),
+class OnboardingViewModel @Inject constructor(
+    private val googleSignInManager: GoogleSignInManager
+) : StateViewModel<OnboardingViewModel.OnBoardingUiState>(),
     OneOffEventHandler<OnboardingViewModel.OnBoardingEvent> by OneOffEventHandlerImpl() {
 
     override fun initialState(): OnBoardingUiState = OnBoardingUiState()
+
+    fun turnOnCloudBackup() = viewModelScope.launch {
+        val intent = googleSignInManager.createSignInIntent()
+        sendEvent(OnBoardingEvent.SignInToGoogle(intent))
+    }
+
+    fun handleSignInResult(result: ActivityResult) {
+        viewModelScope.launch {
+            googleSignInManager.handleSignInResult(result)
+                .onSuccess { googleAccount ->
+                    _state.update { it.copy(backupEmail = googleAccount.email ?: "!") }
+                    Timber.d("cloud backup is authorized")
+                }
+                .onFailure { exception ->
+                    _state.update { state -> state.copy(backupEmail = exception.message.orEmpty()) }
+                    Timber.e("cloud backup authorization failed: ${exception.message}")
+                }
+        }
+    }
 
     fun onCreateNewWalletClick() {
         viewModelScope.launch {
@@ -24,10 +49,11 @@ class OnboardingViewModel @Inject constructor() :
     }
 
     data class OnBoardingUiState(
-        val showWarning: Boolean = false
+        val backupEmail: String = ""
     ) : UiState
 
     sealed interface OnBoardingEvent : OneOffEvent {
-        object CreateNewWallet : OnBoardingEvent
+        data object CreateNewWallet : OnBoardingEvent
+        data class SignInToGoogle(val signInIntent: Intent) : OnBoardingEvent
     }
 }
