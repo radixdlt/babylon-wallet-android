396abd2c41071381fa88ed1a28106fd8f8bb46c7
micbakos-rdx <michael.bakogiannis@rdx.works>
2024-04-25 14:22:38 +0300

    Fix AccountSettingsViewModelTest

--- app/src/test/java/com/babylon/wallet/android/presentation/accountsettings/AccountSettingsViewModelTest.kt
+++ app/src/test/java/com/babylon/wallet/android/presentation/accountsettings/AccountSettingsViewModelTest.kt
@@ -1,107 +1,147 @@
 package com.babylon.wallet.android.presentation.accountsettings
 
-//@OptIn(ExperimentalCoroutinesApi::class)
-//internal class AccountSettingsViewModelTest : StateViewModelTest<AccountSettingsViewModel>() {
-//
-//    private val getFreeXrdUseCase = mockk<GetFreeXrdUseCase>()
-//    private val savedStateHandle = mockk<SavedStateHandle>()
-//    private val getProfileUseCase = mockk<GetProfileUseCase>()
-//    private val renameAccountDisplayNameUseCase = mockk<RenameAccountDisplayNameUseCase>()
-//    private val changeEntityVisibilityUseCase = mockk<ChangeEntityVisibilityUseCase>()
-//    private val sampleProfile = sampleDataProvider.sampleProfile()
-//    private val sampleAddress = AccountAddress.init(sampleProfile.currentNetwork!!.accounts.first().address)
-//    private val eventBus = mockk<AppEventBus>()
-//    private val sampleTxId = "txId1"
-//
-//    override fun initVM(): AccountSettingsViewModel {
-//        return AccountSettingsViewModel(
-//            getFreeXrdUseCase,
-//            getProfileUseCase,
-//            renameAccountDisplayNameUseCase,
-//            savedStateHandle,
-//            changeEntityVisibilityUseCase,
-//            TestScope(),
-//            eventBus
-//        )
-//    }
-//
-//    @Before
-//    override fun setUp() {
-//        super.setUp()
-//        every { getFreeXrdUseCase.getFaucetState(any()) } returns flowOf(FaucetState.Available(true))
-//        coEvery { getFreeXrdUseCase(any()) } returns Result.success(sampleTxId)
-//        every { getProfileUseCase() } returns flowOf(sampleDataProvider.sampleProfile())
-//        every { savedStateHandle.get<String>(ARG_ACCOUNT_SETTINGS_ADDRESS) } returns sampleAddress.string
-//        coEvery { changeEntityVisibilityUseCase.hideAccount(any()) } just Runs
-//        coEvery { eventBus.sendEvent(any()) } just Runs
-//    }
-//
-//    @Test
-//    fun `hide account sets proper state and fire one-off event`() = runTest {
-//        val vm = vm.value
-//        advanceUntilIdle()
-//        vm.onHideAccount()
-//        advanceUntilIdle()
-//        coVerify(exactly = 1) { changeEntityVisibilityUseCase.hideAccount(any()) }
-//        vm.oneOffEvent.test {
-//            val item = expectMostRecentItem()
-//            assert(item is Event.AccountHidden)
-//        }
-//    }
-//
-//    @Test
-//    fun `account rename valid & invalid`() = runTest {
-//        val vm = vm.value
-//        advanceUntilIdle()
-//        vm.onRenameAccountNameChange("new valid name")
-//        vm.state.test {
-//            val item = expectMostRecentItem()
-//            assert(item.isNewNameValid)
-//        }
-//        vm.onRenameAccountNameChange("new name very very very very very very very very very very very long")
-//        vm.state.test {
-//            val item = expectMostRecentItem()
-//            assert(item.isNewNameValid.not())
-//            assert(item.isNewNameLengthMoreThanTheMaximum)
-//        }
-//    }
-//
-//    @Test
-//    fun `initial state is correct when free xrd enabled`() = runTest {
-//        val vm = vm.value
-//        advanceUntilIdle()
-//        val state = vm.state.first()
-//        assert(state.faucetState is FaucetState.Available)
-//    }
-//
-//    @Test
-//    fun `initial state is correct when free xrd not enabled`() = runTest {
-//        every { getFreeXrdUseCase.getFaucetState(any()) } returns flow { emit(FaucetState.Available(false)) }
-//        val vm = vm.value
-//        advanceUntilIdle()
-//        val state = vm.state.first()
-//        assert(state.faucetState is FaucetState.Available && !(state.faucetState as FaucetState.Available).isEnabled)
-//    }
-//
-//    @Test
-//    fun `get free xrd success sets proper state`() = runTest {
-//        val vm = vm.value
-//        advanceUntilIdle()
-//        vm.onGetFreeXrdClick()
-//        advanceUntilIdle()
-//        coVerify(exactly = 1) { eventBus.sendEvent(AppEvent.RefreshResourcesNeeded) }
-//        coVerify(exactly = 1) { getFreeXrdUseCase(sampleAddress) }
-//    }
-//
-//    @Test
-//    fun `get free xrd failure sets proper state`() = runTest {
-//        coEvery { getFreeXrdUseCase(any()) } returns Result.failure(Exception())
-//        val vm = vm.value
-//        advanceUntilIdle()
-//        vm.onGetFreeXrdClick()
-//        advanceUntilIdle()
-//        val state = vm.state.first()
-//        coVerify(exactly = 1) { getFreeXrdUseCase(sampleAddress) }
-//        assert(state.error != null)
-//    }
-//}
+import androidx.lifecycle.SavedStateHandle
+import app.cash.turbine.test
+import com.babylon.wallet.android.domain.usecases.FaucetState
+import com.babylon.wallet.android.domain.usecases.GetFreeXrdUseCase
+import com.babylon.wallet.android.presentation.StateViewModelTest
+import com.babylon.wallet.android.presentation.account.settings.ARG_ACCOUNT_SETTINGS_ADDRESS
+import com.babylon.wallet.android.presentation.account.settings.AccountSettingsViewModel
+import com.babylon.wallet.android.presentation.account.settings.Event
+import com.babylon.wallet.android.utils.AppEvent
+import com.babylon.wallet.android.utils.AppEventBus
+import com.radixdlt.sargon.Gateway
+import com.radixdlt.sargon.NetworkId
+import com.radixdlt.sargon.Profile
+import com.radixdlt.sargon.extensions.forNetwork
+import com.radixdlt.sargon.extensions.invoke
+import com.radixdlt.sargon.extensions.string
+import com.radixdlt.sargon.samples.sample
+import io.mockk.Runs
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.every
+import io.mockk.just
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.TestScope
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+import rdx.works.core.sargon.changeGateway
+import rdx.works.core.sargon.currentNetwork
+import rdx.works.core.sargon.unHideAllEntities
+import rdx.works.profile.domain.ChangeEntityVisibilityUseCase
+import rdx.works.profile.domain.GetProfileUseCase
+import rdx.works.profile.domain.account.RenameAccountDisplayNameUseCase
+
+@OptIn(ExperimentalCoroutinesApi::class)
+internal class AccountSettingsViewModelTest : StateViewModelTest<AccountSettingsViewModel>() {
+
+    private val getFreeXrdUseCase = mockk<GetFreeXrdUseCase>()
+    private val savedStateHandle = mockk<SavedStateHandle>()
+    private val getProfileUseCase = mockk<GetProfileUseCase>()
+    private val renameAccountDisplayNameUseCase = mockk<RenameAccountDisplayNameUseCase>()
+    private val changeEntityVisibilityUseCase = mockk<ChangeEntityVisibilityUseCase>()
+    private val sampleProfile = Profile.sample().changeGateway(Gateway.forNetwork(NetworkId.MAINNET)).unHideAllEntities()
+    private val sampleAddress = sampleProfile.currentNetwork!!.accounts().first().address
+    private val eventBus = mockk<AppEventBus>()
+    private val sampleTxId = "txId1"
+
+    override fun initVM(): AccountSettingsViewModel {
+        return AccountSettingsViewModel(
+            getFreeXrdUseCase,
+            getProfileUseCase,
+            renameAccountDisplayNameUseCase,
+            savedStateHandle,
+            changeEntityVisibilityUseCase,
+            TestScope(),
+            eventBus
+        )
+    }
+
+    @Before
+    override fun setUp() {
+        super.setUp()
+        every { getFreeXrdUseCase.getFaucetState(any()) } returns flowOf(FaucetState.Available(true))
+        coEvery { getFreeXrdUseCase(any()) } returns Result.success(sampleTxId)
+        coEvery { getProfileUseCase() } returns sampleProfile
+        every { getProfileUseCase.flow } returns flowOf(sampleProfile)
+        every { savedStateHandle.get<String>(ARG_ACCOUNT_SETTINGS_ADDRESS) } returns sampleAddress.string
+        coEvery { changeEntityVisibilityUseCase.hideAccount(any()) } just Runs
+        coEvery { eventBus.sendEvent(any()) } just Runs
+    }
+
+    @Test
+    fun `hide account sets proper state and fire one-off event`() = runTest {
+        val vm = vm.value
+        advanceUntilIdle()
+        vm.onHideAccount()
+        advanceUntilIdle()
+        coVerify(exactly = 1) { changeEntityVisibilityUseCase.hideAccount(any()) }
+        vm.oneOffEvent.test {
+            val item = expectMostRecentItem()
+            assert(item is Event.AccountHidden)
+        }
+    }
+
+    @Test
+    fun `account rename valid & invalid`() = runTest {
+        val vm = vm.value
+        advanceUntilIdle()
+        vm.onRenameAccountNameChange("new valid name")
+        vm.state.test {
+            val item = expectMostRecentItem()
+            assert(item.isNewNameValid)
+        }
+        vm.onRenameAccountNameChange("new name very very very very very very very very very very very long")
+        vm.state.test {
+            val item = expectMostRecentItem()
+            assert(item.isNewNameValid.not())
+            assert(item.isNewNameLengthMoreThanTheMaximum)
+        }
+    }
+
+    @Test
+    fun `initial state is correct when free xrd enabled`() = runTest {
+        val vm = vm.value
+        advanceUntilIdle()
+        val state = vm.state.first()
+        assert(state.faucetState is FaucetState.Available)
+    }
+
+    @Test
+    fun `initial state is correct when free xrd not enabled`() = runTest {
+        every { getFreeXrdUseCase.getFaucetState(any()) } returns flow { emit(FaucetState.Available(false)) }
+        val vm = vm.value
+        advanceUntilIdle()
+        val state = vm.state.first()
+        assert(state.faucetState is FaucetState.Available && !(state.faucetState as FaucetState.Available).isEnabled)
+    }
+
+    @Test
+    fun `get free xrd success sets proper state`() = runTest {
+        val vm = vm.value
+        advanceUntilIdle()
+        vm.onGetFreeXrdClick()
+        advanceUntilIdle()
+        coVerify(exactly = 1) { eventBus.sendEvent(AppEvent.RefreshResourcesNeeded) }
+        coVerify(exactly = 1) { getFreeXrdUseCase(sampleAddress) }
+    }
+
+    @Test
+    fun `get free xrd failure sets proper state`() = runTest {
+        coEvery { getFreeXrdUseCase(any()) } returns Result.failure(Exception())
+        val vm = vm.value
+        advanceUntilIdle()
+        vm.onGetFreeXrdClick()
+        advanceUntilIdle()
+        val state = vm.state.first()
+        coVerify(exactly = 1) { getFreeXrdUseCase(sampleAddress) }
+        assert(state.error != null)
+    }
+}
--- app/src/test/java/com/babylon/wallet/android/presentation/createaccount/withledger/AddLedgerDeviceViewModelTest.kt
+++ app/src/test/java/com/babylon/wallet/android/presentation/createaccount/withledger/AddLedgerDeviceViewModelTest.kt
@@ -1,78 +1,147 @@
 package com.babylon.wallet.android.presentation.createaccount.withledger
 
-//@OptIn(ExperimentalCoroutinesApi::class)
-//class AddLedgerDeviceViewModelTest : StateViewModelTest<AddLedgerDeviceViewModel>() {
-//
-//    private val firstDeviceId = "5f47ec336e9e7891bff04004c817201e73c097b6b1e1b3a26bc501e0010996f5"
-//
-//    private val getProfileUseCaseMock = mockk<GetProfileUseCase>()
-//    private val ledgerMessengerMock = mockk<LedgerMessenger>()
-//    private val addLedgerFactorSourceUseCaseMock = mockk<AddLedgerFactorSourceUseCase>()
-//
-//    override fun initVM(): AddLedgerDeviceViewModel {
-//        return AddLedgerDeviceViewModel(
-//            getProfileUseCase = getProfileUseCaseMock,
-//            ledgerMessenger = ledgerMessengerMock,
-//            addLedgerFactorSourceUseCase = addLedgerFactorSourceUseCaseMock
-//        )
-//    }
-//
-//    @Before
-//    fun setup() {
-//        coEvery { ledgerMessengerMock.isAnyLinkedConnectorConnected } returns flowOf(true)
-//    }
-//
-//    @Test
-//    fun `sending ledger device info requests call proper method and sets proper state`() = runTest {
-//        coEvery { getProfileUseCaseMock() } returns flowOf(profile())
-//        coEvery { ledgerMessengerMock.sendDeviceInfoRequest(any()) } returns Result.success(
-//            MessageFromDataChannel.LedgerResponse.GetDeviceInfoResponse(
-//                interactionId = "1",
-//                model = MessageFromDataChannel.LedgerResponse.LedgerDeviceModel.NanoS,
-//                deviceId = HexCoded32Bytes(firstDeviceId)
-//            )
-//        )
-//        val vm = vm.value
-//        advanceUntilIdle()
-//        vm.onSendAddLedgerRequestClick()
-//        advanceUntilIdle()
-//        vm.state.test {
-//            val item = expectMostRecentItem()
-//            assert(item.showContent == AddLedgerDeviceUiState.ShowContent.NameLedgerDevice)
-//            assert(item.newConnectedLedgerDevice != null)
-//        }
-//        coVerify(exactly = 1) { ledgerMessengerMock.sendDeviceInfoRequest(any()) }
-//    }
-//
-//    @Test
-//    fun `adding ledger and providing name`() = runTest {
-//        val ledgerDeviceToAdd = (profile().factorSources[1] as LedgerHardwareWalletFactorSource)
-//        coEvery {
-//            addLedgerFactorSourceUseCaseMock(
-//                ledgerId = (profile().factorSources[1] as LedgerHardwareWalletFactorSource).id.body,
-//                model = LedgerHardwareWalletFactorSource.DeviceModel.NANO_S,
-//                name = ledgerDeviceToAdd.hint.name
-//            )
-//        } returns AddLedgerFactorSourceResult.Added(
-//            ledgerFactorSource = ledgerDeviceToAdd
-//        )
-//        coEvery { getProfileUseCaseMock() } returns flowOf(profile())
-//        coEvery { ledgerMessengerMock.sendDeviceInfoRequest(any()) } returns Result.success(
-//            MessageFromDataChannel.LedgerResponse.GetDeviceInfoResponse(
-//                interactionId = "1",
-//                model = MessageFromDataChannel.LedgerResponse.LedgerDeviceModel.NanoS,
-//                deviceId = ledgerDeviceToAdd.id.body
-//            )
-//        )
-//        val vm = vm.value
-//        advanceUntilIdle()
-//        vm.onSendAddLedgerRequestClick()
-//        advanceUntilIdle()
-//        vm.onConfirmLedgerNameClick("My Ledger")
-//        advanceUntilIdle()
-//        vm.state.test {
-//            val item = expectMostRecentItem()
-//            assert(item.showContent == AddLedgerDeviceUiState.ShowContent.AddLedgerDeviceInfo)
-//        }
-//    }
-//}
+import app.cash.turbine.test
+import com.babylon.wallet.android.data.dapp.LedgerMessenger
+import com.babylon.wallet.android.domain.model.MessageFromDataChannel
+import com.babylon.wallet.android.presentation.StateViewModelTest
+import com.babylon.wallet.android.presentation.settings.securitycenter.ledgerhardwarewallets.AddLedgerDeviceUiState
+import com.babylon.wallet.android.presentation.settings.securitycenter.ledgerhardwarewallets.AddLedgerDeviceViewModel
+import com.radixdlt.sargon.Exactly32Bytes
+import com.radixdlt.sargon.FactorSource
+import com.radixdlt.sargon.FactorSourceCommon
+import com.radixdlt.sargon.FactorSourceCryptoParameters
+import com.radixdlt.sargon.FactorSourceId
+import com.radixdlt.sargon.FactorSourceIdFromHash
+import com.radixdlt.sargon.FactorSourceKind
+import com.radixdlt.sargon.FactorSources
+import com.radixdlt.sargon.LedgerHardwareWalletFactorSource
+import com.radixdlt.sargon.LedgerHardwareWalletHint
+import com.radixdlt.sargon.LedgerHardwareWalletModel
+import com.radixdlt.sargon.Profile
+import com.radixdlt.sargon.Timestamp
+import com.radixdlt.sargon.extensions.append
+import com.radixdlt.sargon.extensions.hexToBagOfBytes
+import com.radixdlt.sargon.extensions.init
+import com.radixdlt.sargon.extensions.invoke
+import com.radixdlt.sargon.extensions.kind
+import com.radixdlt.sargon.samples.sample
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.every
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Ignore
+import org.junit.Test
+import rdx.works.core.sargon.babylon
+import rdx.works.core.sargon.getById
+import rdx.works.profile.domain.AddLedgerFactorSourceResult
+import rdx.works.profile.domain.AddLedgerFactorSourceUseCase
+import rdx.works.profile.domain.GetProfileUseCase
+
+@Ignore("TODO integration")
+@OptIn(ExperimentalCoroutinesApi::class)
+class AddLedgerDeviceViewModelTest : StateViewModelTest<AddLedgerDeviceViewModel>() {
+
+    private val firstDeviceId = FactorSourceId.Hash(
+        FactorSourceIdFromHash(
+            FactorSourceKind.LEDGER_HQ_HARDWARE_WALLET, Exactly32Bytes.init(
+                "5f47ec336e9e7891bff04004c817201e73c097b6b1e1b3a26bc501e0010996f5".hexToBagOfBytes()
+            )
+        )
+    )
+    private val profile = Profile.sample().let {
+        val factorSources = FactorSources.init(
+            it.factorSources().filterNot { fs -> fs.kind == FactorSourceKind.LEDGER_HQ_HARDWARE_WALLET }
+        ).append(
+            FactorSource.Ledger(
+                LedgerHardwareWalletFactorSource(
+                    firstDeviceId.value,
+                    FactorSourceCommon(
+                        cryptoParameters = FactorSourceCryptoParameters.babylon,
+                        addedOn = Timestamp.now(),
+                        lastUsedOn = Timestamp.now(),
+                        flags = emptyList()
+                    ),
+                    LedgerHardwareWalletHint("Name", LedgerHardwareWalletModel.NANO_S)
+                )
+            )
+        )
+        it.copy(factorSources = factorSources)
+    }
+
+    private val getProfileUseCaseMock = mockk<GetProfileUseCase>()
+    private val ledgerMessengerMock = mockk<LedgerMessenger>()
+    private val addLedgerFactorSourceUseCaseMock = mockk<AddLedgerFactorSourceUseCase>()
+
+    override fun initVM(): AddLedgerDeviceViewModel {
+        return AddLedgerDeviceViewModel(
+            getProfileUseCase = getProfileUseCaseMock,
+            ledgerMessenger = ledgerMessengerMock,
+            addLedgerFactorSourceUseCase = addLedgerFactorSourceUseCaseMock
+        )
+    }
+
+    @Before
+    fun setup() {
+        coEvery { ledgerMessengerMock.isAnyLinkedConnectorConnected } returns flowOf(true)
+    }
+
+    @Test
+    fun `sending ledger device info requests call proper method and sets proper state`() = runTest {
+        coEvery { getProfileUseCaseMock() } returns profile
+        every { getProfileUseCaseMock.flow } returns flowOf(profile)
+        coEvery { ledgerMessengerMock.sendDeviceInfoRequest(any()) } returns Result.success(
+            MessageFromDataChannel.LedgerResponse.GetDeviceInfoResponse(
+                interactionId = "1",
+                model = MessageFromDataChannel.LedgerResponse.LedgerDeviceModel.NanoS,
+                deviceId = firstDeviceId.value.body
+            )
+        )
+        val vm = vm.value
+        advanceUntilIdle()
+        vm.onSendAddLedgerRequestClick()
+        advanceUntilIdle()
+        vm.state.test {
+            val item = expectMostRecentItem()
+            assert(item.showContent == AddLedgerDeviceUiState.ShowContent.NameLedgerDevice)
+            assert(item.newConnectedLedgerDevice != null)
+        }
+        coVerify(exactly = 1) { ledgerMessengerMock.sendDeviceInfoRequest(any()) }
+    }
+
+    @Test
+    fun `adding ledger and providing name`() = runTest {
+        val ledgerDeviceToAdd = profile.factorSources.getById(firstDeviceId) as FactorSource.Ledger
+        coEvery {
+            addLedgerFactorSourceUseCaseMock(
+                ledgerId = firstDeviceId,
+                model = LedgerHardwareWalletModel.NANO_S,
+                name = "Name"
+            )
+        } returns AddLedgerFactorSourceResult.Added(
+            ledgerFactorSource = ledgerDeviceToAdd
+        )
+        coEvery { getProfileUseCaseMock() } returns profile
+        coEvery { ledgerMessengerMock.sendDeviceInfoRequest(any()) } returns Result.success(
+            MessageFromDataChannel.LedgerResponse.GetDeviceInfoResponse(
+                interactionId = "1",
+                model = MessageFromDataChannel.LedgerResponse.LedgerDeviceModel.NanoS,
+                deviceId = ledgerDeviceToAdd.value.id.body
+            )
+        )
+        val vm = vm.value
+        advanceUntilIdle()
+        vm.onSendAddLedgerRequestClick()
+        advanceUntilIdle()
+        vm.onConfirmLedgerNameClick("My Ledger")
+        advanceUntilIdle()
+        vm.state.test {
+            val item = expectMostRecentItem()
+            assert(item.showContent == AddLedgerDeviceUiState.ShowContent.AddLedgerDeviceInfo)
+        }
+    }
+}
