621a10e016829600b51d10f17d75e30ddbad43d3
micbakos-rdx <michael.bakogiannis@rdx.works>
2024-04-29 15:47:53 +0300

    Fix CreateAccountConfirmationViewModelTest

--- app/src/test/java/com/babylon/wallet/android/presentation/CreateAccountConfirmationViewModelTest.kt
+++ app/src/test/java/com/babylon/wallet/android/presentation/CreateAccountConfirmationViewModelTest.kt
@@ -7,15 +7,19 @@ import com.babylon.wallet.android.presentation.account.createaccount.confirmatio
 import com.babylon.wallet.android.presentation.account.createaccount.confirmation.CreateAccountConfirmationViewModel
 import com.babylon.wallet.android.presentation.account.createaccount.confirmation.CreateAccountRequestSource
 import com.babylon.wallet.android.presentation.navigation.Screen
+import com.radixdlt.sargon.Accounts
 import com.radixdlt.sargon.AppearanceId
 import com.radixdlt.sargon.Gateway
 import com.radixdlt.sargon.NetworkId
 import com.radixdlt.sargon.Profile
+import com.radixdlt.sargon.ProfileNetworks
 import com.radixdlt.sargon.extensions.forNetwork
 import com.radixdlt.sargon.extensions.getBy
+import com.radixdlt.sargon.extensions.init
 import com.radixdlt.sargon.extensions.invoke
 import com.radixdlt.sargon.extensions.string
 import com.radixdlt.sargon.samples.sample
+import io.mockk.coEvery
 import io.mockk.every
 import io.mockk.mockk
 import kotlinx.coroutines.CoroutineScope
@@ -28,19 +32,22 @@ import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert
 import org.junit.Before
-import org.junit.Ignore
 import org.junit.Test
 import rdx.works.core.sargon.changeGateway
 import rdx.works.profile.domain.GetProfileUseCase
 
-@Ignore("TODO Integration")
 @ExperimentalCoroutinesApi
 class CreateAccountConfirmationViewModelTest : StateViewModelTest<CreateAccountConfirmationViewModel>() {
 
     private val savedStateHandle = mockk<SavedStateHandle>()
     private val getProfileUseCase = mockk<GetProfileUseCase>()
 
-    private val profile = Profile.sample().changeGateway(Gateway.forNetwork(NetworkId.MAINNET))
+    private val profile = Profile.sample().changeGateway(Gateway.forNetwork(NetworkId.MAINNET)).let {
+        val mainnetNetwork = it.networks.getBy(NetworkId.MAINNET)!!.let { network ->
+            network.copy(accounts = Accounts.init(network.accounts().take(1)))
+        }
+        it.copy(networks = ProfileNetworks.init(mainnetNetwork))
+    }
     private val account = profile.networks.getBy(NetworkId.MAINNET)?.accounts?.invoke()?.first()!!
 
     @Before
@@ -50,6 +57,7 @@ class CreateAccountConfirmationViewModelTest : StateViewModelTest<CreateAccountC
         every { savedStateHandle.get<String>(Screen.ARG_ACCOUNT_NAME) } returns account.displayName.value
         every { savedStateHandle.get<Boolean>(Screen.ARG_HAS_PROFILE) } returns false
         every { getProfileUseCase.flow } returns flowOf(profile)
+        coEvery { getProfileUseCase() } returns profile
     }
 
     @Test
@@ -72,7 +80,7 @@ class CreateAccountConfirmationViewModelTest : StateViewModelTest<CreateAccountC
             CreateAccountConfirmationViewModel.AccountConfirmationUiState(
                 accountName = account.displayName.value,
                 accountAddress = account.address.string,
-                appearanceId = AppearanceId(1u)
+                appearanceId = AppearanceId(0u)
             ),
             viewModel.state.value
         )
@@ -97,7 +105,7 @@ class CreateAccountConfirmationViewModelTest : StateViewModelTest<CreateAccountC
             CreateAccountConfirmationViewModel.AccountConfirmationUiState(
                 accountName = account.displayName.value,
                 accountAddress = account.address.string,
-                appearanceId = AppearanceId(1u)
+                appearanceId = AppearanceId(0u)
             ),
             viewModel.state.value
         )
