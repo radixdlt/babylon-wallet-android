5d4f16d9a8d25eb10bab77a0c56c2a3970cd21a4
micbakos-rdx <michael.bakogiannis@rdx.works>
2024-04-29 10:55:32 +0300

    Fix detekt issues

--- core/src/main/java/rdx/works/core/sargon/GatewayExtensions.kt
+++ core/src/main/java/rdx/works/core/sargon/GatewayExtensions.kt
@@ -7,25 +7,23 @@ import com.radixdlt.sargon.extensions.mainnet
 val Gateway.Companion.default: Gateway
     get() = Gateway.mainnet
 
+@Suppress("ReturnCount")
 val Gateway.Companion.comparator: Comparator<Gateway>
-    get() = object : Comparator<Gateway> {
-        override fun compare(thisGateway: Gateway?, otherGateway: Gateway?): Int {
-            if (thisGateway == null && otherGateway == null) return 0
-            if (thisGateway != null && otherGateway == null) return -1
-            if (thisGateway == null && otherGateway != null) return 1
+    get() = Comparator { thisGateway, otherGateway ->
+        if (thisGateway == null && otherGateway == null) return@Comparator 0
+        if (thisGateway != null && otherGateway == null) return@Comparator -1
+        if (thisGateway == null && otherGateway != null) return@Comparator 1
 
-            requireNotNull(thisGateway)
-            requireNotNull(otherGateway)
+        requireNotNull(thisGateway)
+        requireNotNull(otherGateway)
 
-            if (thisGateway.isWellKnown && !otherGateway.isWellKnown) return -1
-            if (!thisGateway.isWellKnown && otherGateway.isWellKnown) return 1
+        if (thisGateway.isWellKnown && !otherGateway.isWellKnown) return@Comparator -1
+        if (!thisGateway.isWellKnown && otherGateway.isWellKnown) return@Comparator 1
 
-            val idDifference = thisGateway.network.id.value - otherGateway.network.id.value
-            return if (idDifference == 0u) {
-                thisGateway.network.displayDescription.compareTo(otherGateway.network.displayDescription)
-            } else {
-                idDifference.toInt()
-            }
+        val idDifference = thisGateway.network.id.value - otherGateway.network.id.value
+        if (idDifference == 0u) {
+            thisGateway.network.displayDescription.compareTo(otherGateway.network.displayDescription)
+        } else {
+            idDifference.toInt()
         }
-
     }
