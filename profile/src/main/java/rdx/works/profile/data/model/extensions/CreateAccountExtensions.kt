43c8118e53fc09f5368030d210bd9fc394a54682
Jakub Porzuczek <jakub.porzuczek@rdx.works>
2024-04-30 15:33:15 +0200

    properly set curve of account public key for legacy olympia accounts

--- profile/src/main/java/rdx/works/profile/data/model/extensions/CreateAccountExtensions.kt
+++ profile/src/main/java/rdx/works/profile/data/model/extensions/CreateAccountExtensions.kt
@@ -45,8 +45,13 @@ fun Profile.createAccount(
         ).addressString()
     }
 
+    val curve = if (isForLegacyOlympia) {
+        Slip10Curve.SECP_256K1
+    } else {
+        Slip10Curve.CURVE_25519
+    }
     val unsecuredSecurityState = SecurityState.unsecured(
-        publicKey = FactorInstance.PublicKey(compressedPublicKey.toHexString(), Slip10Curve.CURVE_25519),
+        publicKey = FactorInstance.PublicKey(compressedPublicKey.toHexString(), curve),
         derivationPath = derivationPath,
         factorSourceId = (factorSource as FactorSource).id as FactorSource.FactorSourceID.FromHash
     )
