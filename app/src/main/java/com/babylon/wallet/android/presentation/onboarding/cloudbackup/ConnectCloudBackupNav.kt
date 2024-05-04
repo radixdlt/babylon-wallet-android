9fa0d85cedd127eacf974ff9576e0151c99a588c
giannis.tsepas <giannis.tsepas@rdx.works>
2024-05-04 17:06:59 +0300

    replaced draft CloudBackupInfoScreen with new ConnectCloudBackupScreen

---
+++ app/src/main/java/com/babylon/wallet/android/presentation/onboarding/cloudbackup/ConnectCloudBackupNav.kt
@@ -0,0 +1,36 @@
+package com.babylon.wallet.android.presentation.onboarding.cloudbackup
+
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+
+const val ROUTE_CONNECT_CLOUD_BACKUP = "connect_cloud_backup_screen"
+
+fun NavController.connectCloudBackupScreen() {
+    navigate(ROUTE_CONNECT_CLOUD_BACKUP)
+}
+
+fun NavGraphBuilder.connectCloudBackupScreen(
+    onBackClick: () -> Unit,
+    onContinueToCreateAccount: () -> Unit,
+    onSkipClick: () -> Unit
+) {
+    composable(
+        route = ROUTE_CONNECT_CLOUD_BACKUP,
+        enterTransition = {
+            slideIntoContainer(AnimatedContentTransitionScope.SlideDirection.Up)
+        },
+        exitTransition = {
+            slideOutOfContainer(AnimatedContentTransitionScope.SlideDirection.Down)
+        }
+    ) {
+        ConnectCloudBackupScreen(
+            viewModel = hiltViewModel(),
+            onBackClick = onBackClick,
+            onContinueToCreateAccount = onContinueToCreateAccount,
+            onSkipClick = onSkipClick
+        )
+    }
+}
---
+++ app/src/main/java/com/babylon/wallet/android/presentation/onboarding/cloudbackup/ConnectCloudBackupScreen.kt
@@ -0,0 +1,182 @@
+package com.babylon.wallet.android.presentation.onboarding.cloudbackup
+
+import androidx.activity.compose.BackHandler
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.babylon.wallet.android.designsystem.composable.RadixPrimaryButton
+import com.babylon.wallet.android.designsystem.composable.RadixTextButton
+import com.babylon.wallet.android.designsystem.theme.RadixTheme
+import com.babylon.wallet.android.presentation.common.UiMessage
+import com.babylon.wallet.android.presentation.ui.RadixWalletPreviewTheme
+import com.babylon.wallet.android.presentation.ui.composables.BackIconType
+import com.babylon.wallet.android.presentation.ui.composables.RadixCenteredTopAppBar
+import com.babylon.wallet.android.presentation.ui.composables.RadixSnackbarHost
+import com.babylon.wallet.android.presentation.ui.composables.SnackbarUIMessage
+
+@Composable
+fun ConnectCloudBackupScreen(
+    modifier: Modifier = Modifier,
+    viewModel: ConnectCloudBackupViewModel,
+    onBackClick: () -> Unit,
+    onContinueToCreateAccount: () -> Unit,
+    onSkipClick: () -> Unit
+) {
+    val state by viewModel.state.collectAsStateWithLifecycle()
+
+    BackHandler { onBackClick() }
+
+    val signInLauncher = rememberLauncherForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        viewModel.handleSignInResult(result)
+    }
+
+    LaunchedEffect(Unit) {
+        viewModel.oneOffEvent.collect { event ->
+            when (event) {
+                is ConnectCloudBackupViewModel.Event.SignInToGoogle -> {
+                    signInLauncher.launch(event.signInIntent)
+                }
+                is ConnectCloudBackupViewModel.Event.ProceedToCreateAccountWithCloudBackupEnabled -> {
+                    onContinueToCreateAccount()
+                }
+            }
+        }
+    }
+
+    ConnectCloudBackupContent(
+        modifier = modifier,
+        errorMessage = state.errorMessage,
+        onErrorMessageShown = viewModel::onErrorMessageShown,
+        onBackClick = onBackClick,
+        onLoginToGoogleClick = viewModel::onSignInToGoogleClick,
+        onSkipClick = onSkipClick
+    )
+}
+
+@Composable
+private fun ConnectCloudBackupContent(
+    modifier: Modifier = Modifier,
+    errorMessage: UiMessage?,
+    onErrorMessageShown: () -> Unit,
+    onBackClick: () -> Unit,
+    onLoginToGoogleClick: () -> Unit,
+    onSkipClick: () -> Unit
+) {
+    val snackBarHostState = remember { SnackbarHostState() }
+
+    SnackbarUIMessage(
+        message = errorMessage,
+        snackbarHostState = snackBarHostState,
+        onMessageShown = onErrorMessageShown
+    )
+
+    Scaffold(
+        modifier = modifier.imePadding(),
+        topBar = {
+            RadixCenteredTopAppBar(
+                title = "",
+                onBackClick = onBackClick,
+                actions = {
+                    RadixTextButton(
+                        text = "Skip", // TODO crowdin
+                        onClick = {
+                            onSkipClick() // continue without back up
+                        }
+                    )
+                },
+                backIconType = BackIconType.Close,
+                windowInsets = WindowInsets.statusBars,
+            )
+        },
+        snackbarHost = {
+            RadixSnackbarHost(
+                modifier = Modifier.padding(RadixTheme.dimensions.paddingDefault),
+                hostState = snackBarHostState
+            )
+        },
+        containerColor = RadixTheme.colors.defaultBackground
+    ) { padding ->
+
+        Column(
+            modifier = Modifier
+                .padding(padding)
+                .padding(horizontal = RadixTheme.dimensions.paddingDefault)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.weight(0.3f))
+            Icon(
+                painter = painterResource(id = com.babylon.wallet.android.designsystem.R.drawable.ic_backup_google),
+                tint = Color.Unspecified,
+                contentDescription = null
+            )
+
+            Spacer(modifier = Modifier.weight(0.1f))
+            Text(
+                modifier = Modifier
+                    .widthIn(max = 250.dp)
+                    .padding(bottom = RadixTheme.dimensions.paddingDefault),
+                text = "Back up your wallet settings", // TODO Crowdin
+                style = RadixTheme.typography.title,
+                color = RadixTheme.colors.gray1,
+                textAlign = TextAlign.Center
+            )
+
+            Text(
+                modifier = Modifier.widthIn(max = 300.dp),
+                text = "Connect to Google Drive to automatically backup your Radix wallet settings.", // TODO Crowdin
+                style = RadixTheme.typography.secondaryHeader,
+                color = RadixTheme.colors.gray2,
+                textAlign = TextAlign.Center
+            )
+
+            Spacer(Modifier.weight(0.4f))
+            RadixPrimaryButton(
+                modifier = Modifier.fillMaxWidth(),
+                text = "Back up to Google Drive", // TODO Crowdin
+                onClick = onLoginToGoogleClick
+            )
+            Spacer(modifier = Modifier.weight(0.1f))
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ConnectCloudBackupScreenPreview() {
+    RadixWalletPreviewTheme {
+        ConnectCloudBackupContent(
+            errorMessage = null,
+            onErrorMessageShown = {},
+            onBackClick = {},
+            onLoginToGoogleClick = {},
+            onSkipClick = {}
+        )
+    }
+}
