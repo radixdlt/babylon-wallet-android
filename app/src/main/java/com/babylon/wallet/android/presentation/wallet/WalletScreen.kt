606e1eac6f7057c31bcc92ada5b25780db70f9d7
Sergiu Puhalschi <sergiu.puhalschi@rdx.works>
2024-05-27 18:32:01 +0300

    Fix detekt issue

--- app/src/main/java/com/babylon/wallet/android/presentation/wallet/WalletScreen.kt
+++ app/src/main/java/com/babylon/wallet/android/presentation/wallet/WalletScreen.kt
@@ -16,8 +16,6 @@ import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.pullrefresh.PullRefreshIndicator
 import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.material.pullrefresh.rememberPullRefreshState
-import androidx.compose.material3.Badge
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
