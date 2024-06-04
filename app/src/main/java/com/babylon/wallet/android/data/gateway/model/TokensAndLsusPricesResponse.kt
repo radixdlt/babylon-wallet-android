e0068e2b4f3b6b1a25e2e82d941eb06586f69532
Sergiu Puhalschi <sergiu.puhalschi@rdx.works>
2024-06-04 19:37:54 +0300

    Fallback to 0 when fiat prices parsing fails because of decimal overflow

--- app/src/main/java/com/babylon/wallet/android/data/gateway/model/TokensAndLsusPricesResponse.kt
+++ app/src/main/java/com/babylon/wallet/android/data/gateway/model/TokensAndLsusPricesResponse.kt
@@ -3,7 +3,9 @@ package com.babylon.wallet.android.data.gateway.model
 import com.babylon.wallet.android.data.repository.cache.database.TokenPriceEntity
 import com.radixdlt.sargon.ResourceAddress
 import com.radixdlt.sargon.extensions.init
+import com.radixdlt.sargon.extensions.orZero
 import com.radixdlt.sargon.extensions.toDecimal192
+import com.radixdlt.sargon.extensions.toDecimal192OrNull
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import rdx.works.core.InstantGenerator
@@ -44,7 +46,7 @@ data class TokensAndLsusPricesResponse(
             val lsus = this.lsus.map { lsuPrice ->
                 TokenPriceEntity(
                     resourceAddress = ResourceAddress.init(lsuPrice.resourceAddress),
-                    price = lsuPrice.usdPrice.toDecimal192(),
+                    price = lsuPrice.usdPrice.toDecimal192OrNull().orZero(),
                     currency = SupportedCurrency.USD.code,
                     synced = instantGenerator
                 )
@@ -52,7 +54,7 @@ data class TokensAndLsusPricesResponse(
             val tokens = this.tokens.map { tokenPrice ->
                 TokenPriceEntity(
                     resourceAddress = ResourceAddress.init(tokenPrice.resourceAddress),
-                    price = tokenPrice.usdPrice.toDecimal192(),
+                    price = tokenPrice.usdPrice.toDecimal192OrNull().orZero(),
                     currency = SupportedCurrency.USD.code,
                     synced = instantGenerator
                 )
