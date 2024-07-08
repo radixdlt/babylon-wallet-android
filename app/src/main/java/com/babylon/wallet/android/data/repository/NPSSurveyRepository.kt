06063afe22797b4985f678c4a4bc7536d439b164
Jakub Porzuczek <jakub.porzuczek@rdx.works>
2024-07-08 12:34:37 +0200

    Use body instead of query params when submitting NPS survey

--- app/src/main/java/com/babylon/wallet/android/data/repository/NPSSurveyRepository.kt
+++ app/src/main/java/com/babylon/wallet/android/data/repository/NPSSurveyRepository.kt
@@ -2,6 +2,7 @@ package com.babylon.wallet.android.data.repository
 
 import com.babylon.wallet.android.BuildConfig
 import com.babylon.wallet.android.data.gateway.survey.NPSSurveyApi
+import com.babylon.wallet.android.data.gateway.survey.SurveyRequest
 import com.babylon.wallet.android.data.gateway.survey.SurveyResponse
 import com.babylon.wallet.android.di.coroutines.IoDispatcher
 import kotlinx.coroutines.CoroutineDispatcher
@@ -30,11 +31,11 @@ class NPSSurveyRepositoryImpl @Inject constructor(
         return with(ioDispatcher) {
             val uuid = preferencesManager.surveyUuid.first()
             nPSSurveyApi.storeSurveyResponse(
-                params = mapOf(
-                    NPSSurveyApi.PARAM_ID to uuid,
-                    NPSSurveyApi.PARAM_FORM_UUID to BuildConfig.REFINER_FORM_UUID,
-                    NPSSurveyApi.PARAM_NPS to npsQuestion,
-                    NPSSurveyApi.PARAM_WHAT_DO_YOU_VALUE to reason
+                SurveyRequest(
+                    id = uuid,
+                    formUuid = BuildConfig.REFINER_FORM_UUID,
+                    nps = npsQuestion,
+                    whatDoYouValue = reason
                 )
             ).toResult()
         }
