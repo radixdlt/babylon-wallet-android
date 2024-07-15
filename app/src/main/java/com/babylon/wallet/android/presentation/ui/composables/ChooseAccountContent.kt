bd17673d05fd9129eabd29dae7c46aeb1a8ebc5e
Sergiu Puhalschi <sergiu.puhalschi@rdx.works>
2024-07-15 11:26:55 +0300

    Remove unnecessary import

--- app/src/main/java/com/babylon/wallet/android/presentation/ui/composables/ChooseAccountContent.kt
+++ app/src/main/java/com/babylon/wallet/android/presentation/ui/composables/ChooseAccountContent.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.lazy.LazyColumn
