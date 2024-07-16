5cfb1f2ee2be7660914650a49f6ce24c7662dc54
giannis.tsepas <giannis.tsepas@rdx.works>
2024-07-16 16:38:34 +0300

    const val for route in GetSignaturesNavGraph

--- app/src/main/java/com/babylon/wallet/android/presentation/accessfactorsources/signatures/GetSignaturesNavGraph.kt
+++ app/src/main/java/com/babylon/wallet/android/presentation/accessfactorsources/signatures/GetSignaturesNavGraph.kt
@@ -6,15 +6,17 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.dialog
 
+private const val ROUTE_GET_SIGNATURES = "get_signatures_bottom_sheet"
+
 fun NavController.getSignatures() {
-    navigate("get_signatures_bottom_sheet")
+    navigate(ROUTE_GET_SIGNATURES)
 }
 
 fun NavGraphBuilder.getSignatures(
     onDismiss: () -> Unit
 ) {
     dialog(
-        route = "get_signatures_bottom_sheet",
+        route = ROUTE_GET_SIGNATURES,
         dialogProperties = DialogProperties(usePlatformDefaultWidth = false)
     ) {
         GetSignaturesDialog(
