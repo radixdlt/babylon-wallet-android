8b26690c919c78992ac6a13e709c0430ca969de3
Jakub Porzuczek <jakub.porzuczek@rdx.works>
2024-08-05 11:27:53 +0200

    remove unused import

--- designsystem/src/main/java/com/babylon/wallet/android/designsystem/composable/RadixSecondaryButton.kt
+++ designsystem/src/main/java/com/babylon/wallet/android/designsystem/composable/RadixSecondaryButton.kt
@@ -23,7 +23,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.babylon.wallet.android.designsystem.R
 import com.babylon.wallet.android.designsystem.darken
 import com.babylon.wallet.android.designsystem.theme.RadixTheme
