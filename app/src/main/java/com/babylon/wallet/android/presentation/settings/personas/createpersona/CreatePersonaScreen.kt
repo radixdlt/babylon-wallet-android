bcae4917884405b9be17f5c7290ffd22a07a8d41
Jakub Porzuczek <jakub.porzuczek@rdx.works>
2024-08-06 14:31:09 +0200

    fix bottom padding for add gateway/add persona field sheets

--- app/src/main/java/com/babylon/wallet/android/presentation/settings/personas/createpersona/CreatePersonaScreen.kt
+++ app/src/main/java/com/babylon/wallet/android/presentation/settings/personas/createpersona/CreatePersonaScreen.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.navigationBars
-import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBars
@@ -198,8 +197,7 @@ fun CreatePersonaContent(
                     },
                     onSelectionChanged = onSelectionChanged,
                     modifier = Modifier
-                        .fillMaxSize()
-                        .navigationBarsPadding(),
+                        .fillMaxSize(),
                     anyFieldSelected = state.anyFieldSelected
                 )
             },
--- app/src/main/java/com/babylon/wallet/android/presentation/settings/preferences/gateways/GatewaysScreen.kt
+++ app/src/main/java/com/babylon/wallet/android/presentation/settings/preferences/gateways/GatewaysScreen.kt
@@ -7,14 +7,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.exclude
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.union
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -277,11 +276,11 @@ private fun AddGatewaySheet(
         }
 
         RadixBottomBar(
-            modifier = Modifier.imePadding(),
             onClick = onAddGatewayClick,
             text = stringResource(R.string.gateways_addNewGateway_addGatewayButtonTitle),
             enabled = input.isUrlValid,
-            isLoading = input.isLoading
+            isLoading = input.isLoading,
+            insets = WindowInsets.navigationBars.union(WindowInsets.ime)
         )
     }
 }
