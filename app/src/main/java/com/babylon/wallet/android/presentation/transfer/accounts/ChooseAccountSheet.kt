1cb6f595a8e281d0de160b1896cc02b486853f43
Jakub Porzuczek <jakub.porzuczek@rdx.works>
2024-08-09 11:13:38 +0200

    update sheets insets

--- app/src/main/java/com/babylon/wallet/android/presentation/transfer/accounts/ChooseAccountSheet.kt
+++ app/src/main/java/com/babylon/wallet/android/presentation/transfer/accounts/ChooseAccountSheet.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -112,10 +111,7 @@ fun ChooseAccountSheet(
                     onClick = onChooseAccountSubmitted,
                     text = stringResource(id = R.string.common_choose),
                     enabled = state.isChooseButtonEnabled,
-                    isLoading = state.isLoadingAssetsForAccount,
-                    // Since the sheet is configured inside BottomSheetDialogWrapper, bottom padding is already added
-                    // This should be fixed
-                    insets = WindowInsets(0.dp)
+                    isLoading = state.isLoadingAssetsForAccount
                 )
             }
         }
