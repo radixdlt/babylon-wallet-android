ae9ced6625170b03516ed98ce7fcf1f8379ef057
giannis.tsepas <giannis.tsepas@rdx.works>
2024-09-04 16:52:52 +0300

    replaced hardcoded string with string res

--- app/src/main/java/com/babylon/wallet/android/presentation/ui/composables/LockScreenBackground.kt
+++ app/src/main/java/com/babylon/wallet/android/presentation/ui/composables/LockScreenBackground.kt
@@ -16,10 +16,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.babylon.wallet.android.R
 import com.babylon.wallet.android.designsystem.theme.RadixTheme
+import com.babylon.wallet.android.presentation.ui.RadixWalletPreviewTheme
 
 @Composable
 fun LockScreenBackground(modifier: Modifier = Modifier, onTapToUnlock: (() -> Unit)? = null) {
@@ -60,12 +63,20 @@ fun LockScreenBackground(modifier: Modifier = Modifier, onTapToUnlock: (() -> Un
                 Text(
                     modifier = Modifier
                         .clickable { onTapToUnlock() },
-                    text = "Tap to unlock",
+                    text = stringResource(R.string.splash_tapAnywhereToUnlock),
                     style = RadixTheme.typography.body2Regular,
                     color = RadixTheme.colors.white,
                     textAlign = TextAlign.Center
                 )
             }
         }
+    }
+}
+
+@Preview
+@Composable
+private fun LockScreenBackgroundPreview() {
+    RadixWalletPreviewTheme {
+        LockScreenBackground()
     }
 }
