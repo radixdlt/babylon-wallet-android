2e644d627f41affd066049fc076a131af8af5b10
giannis.tsepas <giannis.tsepas@rdx.works>
2024-09-06 10:47:38 +0300

    removed privacyOverlay view that not working and some minor updates

--- app/src/main/java/com/babylon/wallet/android/MainActivity.kt
+++ app/src/main/java/com/babylon/wallet/android/MainActivity.kt
@@ -1,10 +1,8 @@
 package com.babylon.wallet.android
 
 import android.animation.ObjectAnimator
-import android.content.Context
 import android.content.Intent
 import android.graphics.Color
-import android.graphics.PixelFormat
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
@@ -20,7 +18,6 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.ui.platform.ComposeView
 import androidx.core.animation.doOnEnd
 import androidx.core.splashscreen.SplashScreen
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
@@ -28,20 +25,17 @@ import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
-import androidx.lifecycle.setViewTreeLifecycleOwner
-import androidx.savedstate.setViewTreeSavedStateRegistryOwner
 import com.babylon.wallet.android.LinkConnectionStatusObserver.LinkConnectionsStatus
 import com.babylon.wallet.android.designsystem.theme.DefaultDarkScrim
 import com.babylon.wallet.android.designsystem.theme.DefaultLightScrim
 import com.babylon.wallet.android.designsystem.theme.RadixWalletTheme
 import com.babylon.wallet.android.presentation.BalanceVisibilityObserver
-import com.babylon.wallet.android.presentation.dialogs.lock.AppLockActivity
+import com.babylon.wallet.android.presentation.lockscreen.AppLockActivity
 import com.babylon.wallet.android.presentation.main.AppState
 import com.babylon.wallet.android.presentation.main.MainViewModel
 import com.babylon.wallet.android.presentation.ui.CustomCompositionProviders
 import com.babylon.wallet.android.presentation.ui.composables.DevBannerState
 import com.babylon.wallet.android.presentation.ui.composables.DevelopmentPreviewWrapper
-import com.babylon.wallet.android.presentation.ui.composables.LockScreenBackground
 import com.babylon.wallet.android.presentation.ui.composables.actionableaddress.ActionableAddressViewEntryPoint
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
@@ -51,10 +45,6 @@ import javax.inject.Inject
 // Extending from FragmentActivity because of Biometric
 @AndroidEntryPoint
 class MainActivity : FragmentActivity() {
-
-    private lateinit var windowManager: WindowManager
-
-    private lateinit var privacyOverlay: ComposeView
 
     private val viewModel: MainViewModel by viewModels()
 
@@ -73,20 +63,21 @@ class MainActivity : FragmentActivity() {
     lateinit var actionableAddressViewEntryPoint: ActionableAddressViewEntryPoint
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        windowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
-        setupPrivacyOverlay()
         val splashScreen = installSplashScreen()
         splashScreen.setKeepOnScreenCondition {
             viewModel.state.value.initialAppState == AppState.Loading
         }
         setSplashExitAnimation(splashScreen)
+
         super.onCreate(savedInstanceState)
+
         cloudBackupSyncExecutor.startPeriodicChecks(lifecycleOwner = this)
 
         intent.data?.let {
             intent.replaceExtras(Bundle())
             viewModel.handleDeepLink(it)
         }
+
         setContent {
             RadixWalletTheme {
                 SyncEdgeToEdgeSetupWithSelectedTheme()
@@ -121,7 +112,8 @@ class MainActivity : FragmentActivity() {
                 }
             }
         }
-        monitorLockState()
+
+        monitorAdvancedLockState()
     }
 
     @Composable
@@ -142,36 +134,19 @@ class MainActivity : FragmentActivity() {
         }
     }
 
-    private fun setupPrivacyOverlay() {
-        privacyOverlay = ComposeView(this).apply {
-            setViewTreeSavedStateRegistryOwner(this@MainActivity)
-            setViewTreeLifecycleOwner(this@MainActivity)
-        }
-        privacyOverlay.setContent {
-            RadixWalletTheme {
-                LockScreenBackground()
-            }
-        }
-    }
-
-    private fun monitorLockState() {
+    private fun monitorAdvancedLockState() {
         lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.RESUMED) {
                 viewModel.state.collect { state ->
-                    if (state.isAppLockEnabled) {
+                    if (state.isAdvancedLockEnabled) {
                         window.addFlags(WindowManager.LayoutParams.FLAG_SECURE)
                     }
                     if (state.isAppLocked) {
-                        window.addFlags(WindowManager.LayoutParams.FLAG_SECURE)
                         startActivity(
                             Intent(this@MainActivity, AppLockActivity::class.java).apply {
                                 addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
                             }
                         )
-                    }
-                    try {
-                        windowManager.removeView(privacyOverlay)
-                    } catch (_: Exception) {
                     }
                 }
             }
@@ -220,22 +195,6 @@ class MainActivity : FragmentActivity() {
     override fun onResume() {
         super.onResume()
         viewModel.onAppToForeground()
-    }
-
-    override fun onUserLeaveHint() {
-        if (viewModel.state.value.isAppLockEnabled) {
-            val params = WindowManager.LayoutParams().apply {
-                type = WindowManager.LayoutParams.TYPE_APPLICATION_PANEL
-                token = privacyOverlay.applicationWindowToken
-                width = WindowManager.LayoutParams.MATCH_PARENT
-                height = WindowManager.LayoutParams.MATCH_PARENT
-                format = PixelFormat.TRANSLUCENT
-                flags = WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS or
-                    WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
-            }
-            windowManager.addView(privacyOverlay, params)
-        }
-        super.onUserLeaveHint()
     }
 
     companion object {
