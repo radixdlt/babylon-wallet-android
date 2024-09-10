265cc9f96396ce590edd3d7988bc81e390bc7808
giannis.tsepas <giannis.tsepas@rdx.works>
2024-09-10 12:42:33 +0300

    removed the unnecessary NotInitialised from AppLockStateProvider

--- app/src/main/java/com/babylon/wallet/android/AppLockStateProvider.kt
+++ app/src/main/java/com/babylon/wallet/android/AppLockStateProvider.kt
@@ -3,13 +3,13 @@ package com.babylon.wallet.android
 import com.babylon.wallet.android.di.coroutines.ApplicationScope
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.shareIn
 import kotlinx.coroutines.flow.update
-import rdx.works.core.domain.ProfileState
 import rdx.works.core.sargon.isAdvancedLockEnabled
 import rdx.works.profile.domain.GetProfileUseCase
 import javax.inject.Inject
@@ -18,27 +18,25 @@ import javax.inject.Singleton
 @Singleton
 class AppLockStateProvider @Inject constructor(
     private val getProfileUseCase: GetProfileUseCase,
-    @ApplicationScope coroutineScope: CoroutineScope
+    @ApplicationScope private val applicationScope: CoroutineScope
 ) {
 
     private val _state: MutableStateFlow<State> = MutableStateFlow(State())
 
-    val lockState = combine(
-        getProfileUseCase.state,
-        _state
-    ) { profileState, lockedState ->
-
-        if (profileState is ProfileState.NotInitialised) {
-            LockState.Unlocked
-        } else {
-            lockedState.lockState
-        }
-    }.onStart { // this will execute ONLY when you launch the wallet to make the state aware of the isAdvancedLockEnabled
-        val isAdvancedLockEnabled = getProfileUseCase().isAdvancedLockEnabled
-        _state.update {
-            it.copy(lockState = if (isAdvancedLockEnabled) LockState.Locked else LockState.Unlocked)
-        }
-    }.shareIn(scope = coroutineScope, started = SharingStarted.WhileSubscribed())
+    val lockState: SharedFlow<LockState>
+        get() = _state
+            .onStart {
+                val isAdvancedLockEnabled = getProfileUseCase().isAdvancedLockEnabled
+                _state.update {
+                    it.copy(lockState = if (isAdvancedLockEnabled) LockState.Locked else LockState.Unlocked)
+                }
+            }
+            .map { state ->
+                state.lockState
+            }.shareIn(
+                scope = applicationScope,
+                started = SharingStarted.WhileSubscribed()
+            )
 
     suspend fun lockApp() {
         if (_state.value.isLockingPaused) return
