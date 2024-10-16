304d4fb01d2b641940dad720aa14821ffbfa6d67
giannis.tsepas <giannis.tsepas@rdx.works>
2024-10-16 15:09:53 +0300

    updated InitialUnauthorizedLoginRoute with two new flows to verify entities

--- app/src/main/java/com/babylon/wallet/android/presentation/dapp/unauthorized/InitialUnauthorizedLoginRoute.kt
+++ app/src/main/java/com/babylon/wallet/android/presentation/dapp/unauthorized/InitialUnauthorizedLoginRoute.kt
@@ -1,8 +1,10 @@
 package com.babylon.wallet.android.presentation.dapp.unauthorized
 
 import com.babylon.wallet.android.domain.model.messages.RequiredPersonaFields
+import com.babylon.wallet.android.presentation.dapp.unauthorized.verifyentities.EntitiesForProofWithSignatures
 
 sealed interface InitialUnauthorizedLoginRoute {
+
     data class OnetimePersonaData(
         val requiredPersonaFields: RequiredPersonaFields
     ) : InitialUnauthorizedLoginRoute
@@ -10,5 +12,15 @@ sealed interface InitialUnauthorizedLoginRoute {
     data class ChooseAccount(
         val numberOfAccounts: Int,
         val isExactAccountsCount: Boolean
+    ) : InitialUnauthorizedLoginRoute
+
+    data class VerifyPersona(
+        val walletUnauthorizedRequestInteractionId: String,
+        val entitiesForProofWithSignatures: EntitiesForProofWithSignatures
+    ) : InitialUnauthorizedLoginRoute
+
+    data class VerifyAccounts(
+        val walletUnauthorizedRequestInteractionId: String,
+        val entitiesForProofWithSignatures: EntitiesForProofWithSignatures
     ) : InitialUnauthorizedLoginRoute
 }
