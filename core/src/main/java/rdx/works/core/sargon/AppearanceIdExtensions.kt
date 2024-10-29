813b2685ee99e24f149a445ea37a8ae03b695868
micbakos-rdx <michael.bakogiannis@rdx.works>
2024-10-29 10:24:52 +0200

    WIP

--- core/src/main/java/rdx/works/core/sargon/AppearanceIdExtensions.kt
+++ core/src/main/java/rdx/works/core/sargon/AppearanceIdExtensions.kt
@@ -2,11 +2,24 @@ package rdx.works.core.sargon
 
 import com.radixdlt.sargon.AppearanceId
 import com.radixdlt.sargon.DerivationPath
+import com.radixdlt.sargon.Hardened
+import com.radixdlt.sargon.extensions.asHardened
+import com.radixdlt.sargon.extensions.indexInLocalKeySpace
 import com.radixdlt.sargon.extensions.init
-import com.radixdlt.sargon.extensions.nonHardenedIndex
 
 fun AppearanceId.Companion.from(derivationPath: DerivationPath): AppearanceId {
-    return from(offset = derivationPath.nonHardenedIndex)
+    val hardened = when (derivationPath) {
+        is DerivationPath.Account -> derivationPath.value.index
+        is DerivationPath.Bip44Like -> derivationPath.value.index.asHardened()
+        is DerivationPath.Identity -> derivationPath.value.index
+    }
+
+    val index = when (hardened) {
+        is Hardened.Securified -> hardened.v1.indexInLocalKeySpace
+        is Hardened.Unsecurified -> hardened.v1.indexInLocalKeySpace
+    }
+
+    return from(offset = index)
 }
 
 fun AppearanceId.Companion.from(offset: UInt): AppearanceId = AppearanceId.init((offset % AppearanceId.MAX).toUByte())
