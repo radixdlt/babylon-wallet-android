19b637483f198e9b0828090b8f783a4198e26757
micbakos-rdx <michael.bakogiannis@rdx.works>
2024-10-31 10:53:29 +0200

    Fix pool contributions

--- app/src/main/java/com/babylon/wallet/android/presentation/transaction/analysis/processor/PoolContributionProcessor.kt
+++ app/src/main/java/com/babylon/wallet/android/presentation/transaction/analysis/processor/PoolContributionProcessor.kt
@@ -3,15 +3,21 @@ package com.babylon.wallet.android.presentation.transaction.analysis.processor
 import com.babylon.wallet.android.domain.model.Transferable
 import com.babylon.wallet.android.domain.model.TransferableAsset
 import com.babylon.wallet.android.domain.usecases.assets.ResolveAssetsFromAddressUseCase
+import com.babylon.wallet.android.presentation.model.FungibleAmount
 import com.babylon.wallet.android.presentation.transaction.PreviewType
 import com.babylon.wallet.android.presentation.transaction.model.AccountWithTransferableResources
+import com.babylon.wallet.android.presentation.transaction.model.TransferableX
 import com.radixdlt.sargon.Account
 import com.radixdlt.sargon.Decimal192
 import com.radixdlt.sargon.DetailedManifestClass
 import com.radixdlt.sargon.ExecutionSummary
+import com.radixdlt.sargon.ResourceAddress
+import com.radixdlt.sargon.TrackedPoolContribution
 import com.radixdlt.sargon.extensions.address
 import com.radixdlt.sargon.extensions.orZero
+import com.radixdlt.sargon.extensions.plus
 import com.radixdlt.sargon.extensions.sumOf
+import com.radixdlt.sargon.extensions.toDecimal192
 import rdx.works.core.domain.assets.Asset
 import rdx.works.core.domain.assets.PoolUnit
 import rdx.works.profile.domain.GetProfileUseCase
@@ -33,52 +39,49 @@ class PoolContributionProcessor @Inject constructor(
 
         return PreviewType.Transfer.Pool(
             from = withdraws,
-            to = deposits,
+            to = deposits.augmentWithContributions(contributions = classification.poolContributions),
             badges = badges,
             actionType = PreviewType.Transfer.Pool.ActionType.Contribution,
             newlyCreatedNFTItems = summary.newlyCreatedNonFungibleItems()
         )
     }
 
-    // TODO micbakos
-    private fun ExecutionSummary.extractDeposits(
-        classification: DetailedManifestClass.PoolContribution,
-        assets: List<Asset>,
-        defaultDepositGuarantee: Decimal192,
-        involvedOwnedAccounts: List<Account>
-    ): List<AccountWithTransferableResources> {
-        val to = deposits.map { depositsPerAddress ->
-            depositsPerAddress.value.map { deposit ->
-                val poolUnit = assets.find { it.resource.address == deposit.address } as? PoolUnit
-                if (poolUnit == null) {
-                    resolveDepositingAsset(deposit, assets, defaultDepositGuarantee)
-                } else {
-                    val contributions = classification.poolContributions.filter {
-                        it.poolUnitsResourceAddress == deposit.address
+    private fun List<AccountWithTransferableResources>.augmentWithContributions(
+        contributions: List<TrackedPoolContribution>
+    ): List<AccountWithTransferableResources> = map { accountWithTransferables ->
+        val augmentedTransferables = accountWithTransferables.resources.map { transferable ->
+            val poolUnit = (transferable as? TransferableX.FungibleType.PoolUnit) ?: return@map transferable
+
+            var totalPoolUnitAmount = 0.toDecimal192();
+            val contributionsPerResource = mutableMapOf<ResourceAddress, Decimal192>()
+
+            contributions
+                .filter {
+                    it.poolUnitsResourceAddress == poolUnit.resourceAddress
+                }.forEach { contribution ->
+                    contribution.contributedResources.forEach { (address, amount) ->
+                        val currentAmount = contributionsPerResource.getOrDefault(address, 0.toDecimal192())
+
+                        contributionsPerResource[address] = currentAmount + amount
                     }
-                    val contributedResourceAddresses = contributions.first().contributedResources.keys
-                    val guaranteeType = deposit.guaranteeType(defaultDepositGuarantee)
-                    val poolUnitAmount = contributions.find {
-                        it.poolUnitsResourceAddress == poolUnit.resourceAddress
-                    }?.poolUnitsAmount.orZero()
-                    val contributionPerResource = contributedResourceAddresses.associateWith { contributedResourceAddress ->
-                        contributions.mapNotNull {
-                            it.contributedResources[contributedResourceAddress]
-                        }.sumOf { it }
-                    }
-                    Transferable.Depositing(
-                        transferable = TransferableAsset.Fungible.PoolUnitAsset(
-                            amount = contributions.map { it.poolUnitsAmount }.sumOf { it },
-                            unit = poolUnit.copy(
-                                stake = poolUnit.stake.copy(ownedAmount = poolUnitAmount)
-                            ),
-                            contributionPerResource = contributionPerResource
-                        ),
-                        guaranteeType = guaranteeType,
-                    )
+
+                    totalPoolUnitAmount += contribution.poolUnitsAmount
                 }
-            }.toAccountWithTransferableResources(depositsPerAddress.key, involvedOwnedAccounts)
+
+            val newAmount = when (poolUnit.amount) {
+                is FungibleAmount.Exact -> FungibleAmount.Exact(totalPoolUnitAmount)
+                is FungibleAmount.Predicted -> poolUnit.amount.copy(
+                    amount = totalPoolUnitAmount
+                )
+                else -> FungibleAmount.Exact(totalPoolUnitAmount)
+            }
+
+            poolUnit.copy(
+                amount = newAmount,
+                contributionPerResource = contributionsPerResource.mapValues { FungibleAmount.Exact(it.value) }
+            )
         }
-        return to
+
+        accountWithTransferables.update(augmentedTransferables)
     }
 }
