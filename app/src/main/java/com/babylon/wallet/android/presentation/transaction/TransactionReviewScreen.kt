3082113d475df8b3dd0dc5c6798c90696f9b1cc7
micbakos-rdx <michael.bakogiannis@rdx.works>
2024-11-01 10:33:55 +0200

    Fix badge NFT

--- app/src/main/java/com/babylon/wallet/android/presentation/transaction/TransactionReviewScreen.kt
+++ app/src/main/java/com/babylon/wallet/android/presentation/transaction/TransactionReviewScreen.kt
@@ -39,6 +39,7 @@ import com.babylon.wallet.android.domain.usecases.TransactionFeePayers
 import com.babylon.wallet.android.presentation.common.FullscreenCircularProgressContent
 import com.babylon.wallet.android.presentation.dialogs.info.GlossaryItem
 import com.babylon.wallet.android.presentation.model.FungibleAmount
+import com.babylon.wallet.android.presentation.model.NonFungibleAmount
 import com.babylon.wallet.android.presentation.settings.approveddapps.dappdetail.UnknownAddressesSheetContent
 import com.babylon.wallet.android.presentation.transaction.TransactionReviewViewModel.State
 import com.babylon.wallet.android.presentation.transaction.composables.AccountDepositSettingsTypeContent
@@ -385,7 +386,7 @@ private fun TransactionPreviewContent(
                                     is Resource.NonFungibleResource -> onTransferableNonFungibleClick(
                                         Transferable.NonFungibleType.NFTCollection(
                                             asset = NonFungibleCollection(resource),
-                                            amount = TODO(),
+                                            amount = NonFungibleAmount.Exact(nfts = resource.items),
                                             isNewlyCreated = false
                                         ),
                                         resource.items.firstOrNull()
