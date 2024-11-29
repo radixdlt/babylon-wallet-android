49efa7f0bd9da8aab8e961fd2f880d78088cb50e
micbakos-rdx <michael.bakogiannis@rdx.works>
2024-11-29 10:08:57 +0200

    Fix removing asset

--- app/src/main/java/com/babylon/wallet/android/presentation/transfer/TransferViewModel.kt
+++ app/src/main/java/com/babylon/wallet/android/presentation/transfer/TransferViewModel.kt
@@ -504,7 +504,7 @@ class TransferViewModel @Inject constructor(
                             nonFungibles = assets.nonFungibles.mapWhen(
                                 predicate = {
                                     it.collection.address == forResource.address &&
-                                        it.collection.items.size < forResource.items.size
+                                            it.collection.items.size < forResource.items.size
                                 },
                                 mutation = { NonFungibleCollection(forResource) }
                             )
@@ -610,9 +610,16 @@ sealed class TargetAccount {
         }
     }
 
-    fun removeAsset(asset: SpendingAsset): TargetAccount {
+    fun removeAsset(removingAsset: SpendingAsset): TargetAccount {
         val newSpendingAssets = spendingAssets.toMutableSet().apply {
-            removeIf { it.resourceAddress == asset.resourceAddress }
+            removeIf { asset ->
+                when (asset) {
+                    is SpendingAsset.Fungible -> removingAsset is SpendingAsset.Fungible &&
+                            removingAsset.resourceAddress == asset.resourceAddress
+                    is SpendingAsset.NFT -> removingAsset is SpendingAsset.NFT &&
+                            removingAsset.item.globalId == asset.item.globalId
+                }
+            }
         }.toPersistentSet()
 
         return when (this) {
