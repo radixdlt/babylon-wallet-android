8f14a8ed7b0fcbfbc03dcffb9dc6bee0bbda2949
micbakos-rdx <michael.bakogiannis@rdx.works>
2024-12-05 16:43:42 +0200

    WIP Impl of host interactor

--- app/src/main/java/com/babylon/wallet/android/domain/usecases/signing/SignAndNotariseTransactionUseCase.kt
+++ app/src/main/java/com/babylon/wallet/android/domain/usecases/signing/SignAndNotariseTransactionUseCase.kt
@@ -1,46 +1,40 @@
 package com.babylon.wallet.android.domain.usecases.signing
 
-import com.babylon.wallet.android.data.dapp.model.LedgerErrorCode.UserRejectedSigningOfTransaction
 import com.babylon.wallet.android.data.repository.transaction.TransactionRepository
 import com.babylon.wallet.android.domain.RadixWalletException
 import com.babylon.wallet.android.domain.RadixWalletException.DappRequestException.RejectedByUser
-import com.babylon.wallet.android.domain.RadixWalletException.LedgerCommunicationException.FailedToSignTransaction
 import com.babylon.wallet.android.domain.RadixWalletException.PrepareTransactionException
-import com.babylon.wallet.android.domain.model.signing.SignPurpose
-import com.babylon.wallet.android.domain.model.signing.SignRequest
 import com.babylon.wallet.android.domain.usecases.transaction.TransactionConfig
-import com.babylon.wallet.android.presentation.accessfactorsources.AccessFactorSourcesInput
-import com.babylon.wallet.android.presentation.accessfactorsources.AccessFactorSourcesProxy
 import com.radixdlt.sargon.AccountAddress
+import com.radixdlt.sargon.CommonException
 import com.radixdlt.sargon.Decimal192
 import com.radixdlt.sargon.Epoch
-import com.radixdlt.sargon.IntentSignature
-import com.radixdlt.sargon.IntentSignatures
 import com.radixdlt.sargon.Message
 import com.radixdlt.sargon.NetworkId
 import com.radixdlt.sargon.Nonce
 import com.radixdlt.sargon.NotarizedTransaction
 import com.radixdlt.sargon.PublicKey
+import com.radixdlt.sargon.RoleKind
 import com.radixdlt.sargon.SignedIntent
 import com.radixdlt.sargon.TransactionHeader
 import com.radixdlt.sargon.TransactionIntent
 import com.radixdlt.sargon.TransactionManifest
 import com.radixdlt.sargon.extensions.Curve25519SecretKey
 import com.radixdlt.sargon.extensions.hash
-import com.radixdlt.sargon.extensions.init
 import com.radixdlt.sargon.extensions.mapError
 import com.radixdlt.sargon.extensions.modifyLockFee
 import com.radixdlt.sargon.extensions.random
 import com.radixdlt.sargon.extensions.summary
 import com.radixdlt.sargon.extensions.then
 import com.radixdlt.sargon.extensions.toDecimal192
+import com.radixdlt.sargon.os.SargonOsManager
 import rdx.works.core.domain.transaction.NotarizationResult
 import javax.inject.Inject
 
 class SignAndNotariseTransactionUseCase @Inject constructor(
     private val transactionRepository: TransactionRepository,
     private val resolveSignersUseCase: ResolveSignersUseCase,
-    private val accessFactorSourcesProxy: AccessFactorSourcesProxy,
+    private val sargonOsManager: SargonOsManager,
 ) {
 
     @Suppress("LongParameterList")
@@ -90,38 +84,28 @@ class SignAndNotariseTransactionUseCase @Inject constructor(
         notaryKey: PublicKey,
         tipPercentage: UShort,
         epochRange: OpenEndRange<Epoch>,
-    ) = resolveSignersUseCase(summary = manifest.summary)
-        .then { signers ->
-            val intent = TransactionIntent.from(
-                networkId = networkId,
-                manifest = manifest,
-                message = message,
-                notaryPublicKey = notaryKey,
-                notaryIsSignatory = signers.isEmpty(),
-                tipPercentage = tipPercentage,
-                epochs = epochRange
-            )
+    ) = resolveSignersUseCase(summary = manifest.summary).mapCatching { signers ->
+        val intent = TransactionIntent.from(
+            networkId = networkId,
+            manifest = manifest,
+            message = message,
+            notaryPublicKey = notaryKey,
+            notaryIsSignatory = signers.isEmpty(),
+            tipPercentage = tipPercentage,
+            epochs = epochRange
+        )
 
-            accessFactorSourcesProxy.getSignatures(
-                accessFactorSourcesInput = AccessFactorSourcesInput.ToGetSignatures(
-                    signPurpose = SignPurpose.SignTransaction,
-                    signers = signers,
-                    signRequest = SignRequest.TransactionIntentSignRequest(transactionIntent = intent)
-                )
-            ).map { signaturesResult ->
-                val intentSignatures = signaturesResult.signersWithSignatures.values.map { IntentSignature.init(it) }
-                SignedIntent(
-                    intent = intent,
-                    intentSignatures = IntentSignatures(signatures = intentSignatures)
-                )
-            }
-        }.mapError { error ->
-            if (error is RejectedByUser || (error is FailedToSignTransaction && error.reason == UserRejectedSigningOfTransaction)) {
-                RejectedByUser
-            } else {
-                PrepareTransactionException.SignCompiledTransactionIntent(error)
-            }
+        sargonOsManager.sargonOs.signTransaction(
+            transactionIntent = intent,
+            roleKind = RoleKind.PRIMARY
+        )
+    }.mapError { error ->
+        if (error is CommonException.SigningRejected) {
+            RejectedByUser
+        } else {
+            PrepareTransactionException.SignCompiledTransactionIntent(error)
         }
+    }
 
     private fun notarize(
         signedIntent: SignedIntent,
