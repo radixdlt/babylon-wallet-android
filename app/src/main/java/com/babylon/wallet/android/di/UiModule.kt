da17f6712e8572d6e535cf505a722a73d568284b
micbakos-rdx <michael.bakogiannis@rdx.works>
2024-12-06 11:21:34 +0200

    Make proxy singleton

--- app/src/main/java/com/babylon/wallet/android/di/UiModule.kt
+++ app/src/main/java/com/babylon/wallet/android/di/UiModule.kt
@@ -7,9 +7,10 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityRetainedComponent
+import dagger.hilt.components.SingletonComponent
 
 @Module
-@InstallIn(ActivityRetainedComponent::class)
+@InstallIn(SingletonComponent::class)
 interface UiModule {
 
     @Binds
