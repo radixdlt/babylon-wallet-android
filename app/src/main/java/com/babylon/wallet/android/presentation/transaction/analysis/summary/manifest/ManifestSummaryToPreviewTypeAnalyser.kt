1dc837a16f9e39994097b8fbe756ea4d67829737
Sergiu Puhalschi <sergiu.puhalschi@rdx.works>
2024-12-16 14:59:21 +0200

    Handle non-enclosed pre-auth manifest classification

--- app/src/main/java/com/babylon/wallet/android/presentation/transaction/analysis/summary/manifest/ManifestSummaryToPreviewTypeAnalyser.kt
+++ app/src/main/java/com/babylon/wallet/android/presentation/transaction/analysis/summary/manifest/ManifestSummaryToPreviewTypeAnalyser.kt
@@ -5,6 +5,7 @@ import com.babylon.wallet.android.domain.usecases.assets.ResolveAssetsFromAddres
 import com.babylon.wallet.android.presentation.transaction.PreviewType
 import com.babylon.wallet.android.presentation.transaction.analysis.summary.Summary
 import com.babylon.wallet.android.presentation.transaction.analysis.summary.SummaryToPreviewTypeAnalyzer
+import com.radixdlt.sargon.ManifestClass
 import com.radixdlt.sargon.ManifestSummary
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
@@ -19,6 +20,9 @@ class ManifestSummaryToPreviewTypeAnalyser @Inject constructor(
 ) : SummaryToPreviewTypeAnalyzer<Summary.FromStaticAnalysis> {
 
     override suspend fun analyze(summary: Summary.FromStaticAnalysis): PreviewType {
+        val isConforming = summary.summary.classification.firstOrNull()?.isConforming ?: false
+        if (!isConforming) return PreviewType.NonConforming
+
         val assets = resolveAssetsFromAddressUseCase(addresses = summary.summary.involvedResourceAddresses()).getOrThrow()
         val profile = getProfileUseCase()
 
@@ -47,4 +51,7 @@ class ManifestSummaryToPreviewTypeAnalyser @Inject constructor(
             .mapNotNull { it.getOrNull() }
             .distinctBy { it.first }
     }
+
+    private val ManifestClass.isConforming: Boolean
+        get() = this == ManifestClass.GENERAL_SUBINTENT
 }
