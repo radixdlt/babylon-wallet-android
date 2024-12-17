22649927d322ba30e5f52b58c7d50b23a62634ce
giannis.tsepas <giannis.tsepas@rdx.works>
2024-12-17 20:47:22 +0200

    added composable FactorSourceInstancesList for all kinds of factor sources that will be listed in factor source instances screens

---
+++ app/src/main/java/com/babylon/wallet/android/presentation/settings/securitycenter/securityfactors/composables/FactorSourceInstancesList.kt
@@ -0,0 +1,112 @@
+package com.babylon.wallet.android.presentation.settings.securitycenter.securityfactors.composables
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.babylon.wallet.android.R
+import com.babylon.wallet.android.designsystem.composable.RadixSecondaryButton
+import com.babylon.wallet.android.designsystem.theme.RadixTheme
+import com.babylon.wallet.android.presentation.dialogs.info.GlossaryItem
+import com.babylon.wallet.android.presentation.ui.composables.InfoButton
+import com.babylon.wallet.android.presentation.ui.composables.card.FactorSourceInstanceCardView
+import com.babylon.wallet.android.presentation.ui.model.factors.FactorSourceInstanceCard
+import com.radixdlt.sargon.FactorSourceId
+import kotlinx.collections.immutable.PersistentList
+
+@Composable
+fun FactorSourceInstancesList(
+    modifier: Modifier = Modifier,
+    mainFactorSourceInstance: FactorSourceInstanceCard?,
+    factorSourceInstances: PersistentList<FactorSourceInstanceCard>,
+    @StringRes factorSourceDescriptionText: Int,
+    @StringRes addFactorSourceButtonTitle: Int,
+    onFactorSourceInstanceClick: (FactorSourceId.Hash) -> Unit,
+    onAddFactorSourceClick: () -> Unit,
+    onInfoClick: (GlossaryItem) -> Unit,
+) {
+    LazyColumn(
+        modifier = modifier.fillMaxWidth(),
+        contentPadding = PaddingValues(horizontal = RadixTheme.dimensions.paddingDefault),
+        verticalArrangement = Arrangement.spacedBy(RadixTheme.dimensions.paddingDefault)
+    ) {
+        item {
+            Text(
+                modifier = Modifier.padding(
+                    top = RadixTheme.dimensions.paddingDefault,
+                    bottom = RadixTheme.dimensions.paddingLarge
+                ),
+                text = stringResource(id = factorSourceDescriptionText),
+                style = RadixTheme.typography.body1HighImportance,
+                color = RadixTheme.colors.gray2
+            )
+        }
+        mainFactorSourceInstance?.let {
+            item {
+                Text(
+                    modifier = Modifier.padding(bottom = RadixTheme.dimensions.paddingDefault),
+                    text = stringResource(id = R.string.factorSources_list_default),
+                    style = RadixTheme.typography.body1HighImportance,
+                    color = RadixTheme.colors.gray2
+                )
+                FactorSourceInstanceCardView(
+                    modifier = Modifier
+                        .padding(bottom = RadixTheme.dimensions.paddingMedium)
+                        .clickable { onFactorSourceInstanceClick(it.id) },
+                    item = it
+                )
+            }
+            item {
+                Text(
+                    text = stringResource(id = R.string.factorSources_list_others),
+                    style = RadixTheme.typography.body1HighImportance,
+                    color = RadixTheme.colors.gray2
+                )
+            }
+        }
+
+        items(factorSourceInstances) {
+            FactorSourceInstanceCardView(
+                modifier = Modifier.clickable { onFactorSourceInstanceClick(it.id) },
+                item = it
+            )
+        }
+
+        item {
+            Spacer(modifier = Modifier.height(RadixTheme.dimensions.paddingDefault))
+            RadixSecondaryButton(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentWidth(align = Alignment.CenterHorizontally),
+                text = stringResource(id = addFactorSourceButtonTitle),
+                onClick = onAddFactorSourceClick,
+                throttleClicks = true
+            )
+            InfoButton(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentWidth(align = Alignment.CenterHorizontally)
+                    .padding(
+                        horizontal = RadixTheme.dimensions.paddingDefault,
+                        vertical = RadixTheme.dimensions.paddingLarge
+                    ),
+                text = "Learn about biometrics/PIN", // TODO
+                onClick = {
+                    onInfoClick(GlossaryItem.personas) // TODO
+                }
+            )
+        }
+    }
+}
