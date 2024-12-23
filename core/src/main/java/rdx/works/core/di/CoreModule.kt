84b938b72c543779506efdcf17b4e7d89f76d5da
Sergiu Puhalschi <sergiu.puhalschi@rdx.works>
2024-12-23 17:16:08 +0200

    Bump Sargon version

--- core/src/main/java/rdx/works/core/di/CoreModule.kt
+++ core/src/main/java/rdx/works/core/di/CoreModule.kt
@@ -3,15 +3,15 @@ package rdx.works.core.di
 import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
-import com.radixdlt.sargon.AuthenticationSigningRequest
-import com.radixdlt.sargon.AuthenticationSigningResponse
 import com.radixdlt.sargon.Bios
 import com.radixdlt.sargon.CommonException
 import com.radixdlt.sargon.HostInteractor
 import com.radixdlt.sargon.KeyDerivationRequest
 import com.radixdlt.sargon.KeyDerivationResponse
+import com.radixdlt.sargon.SignRequestOfAuthIntent
 import com.radixdlt.sargon.SignRequestOfSubintent
 import com.radixdlt.sargon.SignRequestOfTransactionIntent
+import com.radixdlt.sargon.SignWithFactorsOutcomeOfAuthIntentHash
 import com.radixdlt.sargon.SignWithFactorsOutcomeOfSubintentHash
 import com.radixdlt.sargon.SignWithFactorsOutcomeOfTransactionIntentHash
 import com.radixdlt.sargon.os.SargonOsManager
@@ -98,15 +98,15 @@ private class HostInteractorStub : HostInteractor {
         throw CommonException.Unknown()
     }
 
-    override suspend fun signAuth(request: AuthenticationSigningRequest): AuthenticationSigningResponse {
-        throw CommonException.Unknown()
-    }
-
     override suspend fun signSubintents(request: SignRequestOfSubintent): SignWithFactorsOutcomeOfSubintentHash {
         throw CommonException.Unknown()
     }
 
     override suspend fun signTransactions(request: SignRequestOfTransactionIntent): SignWithFactorsOutcomeOfTransactionIntentHash {
+        throw CommonException.Unknown()
+    }
+
+    override suspend fun signAuth(request: SignRequestOfAuthIntent): SignWithFactorsOutcomeOfAuthIntentHash {
         throw CommonException.Unknown()
     }
 }
