1a32e04ef96b69b1e0e561467b5fa6ac5c6d6704
giannis.tsepas <giannis.tsepas@rdx.works>
2024-12-29 15:20:30 +0200

    added SelectableSingleChoiceFactorSourceCard in SelectableFactorSourceCardView

--- app/src/main/java/com/babylon/wallet/android/presentation/ui/composables/card/SelectableFactorSourceCardView.kt
+++ app/src/main/java/com/babylon/wallet/android/presentation/ui/composables/card/SelectableFactorSourceCardView.kt
@@ -18,15 +18,54 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.babylon.wallet.android.designsystem.theme.RadixTheme
 import com.babylon.wallet.android.domain.model.Selectable
 import com.babylon.wallet.android.presentation.ui.RadixWalletPreviewTheme
+import com.babylon.wallet.android.presentation.ui.composables.RadixRadioButton
 import com.babylon.wallet.android.presentation.ui.model.factors.FactorSourceCard
+import com.babylon.wallet.android.presentation.ui.model.factors.FactorSourceStatusMessage
 import com.babylon.wallet.android.presentation.ui.modifier.noIndicationClickable
+import com.radixdlt.sargon.Account
 import com.radixdlt.sargon.FactorSourceId
 import com.radixdlt.sargon.FactorSourceKind
 import com.radixdlt.sargon.MnemonicWithPassphrase
 import com.radixdlt.sargon.annotation.UsesSampleValues
 import com.radixdlt.sargon.extensions.init
 import com.radixdlt.sargon.samples.sample
+import com.radixdlt.sargon.samples.sampleMainnet
 import kotlinx.collections.immutable.persistentListOf
+
+@Composable
+fun SelectableSingleChoiceFactorSourceCard(
+    modifier: Modifier = Modifier,
+    item: Selectable<FactorSourceCard>,
+    onSelect: (FactorSourceCard) -> Unit
+) {
+    FactorSourceCardView(
+        modifier = modifier.noIndicationClickable { onSelect(item.data) },
+        item = item.data,
+        endContent = {
+            RadioButtonSelectorView(
+                isSelected = item.selected,
+                onSelectedChange = { onSelect(item.data) }
+            )
+        }
+    )
+}
+
+@Composable
+private fun RadioButtonSelectorView(
+    isSelected: Boolean,
+    onSelectedChange: () -> Unit
+) {
+    Row {
+        Spacer(modifier = Modifier.width(RadixTheme.dimensions.paddingSmall))
+
+        RadixRadioButton(
+            selected = isSelected,
+            onClick = onSelectedChange
+        )
+
+        Spacer(modifier = Modifier.width(RadixTheme.dimensions.paddingDefault))
+    }
+}
 
 @Composable
 fun SelectableMultiChoiceFactorSourceCard(
@@ -121,6 +160,34 @@ private fun CheckboxSelectorView(
         )
 
         Spacer(modifier = Modifier.width(RadixTheme.dimensions.paddingSmall))
+    }
+}
+
+@Composable
+@Preview
+@UsesSampleValues
+private fun SelectableSingleChoiceFactorSourceCardPreview() {
+    RadixWalletPreviewTheme {
+        SelectableSingleChoiceFactorSourceCard(
+            item = Selectable(
+                data = FactorSourceCard(
+                    id = FactorSourceId.Hash.init(
+                        kind = FactorSourceKind.ARCULUS_CARD,
+                        mnemonicWithPassphrase = MnemonicWithPassphrase.sample(),
+                    ),
+                    name = "Arculus Card Secret",
+                    includeDescription = false,
+                    lastUsedOn = "Today",
+                    kind = FactorSourceKind.ARCULUS_CARD,
+                    messages = persistentListOf(FactorSourceStatusMessage.SecurityPrompt.WriteDownSeedPhrase),
+                    accounts = persistentListOf(Account.sampleMainnet()),
+                    personas = persistentListOf(),
+                    hasHiddenEntities = false
+                ),
+                selected = true
+            ),
+            onSelect = {}
+        )
     }
 }
 
