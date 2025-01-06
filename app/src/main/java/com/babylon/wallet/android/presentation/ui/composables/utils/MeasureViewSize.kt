4631c39684fba28579fd5fc991a3f4b1e20a2545
Sergiu Puhalschi <sergiu.puhalschi@rdx.works>
2025-01-06 09:47:43 +0200

    Add doc

--- app/src/main/java/com/babylon/wallet/android/presentation/ui/composables/utils/MeasureViewSize.kt
+++ app/src/main/java/com/babylon/wallet/android/presentation/ui/composables/utils/MeasureViewSize.kt
@@ -5,22 +5,33 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.SubcomposeLayout
 import androidx.compose.ui.unit.DpSize
 
+/**
+ * Measure the size of a view and pass it to the content composable.
+ *
+ * Check [SubcomposeLayout] for more information on how to to use the values
+ * calculated during the measurement as params for the composition of the children.
+ *
+ * @param viewToMeasure The view to measure.
+ * @param modifier The modifier to apply to the layout.
+ * @param content The content composable that will receive the measured size.
+ */
 @Composable
 fun MeasureViewSize(
     viewToMeasure: @Composable () -> Unit,
     modifier: Modifier = Modifier,
     content: @Composable (DpSize) -> Unit,
 ) {
-    SubcomposeLayout(modifier = modifier) { constraints ->
+    SubcomposeLayout(
+        modifier = modifier
+    ) { constraints ->
         val measuredSize = subcompose("viewToMeasure") {
             viewToMeasure()
-        }[0].measure(constraints)
-            .let {
-                DpSize(
-                    width = it.width.toDp(),
-                    height = it.height.toDp()
-                )
-            }
+        }.first().measure(constraints).let {
+            DpSize(
+                width = it.width.toDp(),
+                height = it.height.toDp()
+            )
+        }
 
         val contentPlaceable = subcompose("content") {
             content(measuredSize)
