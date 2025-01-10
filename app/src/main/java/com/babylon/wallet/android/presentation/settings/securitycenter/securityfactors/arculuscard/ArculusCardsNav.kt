62a0ddb1b7683dd91d5c5efc42c024dc40822386
giannis.tsepas <giannis.tsepas@rdx.works>
2025-01-10 12:30:16 +0200

    added ArculusCardsScreen

---
+++ app/src/main/java/com/babylon/wallet/android/presentation/settings/securitycenter/securityfactors/arculuscard/ArculusCardsNav.kt
@@ -0,0 +1,50 @@
+package com.babylon.wallet.android.presentation.settings.securitycenter.securityfactors.arculuscard
+
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import com.babylon.wallet.android.presentation.dialogs.info.GlossaryItem
+import com.radixdlt.sargon.FactorSourceId
+
+private const val ROUTE_ARCULUS_CARDS_SCREEN = "route_arculus_cards_screen"
+
+fun NavController.arculusCards() {
+    navigate(ROUTE_ARCULUS_CARDS_SCREEN) {
+        launchSingleTop = true
+    }
+}
+
+fun NavGraphBuilder.arculusCards(
+    onNavigateToArculusFactorSourceDetails: (factorSourceId: FactorSourceId) -> Unit,
+    onNavigateToAddArculusCard: () -> Unit,
+    onInfoClick: (GlossaryItem) -> Unit,
+    onBackClick: () -> Unit
+) {
+    composable(
+        route = ROUTE_ARCULUS_CARDS_SCREEN,
+        enterTransition = {
+            slideIntoContainer(AnimatedContentTransitionScope.SlideDirection.Left)
+        },
+        exitTransition = {
+            ExitTransition.None
+        },
+        popEnterTransition = {
+            EnterTransition.None
+        },
+        popExitTransition = {
+            slideOutOfContainer(AnimatedContentTransitionScope.SlideDirection.Right)
+        }
+    ) {
+        ArculusCardsScreen(
+            viewModel = hiltViewModel(),
+            onNavigateToArculusFactorSourceDetails = onNavigateToArculusFactorSourceDetails,
+            onNavigateToAddArculusCard = onNavigateToAddArculusCard,
+            onInfoClick = onInfoClick,
+            onBackClick = onBackClick
+        )
+    }
+}
---
+++ app/src/main/java/com/babylon/wallet/android/presentation/settings/securitycenter/securityfactors/arculuscard/ArculusCardsViewModel.kt
@@ -0,0 +1,103 @@
+package com.babylon.wallet.android.presentation.settings.securitycenter.securityfactors.arculuscard
+
+import androidx.lifecycle.viewModelScope
+import com.babylon.wallet.android.di.coroutines.DefaultDispatcher
+import com.babylon.wallet.android.domain.usecases.factorsources.GetFactorSourcesUseCaseOfType
+import com.babylon.wallet.android.presentation.common.OneOffEvent
+import com.babylon.wallet.android.presentation.common.OneOffEventHandler
+import com.babylon.wallet.android.presentation.common.OneOffEventHandlerImpl
+import com.babylon.wallet.android.presentation.common.StateViewModel
+import com.babylon.wallet.android.presentation.common.UiState
+import com.babylon.wallet.android.presentation.ui.model.factors.FactorSourceCard
+import com.babylon.wallet.android.presentation.ui.model.factors.FactorSourceStatusMessage
+import com.babylon.wallet.android.utils.relativeTimeFormatted
+import com.radixdlt.sargon.Account
+import com.radixdlt.sargon.ArculusCardFactorSource
+import com.radixdlt.sargon.FactorSource
+import com.radixdlt.sargon.FactorSourceId
+import com.radixdlt.sargon.Persona
+import com.radixdlt.sargon.ProfileToCheck
+import com.radixdlt.sargon.extensions.asGeneral
+import com.radixdlt.sargon.extensions.kind
+import com.radixdlt.sargon.os.SargonOsManager
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.collections.immutable.PersistentList
+import kotlinx.collections.immutable.persistentListOf
+import kotlinx.collections.immutable.toPersistentList
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ArculusCardsViewModel @Inject constructor(
+    getFactorSourcesUseCaseOfType: GetFactorSourcesUseCaseOfType,
+    private val sargonOsManager: SargonOsManager,
+    @DefaultDispatcher private val defaultDispatcher: CoroutineDispatcher
+) : StateViewModel<ArculusCardsViewModel.State>(),
+    OneOffEventHandler<ArculusCardsViewModel.Event> by OneOffEventHandlerImpl() {
+
+    override fun initialState(): State = State()
+
+    init {
+        getFactorSourcesUseCaseOfType<FactorSource.ArculusCard>()
+            .map { arculusFactorSource ->
+                val entitiesLinkedToDeviceFactorSource = sargonOsManager.sargonOs.entitiesLinkedToFactorSource(
+                    factorSource = FactorSource.ArculusCard(arculusFactorSource.value),
+                    profileToCheck = ProfileToCheck.Current
+                )
+
+                val factorSourceCard = arculusFactorSource.value.toFactorSourceCard(
+                    messages = persistentListOf(),
+                    accounts = entitiesLinkedToDeviceFactorSource.accounts.toPersistentList(),
+                    personas = entitiesLinkedToDeviceFactorSource.personas.toPersistentList(),
+                    hasHiddenEntities = entitiesLinkedToDeviceFactorSource.hiddenAccounts.isNotEmpty() ||
+                        entitiesLinkedToDeviceFactorSource.hiddenPersonas.isNotEmpty()
+                )
+
+                _state.update { state ->
+                    state.copy(arculusFactorSources = state.arculusFactorSources.add(factorSourceCard))
+                }
+            }
+            .flowOn(defaultDispatcher)
+            .launchIn(viewModelScope)
+    }
+
+    private fun ArculusCardFactorSource.toFactorSourceCard(
+        includeDescription: Boolean = false,
+        messages: PersistentList<FactorSourceStatusMessage> = persistentListOf(),
+        accounts: PersistentList<Account> = persistentListOf(),
+        personas: PersistentList<Persona> = persistentListOf(),
+        hasHiddenEntities: Boolean
+    ): FactorSourceCard {
+        return FactorSourceCard(
+            id = id.asGeneral(),
+            name = hint.label,
+            includeDescription = includeDescription,
+            lastUsedOn = common.lastUsedOn.relativeTimeFormatted(),
+            kind = kind,
+            messages = messages,
+            accounts = accounts,
+            personas = personas,
+            hasHiddenEntities = hasHiddenEntities
+        )
+    }
+
+    fun onArculusFactorSourceClick(factorSourceId: FactorSourceId) {
+        viewModelScope.launch {
+            sendEvent(Event.NavigateToArculusFactorSourceDetails(factorSourceId))
+        }
+    }
+
+    data class State(
+        val arculusFactorSources: PersistentList<FactorSourceCard> = persistentListOf(),
+    ) : UiState
+
+    sealed interface Event : OneOffEvent {
+
+        data class NavigateToArculusFactorSourceDetails(val factorSourceId: FactorSourceId) : Event
+    }
+}
