f5b3b1140ae89908bb00a2d0a6883ff418ac5a62
micbakos-rdx <michael.bakogiannis@rdx.works>
2025-02-07 18:17:22 +0200

    Add apply shield interaction in ApplyShieldViewModel

--- app/src/main/java/com/babylon/wallet/android/presentation/settings/securitycenter/applyshield/apply/ApplyShieldViewModel.kt
+++ app/src/main/java/com/babylon/wallet/android/presentation/settings/securitycenter/applyshield/apply/ApplyShieldViewModel.kt
@@ -1,7 +1,12 @@
 package com.babylon.wallet.android.presentation.settings.securitycenter.applyshield.apply
 
 import androidx.lifecycle.viewModelScope
+import com.babylon.wallet.android.data.dapp.IncomingRequestRepository
+import com.babylon.wallet.android.data.dapp.model.TransactionType
 import com.babylon.wallet.android.di.coroutines.DefaultDispatcher
+import com.babylon.wallet.android.domain.model.transaction.UnvalidatedManifestData
+import com.babylon.wallet.android.domain.model.transaction.prepareInternalTransactionRequest
+import com.babylon.wallet.android.presentation.accessfactorsources.applyshield.ApplyShieldViewModel.Event
 import com.babylon.wallet.android.presentation.common.OneOffEvent
 import com.babylon.wallet.android.presentation.common.OneOffEventHandler
 import com.babylon.wallet.android.presentation.common.OneOffEventHandlerImpl
@@ -11,6 +16,8 @@ import com.babylon.wallet.android.presentation.common.UiState
 import com.babylon.wallet.android.utils.callSafely
 import com.radixdlt.sargon.AddressOfAccountOrPersona
 import com.radixdlt.sargon.SecurityStructureId
+import com.radixdlt.sargon.extensions.bytes
+import com.radixdlt.sargon.extensions.toList
 import com.radixdlt.sargon.os.SargonOsManager
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineDispatcher
@@ -22,7 +29,8 @@ import javax.inject.Inject
 @HiltViewModel
 class ApplyShieldViewModel @Inject constructor(
     private val sargonOsManager: SargonOsManager,
-    @DefaultDispatcher private val dispatcher: CoroutineDispatcher
+    @DefaultDispatcher private val dispatcher: CoroutineDispatcher,
+    private val incomingRequestRepository: IncomingRequestRepository
 ) : StateViewModel<ApplyShieldViewModel.State>(),
     OneOffEventHandler<ApplyShieldViewModel.Event> by OneOffEventHandlerImpl() {
 
@@ -40,8 +48,30 @@ class ApplyShieldViewModel @Inject constructor(
             Timber.d("Interaction: $interaction")
             // TODO prepare the batch transactions request and add it to the queue
 
-            _state.update { state -> state.copy(isLoading = false) }
-            sendEvent(Event.ShieldApplied)
+            // This code is temporary
+            if (interaction.transactions.size == 1) {
+                val transaction = interaction.transactions.first()
+                val request = UnvalidatedManifestData(
+                    instructions = transaction.transactionManifestString,
+                    plainMessage = null,
+                    networkId = sargonOsManager.sargonOs.currentNetworkId(),
+                    blobs = transaction.blobs.toList().map { it.bytes },
+                ).prepareInternalTransactionRequest(
+                    transactionType = TransactionType.SecurifyEntity(
+                        entityAddress = entityAddresses.first() //
+                    )
+                )
+
+                _state.update { state -> state.copy(isLoading = false) }
+                sendEvent(Event.ShieldApplied)
+
+                incomingRequestRepository.add(request)
+            } else {
+                // Batch request
+
+                _state.update { state -> state.copy(isLoading = false) }
+                sendEvent(Event.ShieldApplied)
+            }
         }.onFailure { error ->
             _state.update { state ->
                 state.copy(
