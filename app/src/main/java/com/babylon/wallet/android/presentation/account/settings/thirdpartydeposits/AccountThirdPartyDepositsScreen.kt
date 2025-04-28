e1bdf33e9c5c87e810d276a28a2331ea71f944a6
micbakos-rdx <michael.bakogiannis@radixpublishing.com>
2025-04-28 16:12:08 +0300

    Replace Text with material 3 in AccountThirdPartyDepositsScreen

--- app/src/main/java/com/babylon/wallet/android/presentation/account/settings/thirdpartydeposits/AccountThirdPartyDepositsScreen.kt
+++ app/src/main/java/com/babylon/wallet/android/presentation/account/settings/thirdpartydeposits/AccountThirdPartyDepositsScreen.kt
@@ -15,7 +15,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Text
+import androidx.compose.material3.Text
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
