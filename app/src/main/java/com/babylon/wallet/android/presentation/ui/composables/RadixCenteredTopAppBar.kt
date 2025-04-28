8b8ade8fb19ffa5213915146a5da96be29244f0a
micbakos-rdx <michael.bakogiannis@radixpublishing.com>
2025-04-28 14:47:33 +0300

    Use material3 Text, Icon and IconButton in RadixCenteredTopAppBar

--- app/src/main/java/com/babylon/wallet/android/presentation/ui/composables/RadixCenteredTopAppBar.kt
+++ app/src/main/java/com/babylon/wallet/android/presentation/ui/composables/RadixCenteredTopAppBar.kt
@@ -5,9 +5,9 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.statusBars
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Text
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.TopAppBarDefaults
