70481095efa46b36755eb644ea5e2c63de623b4c
micbakos-rdx <michael.bakogiannis@radixpublishing.com>
2025-05-14 09:49:33 +0300

    Move color theme in radix colors

--- designsystem/src/main/java/com/babylon/wallet/android/designsystem/theme/RadixColors.kt
+++ designsystem/src/main/java/com/babylon/wallet/android/designsystem/theme/RadixColors.kt
@@ -2,6 +2,10 @@
 
 package com.babylon.wallet.android.designsystem.theme
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
 
 val Blue1 = Color(0xFF060F8F)
@@ -30,3 +34,102 @@ val White = Color(0xFFFFFFFF)
 val Black = Color(0xFF000000)
 
 
+data class RadixColors(
+    val background: Color,
+    val backgroundSecondary: Color,
+    val backgroundTertiary: Color,
+    val backgroundTransparent: Color,
+    val text: Color,
+    val textSecondary: Color,
+    val textTertiary: Color,
+    val textButton: Color,
+    val icon: Color,
+    val iconSecondary: Color,
+    val iconTertiary: Color,
+    val primaryButton: Color,
+    val divider: Color,
+    val border: Color,
+    val textFieldBorder: Color,
+    val textFieldFocusedBorder: Color,
+    val textFieldBackground: Color,
+    val toggleActive: Color,
+    val ok: Color,
+    val error: Color,
+    val errorSecondary: Color,
+    val warning: Color,
+    val warningSecondary: Color,
+    val card: Color
+)
+
+private val LightColorPalette = RadixColors(
+    background = White,
+    backgroundSecondary = Gray5,
+    backgroundTertiary = Gray4,
+    backgroundTransparent = White.copy(alpha = 0.3f),
+    text = Gray1,
+    textSecondary = Gray2,
+    textTertiary = Gray3,
+    textButton = Blue2,
+    icon = Gray1,
+    iconSecondary = Gray2,
+    iconTertiary = Gray3,
+    primaryButton = Blue2,
+    divider = Gray4,
+    border = Gray1,
+    textFieldBorder = Gray4,
+    textFieldFocusedBorder = Gray1,
+    textFieldBackground = Gray5,
+    toggleActive = Gray1,
+    ok = Green1,
+    error = Red1,
+    errorSecondary = LightRed,
+    warning = Orange3,
+    warningSecondary = LightOrange,
+    card = White
+)
+
+private val DarkColorPalette = RadixColors(
+    background = Color(0xFF28292A),
+    backgroundSecondary = Color(0xFF1E1F1F),
+    backgroundTertiary = Color(0xFF404243),
+    backgroundTransparent = Gray5.copy(alpha = 0.3f),
+    text = Gray5,
+    textSecondary = Gray4,
+    textTertiary = Gray2,
+    textButton = Color(0xFF90CAF9),
+    icon = Gray5,
+    iconSecondary = Gray4,
+    iconTertiary = Gray2,
+    primaryButton = Color(0xFF00C389),
+    divider = Color(0xFF404243),
+    border = Gray5,
+    textFieldBorder = Color(0xFF797B7F),
+    textFieldFocusedBorder = Gray5,
+    textFieldBackground = Color(0xFF404243),
+    toggleActive = Color(0xFF00C389),
+    ok = Green1,
+    error = Orange2,
+    errorSecondary = LightRed,
+    warning = Color(0xFFC47F00),
+    warningSecondary = Color(0xFFE6DCCB),
+    card = Color(0xFF28292A)
+)
+
+internal val LocalRadixColors = staticCompositionLocalOf<RadixColors> {
+    error("No RadixColors provided")
+}
+
+@Composable
+internal fun ProvideRadixColors(content: @Composable () -> Unit) {
+    val isDarkMode = LocalRadixThemeConfig.current.isDarkTheme
+    val colors = remember(isDarkMode) {
+        if (isDarkMode) {
+            DarkColorPalette
+        } else {
+            LightColorPalette
+        }
+    }
+    CompositionLocalProvider(LocalRadixColors provides colors) {
+        content()
+    }
+}
