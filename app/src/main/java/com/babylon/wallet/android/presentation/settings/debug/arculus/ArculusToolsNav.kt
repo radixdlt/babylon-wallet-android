6f660b9ed6aaba33b0ae5ee059a7f166a8a4ab5c
Sergiu Puhalschi <sergiu@radixpublishing.com>
2025-08-26 16:11:15 +0300

    Fix duplicate key collision crash because of code obfuscation

---
+++ app/src/main/java/com/babylon/wallet/android/presentation/settings/debug/arculus/ArculusToolsNav.kt
@@ -0,0 +1,40 @@
+package com.babylon.wallet.android.presentation.settings.debug.arculus
+
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+
+private const val ROUTE_ARCULUS_TOOLS = "debug_arculus_tools"
+
+fun NavController.arculusTools() {
+    navigate(ROUTE_ARCULUS_TOOLS)
+}
+
+fun NavGraphBuilder.arculusTools(
+    onBackClick: () -> Unit
+) {
+    composable(
+        route = ROUTE_ARCULUS_TOOLS,
+        enterTransition = {
+            slideIntoContainer(AnimatedContentTransitionScope.SlideDirection.Left)
+        },
+        exitTransition = {
+            ExitTransition.None
+        },
+        popEnterTransition = {
+            EnterTransition.None
+        },
+        popExitTransition = {
+            slideOutOfContainer(AnimatedContentTransitionScope.SlideDirection.Right)
+        }
+    ) {
+        ArculusToolsScreen(
+            viewModel = hiltViewModel(),
+            onBackClick = onBackClick
+        )
+    }
+}
--- app/src/main/java/com/babylon/wallet/android/presentation/settings/debug/arculus/ArculusToolsViewModel.kt
+++ app/src/main/java/com/babylon/wallet/android/presentation/settings/debug/arculus/ArculusToolsViewModel.kt
@@ -1,4 +1,4 @@
-package com.babylon.wallet.android.presentation.settings.debug
+package com.babylon.wallet.android.presentation.settings.debug.arculus
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
