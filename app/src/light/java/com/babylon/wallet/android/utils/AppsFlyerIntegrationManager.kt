4ec3aed6445885a37e0e56eac6af7cf5df1e3d46
Sergiu Puhalschi <sergiu@radixpublishing.com>
2026-02-10 16:03:29 +0200

    Split into light and full version

---
+++ app/src/light/java/com/babylon/wallet/android/utils/AppsFlyerIntegrationManager.kt
@@ -0,0 +1,12 @@
+package com.babylon.wallet.android.utils
+
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class AppsFlyerIntegrationManager @Inject constructor() {
+
+    fun init() {
+        // No-op: AppsFlyer not available in light flavor
+    }
+}
